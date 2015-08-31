3fcc05ea50dc7eb7de3527dc3fb35d0cad0ba774
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2015-08-30 20:10:26 -0700

    Created BaseQuery objects to store query logic Created DataConfidence Indicator and Rest controller Created CheckDataFeedDao and querys for the Data Confidence Indicators

---
+++ src/main/java/com/thinkbiganalytics/common/constants/CheckDataStepConstants.java
@@ -0,0 +1,10 @@
+package com.thinkbiganalytics.common.constants;
+
+/**
+ * Created by sr186054 on 8/28/15.
+ */
+public class CheckDataStepConstants {
+
+    public static final String VALIDATION_KEY = "checkData.validationCheck";
+    public static final String VALIDATION_MESSAGE_KEY = "checkData.validationMessage";
+}
