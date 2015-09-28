5da8fec25fec065ca723376c0e852578f25501d0
matt-hutton <matt.hutton@thinkbiganalytics.com>
2015-09-28 09:19:06 -0700

    switched to quartz scheduelr

---
+++ src/main/java/com/thinkbiganalytics/scheduler/quartz/SimpleSchedulerSetup.java
@@ -0,0 +1,65 @@
+package com.thinkbiganalytics.scheduler.quartz;
+
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
+
+import java.util.UUID;
+
+/**
+ * Configures Quartz to run a job with one step
+ */
+public class SimpleSchedulerSetup implements InitializingBean {
+
+    @Autowired
+    private QuartzScheduler quartzScheduler;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    private Object jobRunner;
+
+    private String cronExpression;
+
+    private String targetMethod;
+
+    /**
+     * Configure the quartz scheduler
+     * @param jobRunner the target job runner (implementing a run() method)
+     * @param cronExpression a cron expression for the schedule
+     */
+    public SimpleSchedulerSetup(Object jobRunner, String cronExpression) {
+        this(jobRunner, "run", cronExpression);
+    }
+
+    /**
+     * Configure the quartz scheduler
+     * @param jobRunner the target job runner
+     * @param targetMethod the method to invoke on the job runner
+     * @param cronExpression a cron expression for the schedule
+     *
+     */
+    public SimpleSchedulerSetup(Object jobRunner, String targetMethod, String cronExpression) {
+        this.jobRunner = jobRunner;
+        this.cronExpression = cronExpression;
+        this.targetMethod = targetMethod;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        MethodInvokingJobDetailFactoryBean jobDetailFactory = new MethodInvokingJobDetailFactoryBean();
+        jobDetailFactory.setTargetObject(this.jobRunner);
+        jobDetailFactory.setTargetMethod(this.targetMethod);
+        applicationContext.getAutowireCapableBeanFactory().initializeBean(jobDetailFactory, UUID.randomUUID().toString());
+
+        CronTriggerFactoryBean triggerFactoryBean = new CronTriggerFactoryBean();
+        triggerFactoryBean.setCronExpression(cronExpression);
+        triggerFactoryBean.setJobDetail(jobDetailFactory.getObject());
+        applicationContext.getAutowireCapableBeanFactory().initializeBean(triggerFactoryBean, UUID.randomUUID().toString());
+
+        quartzScheduler.scheduleJob(jobDetailFactory, triggerFactoryBean);
+    }
+}
