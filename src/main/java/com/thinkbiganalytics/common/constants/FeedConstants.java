9052574dde881202aa33adfeea24e7de843a22ee
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2015-10-07 09:59:53 -0700

    remove pipeline-controller dep from batch-common

---
+++ src/main/java/com/thinkbiganalytics/common/constants/FeedConstants.java
@@ -0,0 +1,13 @@
+package com.thinkbiganalytics.common.constants;
+
+/**
+ * Created by sr186054 on 10/7/15.
+ */
+public class FeedConstants {
+
+    public static final String PARAM__FEED_NAME = "feed"; // value is user defined String
+    public static final String PARAM__FEED_IS_PARENT = "feedIsParent"; // value is true false
+    public static final String PARAM__JOB_TYPE = "jobType"; // values defined below
+    public static final String PARAM_VALUE__JOB_TYPE_FEED = "FEED";
+    public static final String PARAM_VALUE__JOB_TYPE_CHECK = "CHECK";
+}
