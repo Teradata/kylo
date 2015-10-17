4dd01f4875c8004495fb2899265fab2dd8607351
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2015-10-16 17:16:06 -0700

    document JiraClient Interface

--- src/main/java/com/thinkbiganalytics/jira/JiraClient.java
+++ src/main/java/com/thinkbiganalytics/jira/JiraClient.java
@@ -7,7 +7,24 @@ import com.thinkbiganalytics.jira.domain.Issue;
  */
 public interface JiraClient {
 
+    /**
+     * Create a new Jira Issue
+     * @param projectKey The String Key.  If you have issue  IT-1, then this is "IT"
+     * @param summary
+     * @param description
+     * @param issueType  the String type (i.e. "Bug", "Task")
+     * @param assigneeName  the jira username
+     * @return
+     * @throws JiraException
+     */
     Issue createIssue(String projectKey, String summary, String description, String issueType, String assigneeName) throws JiraException;
+
+    /**
+     * Get a Jira Issue by its Key (i.e. JIRA-1)
+     * @param key
+     * @return
+     * @throws JiraException
+     */
     Issue getIssue(String key) throws JiraException;
 
 }
