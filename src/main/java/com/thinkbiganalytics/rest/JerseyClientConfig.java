0a958365c8337f8cb1661e8d6839aacf9d48f1ea
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2015-11-08 14:09:15 -0800

    Generic tba-rest-client, supports Jersey Pooled Connection. tba-jira uses tba-rest-client for connection pooling.

---
+++ src/main/java/com/thinkbiganalytics/rest/JerseyClientConfig.java
@@ -0,0 +1,155 @@
+package com.thinkbiganalytics.rest;
+
+
+import org.glassfish.jersey.internal.util.collection.Values;
+
+/**
+ * Created by sr186054 on 10/15/15.
+ */
+public class JerseyClientConfig {
+    private String host;
+    private String username;
+    private String password;
+    private boolean https;
+
+    private boolean keystoreOnClasspath;
+
+    private String keystorePath;
+    private String keystorePassword;
+
+    //Values are in milliseconds
+    private Integer readTimeout = null;
+    private Integer connectTimeout = null;
+
+    private boolean useConnectionPooling = true;
+
+
+
+    public JerseyClientConfig() {
+
+    }
+
+    public JerseyClientConfig(String host, String username, String password) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        this.https = false;
+        this.keystoreOnClasspath = false;
+        this.keystorePath = null;
+        this.keystorePassword = null;
+    }
+    public JerseyClientConfig(String host, String username, String password, boolean https, boolean keystoreOnClasspath, String keystorePath, String keystorePassword) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        this.https = https;
+        this.keystoreOnClasspath = keystoreOnClasspath;
+        this.keystorePath = keystorePath;
+        this.keystorePassword = keystorePassword;
+    }
+
+    public JerseyClientConfig(String host, String username, String password, boolean https, boolean keystoreOnClasspath, String keystorePath, String keystorePassword, Integer readTimeout, Integer connectTimeout) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        this.https = https;
+        this.keystoreOnClasspath = keystoreOnClasspath;
+        this.keystorePath = keystorePath;
+        this.keystorePassword = keystorePassword;
+        this.readTimeout = readTimeout;
+        this.connectTimeout = connectTimeout;
+    }
+
+    public String getKeystorePath() {
+        return keystorePath;
+    }
+
+    public void setKeystorePath(String keystorePath) {
+        this.keystorePath = keystorePath;
+    }
+
+    public String getKeystorePassword() {
+        return keystorePassword;
+    }
+
+    public void setKeystorePassword(String keystorePassword) {
+        this.keystorePassword = keystorePassword;
+    }
+
+    public String getHost() {
+        return host;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean isHttps() {
+        return https;
+    }
+
+    public void setHttps(boolean https) {
+        this.https = https;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl(){
+        String url = host;
+        if(https){
+            url = "https://"+url;
+        }
+        else {
+            url ="http://"+url;
+        }
+        return url;
+    }
+
+    public boolean isKeystoreOnClasspath() {
+        return keystoreOnClasspath;
+    }
+
+    public void setKeystoreOnClasspath(boolean keystoreOnClasspath) {
+        this.keystoreOnClasspath = keystoreOnClasspath;
+    }
+
+    public Integer getReadTimeout() {
+        return readTimeout;
+    }
+
+    public void setReadTimeout(Integer readTimeout) {
+        this.readTimeout = readTimeout;
+    }
+
+    public Integer getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Integer connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+
+    public boolean isUseConnectionPooling() {
+        return useConnectionPooling;
+    }
+
+    public void setUseConnectionPooling(boolean useConnectionPooling) {
+        this.useConnectionPooling = useConnectionPooling;
+    }
+}
---
+++ src/main/java/com/thinkbiganalytics/rest/JodaTimeMapperProvider.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2015.
+ */
+
+package com.thinkbiganalytics.rest;
+
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.joda.JodaMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * Created by sr186054 on 10/16/15.
+ */
+@Provider
+public class JodaTimeMapperProvider implements ContextResolver<ObjectMapper> {
+
+    final ObjectMapper defaultObjectMapper;
+   // final ObjectMapper combinedObjectMapper;
+
+    public JodaTimeMapperProvider() {
+        defaultObjectMapper = createDefaultMapper();
+     //   combinedObjectMapper = createCombinedObjectMapper();
+    }
+
+    @Override
+    public ObjectMapper getContext(final Class<?> type) {
+
+         return new JodaMapper();
+
+    }
+
+    private static ObjectMapper createDefaultMapper() {
+        final ObjectMapper result = new ObjectMapper();
+        result.enable(SerializationFeature.INDENT_OUTPUT);
+
+        return result;
+    }
+
+    private static AnnotationIntrospector createJaxbJacksonAnnotationIntrospector() {
+
+        final AnnotationIntrospector jaxbIntrospector = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+        final AnnotationIntrospector jacksonIntrospector = new JacksonAnnotationIntrospector();
+
+        return AnnotationIntrospector.pair(jacksonIntrospector, jaxbIntrospector);
+    }
+}
