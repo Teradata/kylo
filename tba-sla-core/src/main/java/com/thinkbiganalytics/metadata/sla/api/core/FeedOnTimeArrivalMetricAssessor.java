ba6e2cc8ad7e4687dc9658b32bcd37d57ea4a94e
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2015-11-12 08:54:21 -0800

    TDB-126, and TDB-131 - Fix SLA check for New Feeds

--- tba-sla-core/src/main/java/com/thinkbiganalytics/metadata/sla/api/core/FeedOnTimeArrivalMetricAssessor.java
+++ tba-sla-core/src/main/java/com/thinkbiganalytics/metadata/sla/api/core/FeedOnTimeArrivalMetricAssessor.java
@@ -1,41 +1,36 @@
 /**
- * 
+ *
  */
 package com.thinkbiganalytics.metadata.sla.api.core;
-
-import java.text.ParseException;
-import java.util.Date;
-
-import javax.inject.Inject;
-
-import org.joda.time.DateTime;
-import org.quartz.Calendar;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.thinkbiganalytics.calendar.HolidayCalendarService;
 import com.thinkbiganalytics.metadata.sla.api.AssessmentResult;
 import com.thinkbiganalytics.metadata.sla.api.Metric;
-import com.thinkbiganalytics.metadata.sla.api.ServiceLevelAssessmentException;
 import com.thinkbiganalytics.metadata.sla.spi.MetricAssessmentBuilder;
 import com.thinkbiganalytics.metadata.sla.spi.MetricAssessor;
 import com.thinkbiganalytics.pipelinecontroller.repositories.FeedRepository;
 import com.thinkbiganalytics.pipelinecontroller.rest.dataobjects.ExecutedFeed;
 import com.thinkbiganalytics.scheduler.util.CronExpressionUtil;
+import org.joda.time.DateTime;
+import org.quartz.Calendar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.inject.Inject;
+import java.util.Date;
 
 /**
- *
  * @author Sean Felten
  */
 public class FeedOnTimeArrivalMetricAssessor implements MetricAssessor<FeedOnTimeArrivalMetric> {
     private static final Logger LOG = LoggerFactory.getLogger(FeedOnTimeArrivalMetricAssessor.class);
-    
+
     @Inject
     private FeedRepository feedRepository;
-    
+
     @Inject
     private HolidayCalendarService calendarService;
-    
+
 
     /* (non-Javadoc)
      * @see com.thinkbiganalytics.metadata.sla.spi.MetricAssessor#accepts(com.thinkbiganalytics.metadata.sla.api.Metric)
@@ -52,17 +47,16 @@ public class FeedOnTimeArrivalMetricAssessor implements MetricAssessor<FeedOnTim
     @SuppressWarnings("unchecked")
     public void assess(FeedOnTimeArrivalMetric metric, MetricAssessmentBuilder builder) {
         LOG.debug("Assessing metric: ", metric);
-       
+
         builder.metric(metric);
-        
+
         Calendar calendar = getCalendar(metric);
         String feedName = metric.getFeedName();
         ExecutedFeed feed = this.feedRepository.findLastCompletedFeed(feedName);
 
 
-
         DateTime lastFeedTime = null;
-        if(feed != null){
+        if (feed != null) {
             lastFeedTime = feed.getEndTime();
         }
         Date expectedDate = CronExpressionUtil.getPreviousFireTime(metric.getExpectedExpression());
@@ -76,26 +70,25 @@ public class FeedOnTimeArrivalMetricAssessor implements MetricAssessor<FeedOnTim
         if (isHoliday) {
             LOG.debug("No data expected for feed {} due to a holiday", feedName);
             builder.message("No data expected for feed " + feedName + " due to a holiday")
-                   .result(AssessmentResult.SUCCESS);
-        } else if (lastFeedTime == null && (DateTime.now().isAfter(lateTime) || DateTime.now().isEqual(lateTime)) ){
+                    .result(AssessmentResult.SUCCESS);
+        } else if (lastFeedTime == null && (DateTime.now().isAfter(lateTime) || DateTime.now().isEqual(lateTime))) {
             LOG.debug("No feed with the specified name could be found: {}", feedName);
             builder.message("No feed with the specified name could be found: " + feedName)
                     .result(AssessmentResult.FAILURE);
             return;
-        }
-        else if(lastFeedTime == null && DateTime.now().isBefore(lateTime)) {
-            builder.message("No feed with the specified name could be found: " + feedName+" but the it is still before the late time of "+lateTime)
+        } else if (lastFeedTime == null && DateTime.now().isBefore(lateTime)) {
+            LOG.debug("No feed with the specified name could be found: {}, but the it is still before the late time of {}", feedName,lateTime);
+            builder.message("No feed with the specified name could be found: " + feedName + ", but the it is still before the late time of " + lateTime)
                     .result(AssessmentResult.SUCCESS);
-        }
-       else if (lastFeedTime != null && lastFeedTime.isAfter(expectedTime) && lastFeedTime.isBefore(lateTime)) {
+        } else if (lastFeedTime != null && lastFeedTime.isAfter(expectedTime) && lastFeedTime.isBefore(lateTime)) {
             LOG.debug("Data for feed {} arrived on {}, which was before late time: ", feedName, lastFeedTime, lateTime);
-            
+
             builder.message("Data for feed " + feedName + " arrived on " + lastFeedTime + ", which was before late time: " + lateTime)
-                   .result(AssessmentResult.SUCCESS);
+                    .result(AssessmentResult.SUCCESS);
         } else {
             LOG.debug("Data for feed {} has not arrived before the late time: ", feedName, lateTime);
-            
-            builder.message("Data for feed " + feedName + " has not arrived before the late time: " + lateTime+"\n The last successful feed was on "+lastFeedTime )
+
+            builder.message("Data for feed " + feedName + " has not arrived before the late time: " + lateTime + "\n The last successful feed was on " + lastFeedTime)
                     .result(AssessmentResult.FAILURE);
         }
     }
--- tba-sla-core/src/test/java/com/thinkbiganalytics/metadata/sla/spi/core/FeedOnTimeArrivalMetricAssessorTest.java
+++ tba-sla-core/src/test/java/com/thinkbiganalytics/metadata/sla/spi/core/FeedOnTimeArrivalMetricAssessorTest.java
@@ -1,19 +1,5 @@
 package com.thinkbiganalytics.metadata.sla.spi.core;
 
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.*;
-
-import java.text.ParseException;
-
-import org.joda.time.DateTime;
-import org.joda.time.Period;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.quartz.CronExpression;
-import org.quartz.impl.calendar.HolidayCalendar;
-
 import com.thinkbiganalytics.calendar.HolidayCalendarService;
 import com.thinkbiganalytics.metadata.sla.api.AssessmentResult;
 import com.thinkbiganalytics.metadata.sla.api.Metric;
@@ -23,44 +9,57 @@ import com.thinkbiganalytics.metadata.sla.spi.MetricAssessmentBuilder;
 import com.thinkbiganalytics.pipelinecontroller.repositories.FeedRepository;
 import com.thinkbiganalytics.pipelinecontroller.rest.dataobjects.ExecutedFeed;
 import com.thinkbiganalytics.scheduler.util.CronExpressionUtil;
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.quartz.CronExpression;
+import org.quartz.impl.calendar.HolidayCalendar;
+
+import java.text.ParseException;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class FeedOnTimeArrivalMetricAssessorTest {
-    
+
     private DateTime lateTime;
     private FeedOnTimeArrivalMetric metric;
-    
+
     @Mock
     private FeedRepository feedRepository;
-    
+
     @Mock
     private HolidayCalendar calendar;
-    
+
     @Mock
     private HolidayCalendarService calendarService;
-    
+
     @Mock
     private MetricAssessmentBuilder builder;
 
     @InjectMocks
     private FeedOnTimeArrivalMetricAssessor assessor = new FeedOnTimeArrivalMetricAssessor();
-    
+
     @Before
     public void setUp() throws Exception {
         initMocks(this);
-        
+
         when(this.calendarService.getCalendar(any(String.class))).thenReturn(this.calendar);
         when(this.builder.message(any(String.class))).thenReturn(this.builder);
         when(this.builder.metric(any(Metric.class))).thenReturn(this.builder);
         when(this.builder.result(any(AssessmentResult.class))).thenReturn(this.builder);
-        
+
         CronExpression cron = new CronExpression("0 0 12 1/1 * ? *");  // Noon every day
-        
+
         this.lateTime = new DateTime(CronExpressionUtil.getPreviousFireTime(cron)).plusHours(4);
-        this.metric = new FeedOnTimeArrivalMetric("feed", 
-                                                  cron, 
-                                                  Period.hours(4), 
-                                                  Period.days(2), 
-                                                  "USA");
+        this.metric = new FeedOnTimeArrivalMetric("feed",
+                cron,
+                Period.hours(4),
+                Period.days(2),
+                "USA");
     }
 
     @Test
@@ -68,9 +67,9 @@ public class FeedOnTimeArrivalMetricAssessorTest {
         DateTime feedEnd = this.lateTime.minusMinutes(1);
         when(this.feedRepository.findLastCompletedFeed("feed")).thenReturn(createExecutedFeed(feedEnd));
         when(this.calendar.isTimeIncluded(anyLong())).thenReturn(true);
-        
+
         this.assessor.assess(metric, this.builder);
-        
+
         verify(this.builder).result(AssessmentResult.SUCCESS);
     }
 
@@ -78,7 +77,7 @@ public class FeedOnTimeArrivalMetricAssessorTest {
     public void testNewFeedSuccess() throws ParseException {
         DateTime feedEnd = null;
 
-        CronExpression cron = new CronExpression("0 0 0/1 1/1 * ? *"); //fire every hour on the next hour
+        CronExpression cron = new CronExpression("0 0 0/1 1/1 * ? *"); //fire every hour
 
         this.lateTime = new DateTime(CronExpressionUtil.getPreviousFireTime(cron)).plusHours(4);
         this.metric = new FeedOnTimeArrivalMetric("feed",
@@ -99,7 +98,7 @@ public class FeedOnTimeArrivalMetricAssessorTest {
     public void testNewFeedFailure() throws ParseException {
         DateTime feedEnd = null;
 
-        CronExpression cron = new CronExpression("0 0 0/3 1/1 * ? *"); //fire every hour on the next hour
+        CronExpression cron = new CronExpression("0 0 0/3 1/1 * ? *"); //fire every 3 hours
 
         this.lateTime = new DateTime(CronExpressionUtil.getPreviousFireTime(cron)).plusHours(1);
         this.metric = new FeedOnTimeArrivalMetric("feed",
@@ -115,39 +114,39 @@ public class FeedOnTimeArrivalMetricAssessorTest {
 
         verify(this.builder).result(AssessmentResult.FAILURE);
     }
-    
+
     @Test
     public void testMinuteAfterLate() throws ParseException {
         DateTime feedEnd = this.lateTime.plusMinutes(1);
         when(this.feedRepository.findLastCompletedFeed("feed")).thenReturn(createExecutedFeed(feedEnd));
         when(this.calendar.isTimeIncluded(anyLong())).thenReturn(true);
-        
+
         this.assessor.assess(metric, this.builder);
-        
+
         verify(this.builder).result(AssessmentResult.FAILURE);
     }
-    
+
     @Test
     public void testLateButHoliday() throws ParseException {
         DateTime feedEnd = this.lateTime.plusMinutes(1);
         when(this.feedRepository.findLastCompletedFeed("feed")).thenReturn(createExecutedFeed(feedEnd));
         when(this.calendar.isTimeIncluded(anyLong())).thenReturn(false);
-        
+
         this.assessor.assess(metric, this.builder);
-        
+
         verify(this.builder).result(AssessmentResult.SUCCESS);
     }
-    
+
     @Test
     public void testFeedNotFound() throws ParseException {
         when(this.feedRepository.findLastCompletedFeed("feed")).thenReturn(null);
-        
+
         this.assessor.assess(metric, this.builder);
-        
+
         verify(this.builder).result(AssessmentResult.FAILURE);
     }
 
-    
+
     private ExecutedFeed createExecutedFeed(final DateTime endTime) {
         ExecutedFeed feed = new ExecutedFeed();
         feed.setEndTime(endTime);
