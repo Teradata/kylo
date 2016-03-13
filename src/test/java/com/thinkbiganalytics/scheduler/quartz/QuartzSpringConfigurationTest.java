e60c91ca4f10f23d035e6d5ec58a4e290aeb6cb0
matt-hutton <matt.hutton@thinkbiganalytics.com>
2016-03-12 18:20:09 -0800

    unit tests

--- src/test/java/com/thinkbiganalytics/scheduler/quartz/QuartzSpringConfigurationTest.java
+++ src/test/java/com/thinkbiganalytics/scheduler/quartz/QuartzSpringConfigurationTest.java
@@ -1,15 +1,10 @@
 package com.thinkbiganalytics.scheduler.quartz;
 
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.junit.runner.RunWith;
-
-import javax.inject.Inject;
 
 import static org.junit.Assert.assertNotNull;
 
