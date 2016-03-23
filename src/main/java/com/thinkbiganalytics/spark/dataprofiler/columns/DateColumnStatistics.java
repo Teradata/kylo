69f1c1b1ca9378af973b3b7fb424b85ee0d7ab93
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-03-22 21:12:51 -0700

    fix nullpointer exception on Date/Timestamp profile stats

--- src/main/java/com/thinkbiganalytics/spark/dataprofiler/columns/DateColumnStatistics.java
+++ src/main/java/com/thinkbiganalytics/spark/dataprofiler/columns/DateColumnStatistics.java
@@ -54,13 +54,14 @@ public class DateColumnStatistics extends ColumnStatistics {
 		if (columnValue != null) {
 			columnDateValue = Date.valueOf(String.valueOf(columnValue));
 		}
-		
-		if (maxDate.before(columnDateValue)) {
-			maxDate = columnDateValue;
-		}
-		
-		if (minDate.after(columnDateValue)) {
-			minDate = columnDateValue;
+		if(columnDateValue != null) {
+			if (maxDate.before(columnDateValue)) {
+				maxDate = columnDateValue;
+			}
+
+			if (minDate.after(columnDateValue)) {
+				minDate = columnDateValue;
+			}
 		}
 		
 	}
