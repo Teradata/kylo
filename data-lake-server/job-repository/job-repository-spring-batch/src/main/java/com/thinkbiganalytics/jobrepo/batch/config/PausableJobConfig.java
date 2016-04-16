33aa7543a8b29c2e1f2e12a5519f79e4e930fe08
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-04-15 17:32:03 -0700

    jobrepo spring batch Job Service interaction

---
+++ data-lake-server/job-repository/job-repository-spring-batch/src/main/java/com/thinkbiganalytics/jobrepo/batch/config/PausableJobConfig.java
@@ -0,0 +1,35 @@
+package com.thinkbiganalytics.jobrepo.batch.config;
+
+import com.thinkbiganalytics.jobrepo.batch.scheduling.SchedulerService;
+import com.thinkbiganalytics.jobrepo.batch.scheduling.concurrent.PausableThreadPoolTaskExecutor;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.core.task.TaskExecutor;
+
+/**
+ * Created by sr186054 on 4/14/16.
+ */
+public class PausableJobConfig {
+    public PausableJobConfig() {
+    }
+
+    public static TaskExecutor createPausableThreadPoolExecutor(SchedulerService schedulerService, String beanName, int poolSize, int maxPoolSize, int queueCapacity) {
+        PausableThreadPoolTaskExecutor executor = new PausableThreadPoolTaskExecutor();
+        executor.setSchedulerService(schedulerService);
+        executor.setCorePoolSize(poolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setBeanName(beanName);
+        executor.setThreadNamePrefix(beanName);
+        executor.initialize();
+        return executor;
+    }
+
+    public static JobLauncher createPausableJobLauncher(JobRepository jobRepository, TaskExecutor taskExecutor) {
+        SimpleJobLauncher launcher = new SimpleJobLauncher();
+        launcher.setJobRepository(jobRepository);
+        launcher.setTaskExecutor(taskExecutor);
+        return launcher;
+    }
+}
