3718618df8bfbadb5e9060301d542e7e72dd6b40
Sean Felten <sean.felten@thinkbiganalytics.com>
2016-04-20 20:33:47 -0700

    Dependency fixes

--- data-lake-server/scheduler/scheduler-quartz/src/test/java/com/thinkbiganalytics/scheduler/quartz/QuartzSpringConfigurationTest.java
+++ data-lake-server/scheduler/scheduler-quartz/src/test/java/com/thinkbiganalytics/scheduler/quartz/QuartzSpringConfigurationTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.thinkbiganalytics.scheduler.QuartzSpringConfiguration;
+
 import static org.junit.Assert.assertNotNull;
 
 
