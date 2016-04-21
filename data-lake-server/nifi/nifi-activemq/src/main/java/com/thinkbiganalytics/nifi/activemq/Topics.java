fa31a67bdd762deff5dc946f4bdaf3402c44c25f
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-04-21 10:42:26 -0700

    ported thinkbig-nifi-activemq project to data-lake-server/nifi/nifi-activemq

---
+++ data-lake-server/nifi/nifi-activemq/src/main/java/com/thinkbiganalytics/nifi/activemq/Topics.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2016.
+ */
+
+package com.thinkbiganalytics.nifi.activemq;
+
+/**
+ * Created by sr186054 on 3/3/16.
+ */
+public interface Topics {
+
+   String  THINKBIG_NIFI_EVENT_TOPIC = "thinkbig.nifi.event.topic";
+
+   String NIFI_EVENT_TOPIC_BEAN = "thinkbigNifiEventTopic";
+}
