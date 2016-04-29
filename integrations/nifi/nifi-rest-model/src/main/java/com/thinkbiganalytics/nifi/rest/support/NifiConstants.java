618c50300a3dce1af50f590782709b07fc56221f
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-04-28 23:03:51 -0700

    update Nifi Rest Client Flow Order and use of Reusable Templates

---
+++ integrations/nifi/nifi-rest-model/src/main/java/com/thinkbiganalytics/nifi/rest/support/NifiConstants.java
@@ -0,0 +1,12 @@
+package com.thinkbiganalytics.nifi.rest.support;
+
+/**
+ * Created by sr186054 on 4/28/16.
+ */
+public class NifiConstants {
+
+  public enum NIFI_PORT_TYPE {
+  OUTPUT_PORT,INPUT_PORT
+  }
+
+}
