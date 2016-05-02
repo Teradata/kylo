5d6689d3d8dd63e940359258f2ffa2f55b1f0089
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-05-02 11:19:30 -0700

    fix build

---
+++ services/feed-manager-service/feed-manager-controller/src/main/java/com/thinkbiganalytics/feedmgr/support/Constants.java
@@ -0,0 +1,10 @@
+package com.thinkbiganalytics.feedmgr.support;
+
+/**
+ * Created by sr186054 on 5/1/16.
+ */
+public class Constants {
+
+  public static final String REUSABLE_TEMPLATES_CATEGORY_NAME = "Reusable Templates";
+
+}
