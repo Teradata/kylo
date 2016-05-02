bec7a807e5545bf3149c05157dc151e4d86a4d93
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-05-02 09:12:59 -0700

    fix default values for AvailableFieldPolicies

---
+++ services/feed-manager-service/feed-manager-rest-model/src/main/java/com/thinkbiganalytics/feedmgr/rest/support/SystemNamingService.java
@@ -0,0 +1,25 @@
+package com.thinkbiganalytics.feedmgr.rest.support;
+
+import com.google.common.base.CaseFormat;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created by sr186054 on 4/29/16.
+ */
+public class SystemNamingService {
+
+  public static String generateSystemName(String name){
+    String[] controlChars = {"\"","'","!","@","#","$","%","^","&","*","(",")"};
+    //lower and remove double spaces
+    String  systemName = name.toLowerCase().trim().replaceAll(" +","_");
+    systemName = CaseFormat.LOWER_CAMEL.to(CaseFormat.LOWER_HYPHEN, systemName);
+    systemName = CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_UNDERSCORE, systemName);
+
+    for(String controlChar: controlChars){
+      systemName = StringUtils.remove(systemName, controlChar);
+    }
+    return systemName;
+  }
+
+}
