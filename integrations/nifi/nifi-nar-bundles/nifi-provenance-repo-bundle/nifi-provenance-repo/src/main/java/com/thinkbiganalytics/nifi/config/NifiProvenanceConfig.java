fc86eeba0e5d91bc56e74fdaa7eb6cda3b82b2ff
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-05-04 16:50:15 -0700

    Pulled JMS broker out into a standalone server
    Create Temp database if JMS server is down so we don't lose provenance events in NiFi
    
    Start the H2 database and web console on the fly and shutdown when not in use

--- integrations/nifi/nifi-nar-bundles/nifi-provenance-repo-bundle/nifi-provenance-repo/src/main/java/com/thinkbiganalytics/nifi/config/NifiProvenanceConfig.java
+++ integrations/nifi/nifi-nar-bundles/nifi-provenance-repo-bundle/nifi-provenance-repo/src/main/java/com/thinkbiganalytics/nifi/config/NifiProvenanceConfig.java
@@ -1,10 +1,16 @@
 package com.thinkbiganalytics.nifi.config;
 
 import com.thinkbiganalytics.util.SpringApplicationContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
@@ -15,7 +21,23 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = {"com.thinkbiganalytics"})
 @PropertySource("classpath:config.properties")
 public class NifiProvenanceConfig {
+    @Autowired
+    private Environment env;
 
+    @Bean(name="thinkbigNifiDataSource")
+    public DataSource thinkbigNifiDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("thinkbig.provenance.datasource.driverClassName"));
+        dataSource.setUrl(env.getProperty("thinkbig.provenance.datasource.url"));
+        dataSource.setUsername(env.getProperty("thinkbig.provenance.datasource.username"));
+        dataSource.setPassword(env.getProperty("thinkbig.provenance.datasource.password"));
+        return dataSource;
+    }
+
+    @Bean(name = "jdbcThinkbigNifi")
+    public JdbcTemplate jdbcThinkbigNifiTemplate(@Qualifier("thinkbigNifiDataSource") DataSource thinkbigNifiDataSource) {
+        return new JdbcTemplate(thinkbigNifiDataSource);
+    }
 
     @Bean
     public SpringApplicationContext springApplicationContext() {
