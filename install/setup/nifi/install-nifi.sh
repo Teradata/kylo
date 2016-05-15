03ea202e9195e0de50cea89a2a431b49128c9031
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-05-15 05:59:00 -0700

    switch to use nifi 0.6.1

--- install/setup/nifi/install-nifi.sh
+++ install/setup/nifi/install-nifi.sh
@@ -4,9 +4,9 @@ echo "Creating a new nifi user"
 useradd -m nifi -d /opt/nifi
 cd /opt/nifi
 echo "Download nifi distro and install"
-wget https://archive.apache.org/dist/nifi/0.5.1/nifi-0.5.1-bin.tar.gz
-tar -xvf nifi-0.5.1-bin.tar.gz
-ln -s nifi-0.5.1 current
+wget http://apache.mesi.com.ar/nifi/0.6.1/nifi-0.6.1-bin.tar.gz
+tar -xvf nifi-0.6.1-bin.tar.gz
+ln -s nifi-0.6.1 current
 
 echo "Changing permissions to the nifi user"
 chown -R nifi:users /opt/nifi
