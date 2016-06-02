5327579f512195aa3389560331e398addad1bb2a
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-06-02 10:14:37 -0700

    stored procedures to delete FEED Jobs and metadata for mysql

---
+++ install/setup/sql/mysql/stored_procedures/delete_feed.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE DEFINER=`root`@`%` PROCEDURE `delete_feed`(in category varchar(255), in feed varchar(255))
+BEGIN
+
+CALL delete_feed_jobs(category,feed);
+CALL delete_feed_metadata(category,feed);
+
+END$$
+DELIMITER ;
