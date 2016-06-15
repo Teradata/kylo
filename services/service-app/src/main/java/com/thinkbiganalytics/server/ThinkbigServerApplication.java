bd91594e73003f8e74596b76597d6d51f26ac319
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-06-14 20:07:36 -0700

    PC-179 - Move custom Exception mapper from UI app to service app.
    Move jersey classes to a more specific package

--- services/service-app/src/main/java/com/thinkbiganalytics/server/ThinkbigServerApplication.java
+++ services/service-app/src/main/java/com/thinkbiganalytics/server/ThinkbigServerApplication.java
@@ -1,5 +1,7 @@
 package com.thinkbiganalytics.server;
 
+import com.thinkbiganalytics.rest.SpringJerseyConfiguration;
+
 import org.springframework.beans.factory.config.PropertyOverrideConfigurer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
