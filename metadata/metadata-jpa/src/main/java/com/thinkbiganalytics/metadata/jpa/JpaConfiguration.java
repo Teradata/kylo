c7c690352bc4b0bb6234f4038ecebc8b0fc52947
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-06-14 20:09:47 -0700

    wire CategoryProvider interface instead of the JpaCategoryProvider

--- metadata/metadata-jpa/src/main/java/com/thinkbiganalytics/metadata/jpa/JpaConfiguration.java
+++ metadata/metadata-jpa/src/main/java/com/thinkbiganalytics/metadata/jpa/JpaConfiguration.java
@@ -3,28 +3,31 @@
  */
 package com.thinkbiganalytics.metadata.jpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.sql.DataSource;
-
+import com.thinkbiganalytics.metadata.api.MetadataAccess;
+import com.thinkbiganalytics.metadata.api.category.CategoryProvider;
+import com.thinkbiganalytics.metadata.api.datasource.DatasourceProvider;
+import com.thinkbiganalytics.metadata.api.feed.FeedProvider;
 import com.thinkbiganalytics.metadata.api.feedmgr.category.FeedManagerCategoryProvider;
 import com.thinkbiganalytics.metadata.api.feedmgr.feed.FeedManagerFeedProvider;
 import com.thinkbiganalytics.metadata.api.feedmgr.template.FeedManagerTemplateProvider;
+import com.thinkbiganalytics.metadata.api.op.DataOperationsProvider;
 import com.thinkbiganalytics.metadata.jpa.category.JpaCategoryProvider;
+import com.thinkbiganalytics.metadata.jpa.datasource.JpaDatasourceProvider;
+import com.thinkbiganalytics.metadata.jpa.feed.JpaFeedProvider;
 import com.thinkbiganalytics.metadata.jpa.feedmgr.category.JpaFeedManagerCategoryProvider;
 import com.thinkbiganalytics.metadata.jpa.feedmgr.feed.JpaFeedManagerFeedProvider;
 import com.thinkbiganalytics.metadata.jpa.feedmgr.template.JpaFeedManagerTemplateProvider;
-import org.hibernate.MultiTenancyStrategy;
-import org.hibernate.SessionFactory;
+import com.thinkbiganalytics.metadata.jpa.op.JpaDataOperationsProvider;
+import com.thinkbiganalytics.metadata.jpa.sla.JpaServiceLevelAgreementProvider;
+import com.thinkbiganalytics.metadata.sla.spi.ServiceLevelAgreementProvider;
+
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -32,15 +35,9 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.AbstractPlatformTransactionManager;
 
-import com.thinkbiganalytics.metadata.api.MetadataAccess;
-import com.thinkbiganalytics.metadata.api.datasource.DatasourceProvider;
-import com.thinkbiganalytics.metadata.api.feed.FeedProvider;
-import com.thinkbiganalytics.metadata.api.op.DataOperationsProvider;
-import com.thinkbiganalytics.metadata.jpa.datasource.JpaDatasourceProvider;
-import com.thinkbiganalytics.metadata.jpa.feed.JpaFeedProvider;
-import com.thinkbiganalytics.metadata.jpa.op.JpaDataOperationsProvider;
-import com.thinkbiganalytics.metadata.jpa.sla.JpaServiceLevelAgreementProvider;
-import com.thinkbiganalytics.metadata.sla.spi.ServiceLevelAgreementProvider;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 /**
  *
@@ -143,7 +140,7 @@ public class JpaConfiguration {
     }
 
     @Bean
-    public JpaCategoryProvider jpaCategoryProvider() {
+    public CategoryProvider jpaCategoryProvider() {
         return new JpaCategoryProvider();
     }
 
