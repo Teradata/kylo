18277d437f4067f862af328647ccc26ee0459817
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-06-27 15:24:21 -0700

    PC-259 - modified RPM install and wizard process to support looking at /opt/java/current by default to simplify development

--- install/setup/java/change-nifi-java-home.sh
+++ install/setup/java/change-nifi-java-home.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "You must pass in a JAVA_HOME path"
+    exit 1
+fi
+
 echo "Adding custom Java path to the NiFi startup script"
-sed -i '/\#\!\/bin\/sh/a export JAVA_HOME=\/opt\/java\/current\nexport PATH=\$JAVA_HOME\/bin\:\$PATH' /opt/nifi/current/bin/nifi.sh
+sed -i "/\#\!\/bin\/sh/a export JAVA_HOME=$1\nexport PATH=\$JAVA_HOME\/bin\:\$PATH" /opt/nifi/current/bin/nifi.sh
