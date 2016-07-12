f3c5330e7a4d92d78d027c284b3c74b82022c5ea
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-07-12 12:57:32 -0700

    Modify MySQL setup to work correct with cloudera sandbox

--- install/setup/sql/mysql/setup-mysql.sh
+++ install/setup/sql/mysql/setup-mysql.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-mysql -u $1 --password=$2 < /opt/thinkbig/setup/sql/mysql/thinkbig/create-database.sql
+mysql -u$1 --password=$2 < /opt/thinkbig/setup/sql/mysql/thinkbig/create-database.sql
 echo "Created thinkbig database";
-mysql -u $1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-batch-mysql.sql
-mysql -u $1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-batch-thinkbig-mysql.sql
+mysql -u$1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-batch-mysql.sql
+mysql -u$1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-batch-thinkbig-mysql.sql
 echo "Created Operation Manager Tables";
-mysql -u $1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-batch-thinkbig-indexes.sql
+mysql -u$1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-batch-thinkbig-indexes.sql
 echo "Created Operation Manager Indexes";
 
-mysql -u $1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-metadata-mysql.sql
-mysql -u $1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-metadata-constraints.sql
+mysql -u$1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-metadata-mysql.sql
+mysql -u$1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-metadata-constraints.sql
 echo 'Created Metadata Tables'
-mysql -u $1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-metadata-grants.sql
+mysql -u$1 --password=$2 thinkbig < /opt/thinkbig/setup/sql/mysql/thinkbig/schema-metadata-grants.sql
 echo 'Granted SQL for user nifi'
 
-mysql -u $1 --password=$2 -e 'show databases;'
+mysql -u$1 --password=$2 -e 'show databases;'
