64096be17852ab21d7cfa772f78e6183406022ba
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-08-06 08:44:40 -0700

    PC-320 start of move core/default to plugins

---
+++ plugins/sla-email/src/main/java/com/thinkbiganalytics/metadata/sla/config/EmailServiceLevelAgreementConfiguration.java
@@ -0,0 +1,56 @@
+package com.thinkbiganalytics.metadata.sla.config;
+
+
+import com.thinkbiganalytics.metadata.sla.EmailConfiguration;
+import com.thinkbiganalytics.metadata.sla.EmailServiceLevelAgreementAction;
+import com.thinkbiganalytics.metadata.sla.SlaEmailService;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+/**
+ * Created by sr186054 on 7/22/16.
+ */
+@Configuration
+public class EmailServiceLevelAgreementConfiguration {
+
+
+    @Bean
+    public EmailServiceLevelAgreementAction emailServiceLevelAgreementAction() {
+        return new EmailServiceLevelAgreementAction();
+    }
+
+    @Bean(name = "slaEmailConfiguration")
+    @ConfigurationProperties(prefix = "sla.mail")
+    public EmailConfiguration emailConfiguration() {
+        return new EmailConfiguration();
+    }
+
+    @Bean(name = "slaEmailSender")
+    public JavaMailSender javaMailSender(@Qualifier("slaEmailConfiguration") EmailConfiguration emailConfiguration) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        Properties mailProperties = new Properties();
+        mailProperties.put("mail.smtp.auth", emailConfiguration.isSmtpAuth());
+        mailProperties.put("mail.smtp.starttls.enable", emailConfiguration.isStarttls());
+        mailSender.setJavaMailProperties(mailProperties);
+        mailSender.setHost(emailConfiguration.getHost());
+        mailSender.setPort(emailConfiguration.getPort());
+        mailSender.setProtocol(emailConfiguration.getProtocol());
+        mailSender.setUsername(emailConfiguration.getUsername());
+        mailSender.setPassword(emailConfiguration.getPassword());
+        return mailSender;
+    }
+
+    @Bean
+    public SlaEmailService slaEmailService() {
+        return new SlaEmailService();
+    }
+
+
+}
