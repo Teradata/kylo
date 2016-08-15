87ae9e5494a47e40c3450697235e487b385de526
Gregory Hart <greg.hart@thinkbiganalytics.com>
2016-08-15 16:34:48 -0700

    Moved delete_feed_jobs stored procedure to 0.3.0 update script.

--- install/setup/sql/mysql/thinkbig/0.3.0/update.sh
+++ install/setup/sql/mysql/thinkbig/0.3.0/update.sh
@@ -2,4 +2,5 @@
 
 MY_DIR=$(dirname $0)
 mysql -u$1 --password=$2 < $MY_DIR/alter_tables.sql
+mysql -u$1 --password=$2 < $MY_DIR/../0.2.0/stored_procedures/delete_feed_jobs.sql
 echo "Updated to 0.3.0 release";
