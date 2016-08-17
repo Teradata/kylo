10939b27af3b29443eb2c2c07c4b1e32fbe0dff4
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-08-16 23:21:28 -0700

    add Aggregrated Stats Model

---
+++ integrations/nifi/nifi-nar-bundles/nifi-provenance-repo-bundle/nifi-provenance-repo/src/main/java/com/thinkbiganalytics/nifi/provenance/model/stats/GroupedStats.java
@@ -0,0 +1,57 @@
+package com.thinkbiganalytics.nifi.provenance.model.stats;
+
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sr186054 on 8/16/16.
+ */
+public class GroupedStats extends BaseStatistics {
+
+    private DateTime groupKey;
+    private DateTime minTime;
+    private DateTime maxTime;
+
+    private List<ProvenanceEventStats> eventStatsList;
+
+    public GroupedStats(DateTime groupKey, List<ProvenanceEventStats> eventStats) {
+        this.groupKey = groupKey;
+        this.eventStatsList = new ArrayList<>(eventStats);
+
+        this.totalCount = new Long(eventStats.size());
+
+        eventStatsList.stream().forEach(stats -> {
+            this.bytesIn += stats.getBytesIn();
+            this.bytesOut += stats.getBytesOut();
+            this.duration += stats.getDuration();
+            if (this.time == null) {
+                this.time = stats.getTime();
+            }
+            if (this.minTime == null) {
+                this.minTime = stats.getTime();
+            }
+
+            if (this.maxTime == null) {
+                this.maxTime = stats.getTime();
+            }
+            this.maxTime = (stats.getTime()).isBefore(this.maxTime) ? stats.getTime() : this.maxTime;
+            this.minTime = (stats.getTime()).isBefore(this.minTime) ? stats.getTime() : this.minTime;
+            this.time = this.minTime;
+        });
+    }
+
+    public DateTime getMinTime() {
+        return minTime;
+    }
+
+    public DateTime getMaxTime() {
+        return maxTime;
+    }
+
+    public DateTime getGroupKey() {
+        return groupKey;
+    }
+
+}
