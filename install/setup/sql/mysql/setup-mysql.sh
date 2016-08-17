28456f124d5e04a1bb0df1a7d8e74e94b14ae02a
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-08-17 16:38:34 -0700

    Revert MySQL setup script

--- install/setup/sql/mysql/setup-mysql.sh
+++ install/setup/sql/mysql/setup-mysql.sh
@@ -3,9 +3,9 @@
 MYSQL_DIR=$(dirname $0)
 echo "Executing the master script to install all database scripts"
 
-$MYSQL_DIR/thinkbig/0.2.0/setup-mysql.sh '$1' '$2'
-$MYSQL_DIR/thinkbig/0.3.0/update.sh '$1' '$2'
-$MYSQL_DIR/thinkbig/0.3.1/update.sh '$1' '$2'
+$MYSQL_DIR/thinkbig/0.2.0/setup-mysql.sh $1 $2
+$MYSQL_DIR/thinkbig/0.3.0/update.sh $1 $2
+$MYSQL_DIR/thinkbig/0.3.1/update.sh $1 $2
 
 mysql -u$1 --password=$2 -e 'show databases;'
 
