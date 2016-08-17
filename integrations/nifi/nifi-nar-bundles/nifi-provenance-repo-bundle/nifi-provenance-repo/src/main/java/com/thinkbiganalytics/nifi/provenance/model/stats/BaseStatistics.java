e935d7b4a246fbd3b68a35d58696e19c8524d5cd
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-08-17 06:40:51 -0700

    add some additional stats

--- integrations/nifi/nifi-nar-bundles/nifi-provenance-repo-bundle/nifi-provenance-repo/src/main/java/com/thinkbiganalytics/nifi/provenance/model/stats/BaseStatistics.java
+++ integrations/nifi/nifi-nar-bundles/nifi-provenance-repo-bundle/nifi-provenance-repo/src/main/java/com/thinkbiganalytics/nifi/provenance/model/stats/BaseStatistics.java
@@ -12,6 +12,11 @@ public class BaseStatistics {
     protected Long bytesOut = 0L;
     protected Long duration = 0L;
     protected Long totalCount = 1L;
+    protected Long jobsStarted = 0L;
+    protected Long jobsFinished = 0L;
+    protected Long processorsFailed = 0L;
+    protected Long flowFilesStarted = 0L;
+    protected Long flowFilesFinished = 0L;
 
     public DateTime getTime() {
         return time;
@@ -25,36 +30,44 @@ public class BaseStatistics {
         return bytesIn;
     }
 
-    public void setBytesIn(Long bytesIn) {
-        this.bytesIn = bytesIn;
-    }
-
     public Long getBytesOut() {
         return bytesOut;
-    }
-
-    public void setBytesOut(Long bytesOut) {
-        this.bytesOut = bytesOut;
     }
 
     public Long getDuration() {
         return duration;
     }
 
-    public void setDuration(Long duration) {
-        this.duration = duration;
-    }
-
     public Long getTotalCount() {
         return totalCount;
     }
 
-    public void setTotalCount(Long totalCount) {
-        this.totalCount = totalCount;
+    public Long getJobsStarted() {
+        return jobsStarted;
     }
 
+    public Long getJobsFinished() {
+        return jobsFinished;
+    }
+
+    public Long getProcessorsFailed() {
+        return processorsFailed;
+    }
+
+    public Long getFlowFilesStarted() {
+        return flowFilesStarted;
+    }
+
+    public Long getFlowFilesFinished() {
+        return flowFilesFinished;
+    }
+
+
+    public Double average(Long stat) {
+        return stat.doubleValue() / totalCount.doubleValue();
+    }
     public Double getAvgDuration() {
-        return duration.doubleValue() / totalCount;
+        return average(duration);
     }
 
 
