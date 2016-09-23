5e5da783f71b1916e56cc6044ab3662cd9bb5f0b
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-09-23 06:18:07 -0700

    Added 0.4 sql scripts to the master installation script

--- install/setup/sql/mysql/setup-mysql.sh
+++ install/setup/sql/mysql/setup-mysql.sh
@@ -6,6 +6,7 @@ echo "Executing the master script to install all database scripts"
 $MYSQL_DIR/thinkbig/0.2.0/setup-mysql.sh $1 $2
 $MYSQL_DIR/thinkbig/0.3.0/update.sh $1 $2
 $MYSQL_DIR/thinkbig/0.3.1/update.sh $1 $2
+$MYSQL_DIR/thinkbig/0.4.0/update.sh $1 $2
 
 mysql -u$1 --password=$2 -e 'show databases;'
 
