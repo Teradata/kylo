b9eaa5f516c3ea2e77721280f757cd597191e22c
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-09-27 20:22:29 -0700

    Removed Ranger groups from NiFi template and UI for 0.4.0 release

--- install/setup/nifi/create-symbolic-links.sh
+++ install/setup/nifi/create-symbolic-links.sh
@@ -8,11 +8,6 @@ ln -f -s /opt/nifi/data/lib/thinkbig-nifi-hadoop-service-nar-*.nar /opt/nifi/cur
 ln -f -s /opt/nifi/data/lib/thinkbig-nifi-provenance-repo-nar-*.nar /opt/nifi/current/lib/thinkbig-nifi-provenance-repo-nar.nar
 ln -f -s /opt/nifi/data/lib/thinkbig-nifi-standard-services-nar-*.nar /opt/nifi/current/lib/thinkbig-nifi-standard-services-nar.nar
 
-ln -f -s /opt/nifi/data/lib/thinkbig-nifi-sentry-authorization-nar-*.nar /opt/nifi/current/lib/thinkbig-nifi-sentry-authorization-nar.nar
-ln -f -s /opt/nifi/data/lib/thinkbig-nifi-ranger-authorization-service-nar-*.nar /opt/nifi/current/lib/thinkbig-nifi-ranger-authorization-service-nar.nar
-ln -f -s /opt/nifi/data/lib/thinkbig-nifi-ranger-authorization-nar-*.nar /opt/nifi/current/lib/thinkbig-nifi-ranger-authorization-nar-0.4.0-SNAPSHOT.nar
-ln -f -s /opt/nifi/data/lib/thinkbig-nifi-ranger-authorization-service-api-nar-*.nar /opt/nifi/current/lib/thinkbig-nifi-ranger-authorization-service-api-nar.nar
-
 ln -f -s /opt/nifi/data/lib/app/thinkbig-spark-interpreter-*-jar-with-dependencies.jar /opt/nifi/current/lib/app/thinkbig-spark-interpreter-jar-with-dependencies.jar
 ln -f -s /opt/nifi/data/lib/app/thinkbig-spark-validate-cleanse-*-jar-with-dependencies.jar /opt/nifi/current/lib/app/thinkbig-spark-validate-cleanse-jar-with-dependencies.jar
 ln -f -s /opt/nifi/data/lib/app/thinkbig-spark-job-profiler-*-jar-with-dependencies.jar /opt/nifi/current/lib/app/thinkbig-spark-job-profiler-jar-with-dependencies.jar
