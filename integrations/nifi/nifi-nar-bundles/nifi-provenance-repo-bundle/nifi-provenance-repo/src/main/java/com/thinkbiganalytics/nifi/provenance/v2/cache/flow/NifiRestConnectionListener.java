735f2c4350b375431c42795f77e410b44ad67902
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-09-29 01:19:21 -0700

    PC-584 provenance event capture issues,  cleanup logs

---
+++ integrations/nifi/nifi-nar-bundles/nifi-provenance-repo-bundle/nifi-provenance-repo/src/main/java/com/thinkbiganalytics/nifi/provenance/v2/cache/flow/NifiRestConnectionListener.java
@@ -0,0 +1,10 @@
+package com.thinkbiganalytics.nifi.provenance.v2.cache.flow;
+
+/**
+ * Created by sr186054 on 9/28/16.
+ */
+public interface NifiRestConnectionListener {
+
+    public void onConnected();
+
+}
