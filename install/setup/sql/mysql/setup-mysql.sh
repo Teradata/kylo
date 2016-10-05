eaf3d3d8b6c2a3eeb6d0c0345b735673eb221cb5
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2016-10-05 16:26:39 +0100

    PC-622 Prompt for database hostname

--- install/setup/sql/mysql/setup-mysql.sh
+++ install/setup/sql/mysql/setup-mysql.sh
@@ -3,11 +3,11 @@
 MYSQL_DIR=$(dirname $0)
 echo "Executing the master script to install all database scripts"
 
-$MYSQL_DIR/thinkbig/0.2.0/setup-mysql.sh $1 $2
-$MYSQL_DIR/thinkbig/0.3.0/update.sh $1 $2
-$MYSQL_DIR/thinkbig/0.3.1/update.sh $1 $2
-$MYSQL_DIR/thinkbig/0.4.0/update.sh $1 $2
+$MYSQL_DIR/thinkbig/0.2.0/setup-mysql.sh $1 $2 $3
+$MYSQL_DIR/thinkbig/0.3.0/update.sh $1 $2 $3
+$MYSQL_DIR/thinkbig/0.3.1/update.sh $1 $2 $3
+$MYSQL_DIR/thinkbig/0.4.0/update.sh $1 $2 $3
 
-mysql -u$1 --password=$2 -e 'show databases;'
+mysql -h $1 -u$2 --password=$3 -e 'show databases;'
 
 echo "Database installation complete"
--- install/setup/sql/mysql/thinkbig/0.2.0/stored_procedures/delete-feed.sh
+++ install/setup/sql/mysql/thinkbig/0.2.0/stored_procedures/delete-feed.sh
@@ -1,1 +1,2 @@
-mysql -u $1 --password=$2 -e "CALL thinkbig.delete_feed('$3','$4');"
+#!/usr/bin/env bash
+mysql -h $1 -u $2 --password=$3  -e "CALL thinkbig.delete_feed('$3','$4');"
