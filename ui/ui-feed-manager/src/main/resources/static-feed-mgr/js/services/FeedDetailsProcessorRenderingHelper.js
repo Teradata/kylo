e562933b33409dd7dd3a42941c1866b4ae1607a6
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-10-19 16:55:05 -0700

    update render helper to look for sqoop processor

--- ui/ui-feed-manager/src/main/resources/static-feed-mgr/js/services/FeedDetailsProcessorRenderingHelper.js
+++ ui/ui-feed-manager/src/main/resources/static-feed-mgr/js/services/FeedDetailsProcessorRenderingHelper.js
@@ -7,6 +7,11 @@ angular.module(MODULE_FEED_MGR).factory('FeedDetailsProcessorRenderingHelper', f
         GET_TABLE_DATA_PROCESSOR_TYPE: "com.thinkbiganalytics.nifi.GetTableData",
         GET_TABLE_DATA_PROCESSOR_TYPE2: "com.thinkbiganalytics.nifi.v2.ingest.GetTableData",
         WATERMARK_PROCESSOR: 'com.thinkbiganalytics.nifi.v2.core.watermark.LoadHighWaterMarkProcessor',
+        SQOOP_PROCESSOR: 'com.thinkbiganalytics.nifi.v2.sqoop.core.ImportSqoop',
+
+        isSqoopProcessor: function (processor) {
+            return data.SQOOP_PROCESSOR == processor.type;
+        },
 
         isGetTableDataProcessor: function (processor) {
             return data.GET_TABLE_DATA_PROCESSOR_TYPE == processor.type || data.GET_TABLE_DATA_PROCESSOR_TYPE2 == processor.type;
@@ -23,6 +28,15 @@ angular.module(MODULE_FEED_MGR).factory('FeedDetailsProcessorRenderingHelper', f
 
             return render;
         },
+        isRenderSqoopProcessor: function (inputProcessor) {
+            var render = false;
+            //if the processor to check is GetTable Data it should be rendered only if it is the input, or if the input is watermark
+            if (data.isSqoopProcessor(inputProcessor) || data.isWatermarkProcessor(inputProcessor)) {
+                render = true;
+            }
+
+            return render;
+        },
         updateGetTableDataRendering: function (inputProcessor, nonInputProcessors) {
             var renderGetData = data.isRenderProcessorGetTableDataProcessor(inputProcessor);
             var getTableDataProcessors = _.filter(nonInputProcessors, function (processor) {
@@ -32,7 +46,18 @@ angular.module(MODULE_FEED_MGR).factory('FeedDetailsProcessorRenderingHelper', f
                 processor.userEditable = renderGetData;
             });
             return renderGetData;
+        },
+        updateSqoopProcessorRendering: function (inputProcessor, nonInputProcessors) {
+            var render = data.isRenderSqoopProcessor(inputProcessor);
+            var sqoopProcessors = _.filter(nonInputProcessors, function (processor) {
+                return data.isRenderSqoopProcessor(processor);
+            })
+            _.each(sqoopProcessors, function (processor) {
+                processor.userEditable = render;
+            });
+            return render;
         }
+
     };
     return data;
 
