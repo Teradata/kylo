a7a329d94678f5c23c2e22998152dbe0926d0b6e
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-10-20 21:38:06 -0700

    remove echo when trying to fix git

--- install/bin/encryptSqoopPassword.sh
+++ install/bin/encryptSqoopPassword.sh
@@ -7,6 +7,4 @@ read -p "> " -s passPhrase;
 echo "Please enter the location to write the file to" ;
 read -p "> " fileLocation  
 
-/opt/java/jdk1.8.0_92/bin/java -cp /opt/thinkbig/lib/thinkbig-nifi-hadoop-processors-*.jar com.thinkbiganalytics.nifi.v2.sqoop.security.EncryptPassword $password $passPhrase $fileLocation
-
-echo "fixing git"
+/opt/java/current/bin/java -cp /opt/thinkbig/lib/thinkbig-nifi-hadoop-processors-*.jar com.thinkbiganalytics.nifi.v2.sqoop.security.EncryptPassword $password $passPhrase $fileLocation
