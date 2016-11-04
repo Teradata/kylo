76c88cc62bf0bcfcbac01eae93b883b6f0bba9fc
Gregory Hart <greg.hart@thinkbiganalytics.com>
2016-11-04 13:34:52 -0700

    Fixed offline install of JCE 8.

--- install/setup/generate-offline-install.sh
+++ install/setup/generate-offline-install.sh
@@ -8,6 +8,8 @@ wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/
 
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz -P ./java
 
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip -P ./java
+
 wget https://archive.apache.org/dist/nifi/1.0.0/nifi-1.0.0-bin.tar.gz -P ./nifi
 
 cp /opt/thinkbig/thinkbig-services/lib/mysql-connector-java-*.jar ./nifi
--- install/setup/java/install-java8.sh
+++ install/setup/java/install-java8.sh
@@ -6,6 +6,7 @@ MY_DIR=$(dirname $0)
 
 offline=false
 working_dir=$2
+KYLO_INSTALL_HOME=/opt/thinkbig
 
 if [ $# > 1 ]
 then
@@ -14,6 +15,13 @@ then
         echo "Working in offline mode"
         offline=true
     fi
+fi
+
+if [ ${offline} = true ]
+then
+    JAVA_SETUP_DIR=${working_dir}/java
+else
+    JAVA_SETUP_DIR=${KYLO_INSTALL_HOME}/setup/java
 fi
 
 echo "Installing Java 8 in /opt/java"
@@ -39,5 +47,9 @@ rm -f jdk-8u92-linux-x64.tar.gz
 echo "Creating symbolic link called 'current' to simplify upgrades"
 ln -s jdk1.8.0_92 current
 
-[ $offline = true ] && offline_flag="-o"
-$MY_DIR/install-java-crypt-ext.sh /opt/java/current $offline_flag
+if [ ${offline} = true ]
+then
+    ${JAVA_SETUP_DIR}/install-java-crypt-ext.sh /opt/java/current -O ${working_dir}
+else
+    ${JAVA_SETUP_DIR}/install-java-crypt-ext.sh /opt/java/current
+fi
