42f21572b2b1a099c5a3e9068c5c0e2821251491
Gregory Hart <greg.hart@thinkbiganalytics.com>
2016-11-22 09:17:08 -0800

    Removed old src directories for Spark apps.

--- services/spark-shell-service/src/test/resources/com/thinkbiganalytics/spark/service/transform-service-script1.scala
+++
@@ -1,4 +0,0 @@
-class Transform (destination: String, sendResults: Boolean, sqlContext: org.apache.spark.sql.SQLContext) extends com.thinkbiganalytics.spark.metadata.TransformScript(destination, sendResults, sqlContext) {
-override def dataFrame: org.apache.spark.sql.DataFrame = {sqlContext.range(1,10)}
-}
-new Transform(tableName, true, sqlContext).run()
