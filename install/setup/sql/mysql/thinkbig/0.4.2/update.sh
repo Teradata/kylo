220847eb64de7ef25f890c206e30a4d57af3bfe2
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2016-12-06 14:36:28 -0800

    host flag was missing from one of the scripts

--- install/setup/sql/mysql/thinkbig/0.4.2/update.sh
+++ install/setup/sql/mysql/thinkbig/0.4.2/update.sh
@@ -1,7 +1,8 @@
+use thinkbig;
 #!/bin/bash
 
 MY_DIR=$(dirname $0)
-mysql -u$2 --password=$3 --database=thinkbig -B -N -e "SHOW TABLES" | awk '{print "SET foreign_key_checks = 0; ALTER TABLE", $1, "CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci; SET foreign_key_checks = 1; "}' | mysql --database=thinkbig -u$2 --password=$3
+mysql -h$1 -u$2 --password=$3 --database=thinkbig -B -N -e "SHOW TABLES" | awk '{print "SET foreign_key_checks = 0; ALTER TABLE", $1, "CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci; SET foreign_key_checks = 1; "}' | mysql --database=thinkbig -h$1 -u$2 --password=$3
 mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/schema-0.4.2-upgrade.sql
 echo "Updated to 0.4.2 release";
 
