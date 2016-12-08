1c56dd3e56f5ecdff0071a21f38319b26257198e
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2016-12-07 17:32:16 -0800

    change BATCH_JOB_EXECUTION table to store dates as millis instead of datetime

--- install/setup/sql/mysql/thinkbig/0.5.0/update.sh
+++ install/setup/sql/mysql/thinkbig/0.5.0/update.sh
@@ -4,5 +4,6 @@ MY_DIR=$(dirname $0)
 mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/schema-0.5.0-upgrade.sql
 mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/check_data_job_relationships.sql
 mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/alter_datetime_columns.sql
+mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/create_db_views.sql
 echo "Updated to 0.5.0 release";
 
