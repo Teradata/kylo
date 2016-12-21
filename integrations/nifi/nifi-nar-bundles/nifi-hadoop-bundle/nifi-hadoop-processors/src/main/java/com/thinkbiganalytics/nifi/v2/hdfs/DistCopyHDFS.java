5cc5f19401dfe85f9a67f6a9ffb182d11604a07e
Piotr Skrydalewicz <piotr.skrydalewicz@thinkbiganalytics.com>
2016-12-21 22:15:28 +0100

    [PC-1006] Created DistCopyHDFS processor using DistCp MR job to perform distributed copy.

---
+++ integrations/nifi/nifi-nar-bundles/nifi-hadoop-bundle/nifi-hadoop-processors/src/main/java/com/thinkbiganalytics/nifi/v2/hdfs/DistCopyHDFS.java
@@ -0,0 +1,168 @@
+package com.thinkbiganalytics.nifi.v2.hdfs;
+
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.tools.DistCp;
+import org.apache.hadoop.tools.DistCpOptions;
+import org.apache.nifi.annotation.behavior.EventDriven;
+import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
+import org.apache.nifi.components.PropertyDescriptor;
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.Relationship;
+import org.apache.nifi.processor.exception.ProcessException;
+import org.apache.nifi.processor.util.StandardValidators;
+
+import javax.annotation.Nonnull;
+import java.util.*;
+
+@CapabilityDescription("Copies files from one HDFS location into another using DistCp MR job")
+@EventDriven
+@Tags({"hadoop", "HDFS", "filesystem", "thinkbig", "copy", "distributed copy", "distcp"})
+public class DistCopyHDFS extends AbstractHadoopProcessor {
+
+    /** Relationship for failure */
+    public static final Relationship REL_FAILURE = new Relationship.Builder()
+            .name("failure")
+            .description("At least one of the provided files not found")
+            .build();
+
+    /** Relationship for success */
+    public static final Relationship REL_SUCCESS = new Relationship.Builder()
+            .name("success")
+            .description("Files copied to new location")
+            .build();
+
+    public static final PropertyDescriptor SOURCE = new PropertyDescriptor.Builder()
+            .name("source.path")
+            .description("Absolute source path, if provided along with 'files' parameter will be treated as absolute " +
+                    "path for relative paths provided in that parameter")
+            .required(false)
+            .addValidator(StandardValidators.ATTRIBUTE_EXPRESSION_LANGUAGE_VALIDATOR)
+            .expressionLanguageSupported(true)
+            .build();
+
+    public static final PropertyDescriptor DESTINATION = new PropertyDescriptor.Builder()
+            .name("destination.path")
+            .description("Absolute destination path")
+            .required(true)
+            .addValidator(StandardValidators.ATTRIBUTE_EXPRESSION_LANGUAGE_VALIDATOR)
+            .expressionLanguageSupported(true)
+            .build();
+
+    public static final PropertyDescriptor FILES = new PropertyDescriptor.Builder()
+            .name("files")
+            .description("JSON-encoded list of files, given like: " +
+                    "[{\n" +
+                    "   \"name\": \"example\",\n" +
+                    " }\n" +
+                    "]")
+            .required(false)
+            .addValidator(StandardValidators.ATTRIBUTE_EXPRESSION_LANGUAGE_VALIDATOR)
+            .expressionLanguageSupported(true)
+            .build();
+
+    /** Output paths to other NiFi processors */
+    private static final Set<Relationship> relationships = ImmutableSet.of(REL_FAILURE, REL_SUCCESS);
+
+    @Override
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return ImmutableList.<PropertyDescriptor>builder().addAll(super.getSupportedPropertyDescriptors()).
+                add(DESTINATION).add(SOURCE).add(FILES).build();
+    }
+
+    @Override
+    public Set<Relationship> getRelationships() {
+        return relationships;
+    }
+
+    @Override
+    public void onTrigger(@Nonnull final ProcessContext context, @Nonnull final ProcessSession session) throws ProcessException {
+        FlowFile flowFile = session.get();
+        if ( flowFile == null ) {
+            return;
+        }
+        final FileSystem fs = getFileSystem(context);
+        if (fs == null) {
+            getLog().error("Couldn't initialize HDFS");
+            session.transfer(flowFile, REL_FAILURE);
+            return;
+        }
+        String filesJSON = context.getProperty(FILES).evaluateAttributeExpressions(flowFile).getValue();
+        String source = context.getProperty(SOURCE).evaluateAttributeExpressions(flowFile).getValue();
+        String destination = context.getProperty(DESTINATION).evaluateAttributeExpressions(flowFile).getValue();
+        Gson jsonParser = new Gson();
+        File[] filesList;
+        ArrayList<Path> pathsList = new ArrayList<>();
+        try {
+            if (!(filesJSON == null) && !filesJSON.isEmpty()) {
+                filesList = jsonParser.fromJson(filesJSON, File[].class);
+                if (filesList == null) {
+                    filesList = new File[0];
+                }
+                if (source != null && !source.isEmpty()) {
+                    for (File f : filesList) {
+                        pathsList.add(new Path(source, f.getName()));
+                    }
+                } else {
+                    for (File f : filesList) {
+                        pathsList.add(new Path(f.getName()));
+                    }
+                }
+            }
+            else {
+                if (source == null || source.isEmpty()) {
+                    getLog().error("At least one of attributes: %s or %s needs to be set");
+
+                    session.transfer(flowFile, REL_FAILURE);
+                    return;
+                }
+                pathsList.add(new Path(source));
+            }
+            DistCp distCp = getDistCp(pathsList, new Path(destination));
+            Job job = distCp.execute();
+            job.waitForCompletion(false);
+        } catch (JsonSyntaxException e) {
+            getLog().error("Files list attribute does not contain a proper JSON array");
+            session.transfer(flowFile, REL_FAILURE);
+            return;
+        } catch (Exception e) {
+            getLog().error("Exception during processor execution: " + e.getMessage());
+            session.transfer(flowFile, REL_FAILURE);
+            return;
+        }
+        session.transfer(flowFile, REL_SUCCESS);
+    }
+
+    protected DistCp getDistCp(List<Path> pathsList, Path destination) throws Exception {
+        final Configuration conf = getConfiguration();
+        DistCpOptions opts = new DistCpOptions(pathsList, destination);
+        return new DistCp(conf, opts);
+    }
+
+    class File {
+        private String name;
+
+        public File(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+}
---
+++ integrations/nifi/nifi-nar-bundles/nifi-hadoop-bundle/nifi-hadoop-processors/src/test/java/com/thinkbiganalytics/nifi/v2/hdfs/DistCopyHDFSTest.java
@@ -0,0 +1,231 @@
+package com.thinkbiganalytics.nifi.v2.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.tools.DistCp;
+import org.apache.nifi.components.ValidationResult;
+import org.apache.nifi.processor.ProcessContext;
+import org.apache.nifi.util.MockProcessContext;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class DistCopyHDFSTest {
+    /** Mock file system and configuration */
+    private final FileSystem fileSystem = Mockito.mock(FileSystem.class);
+    private final Configuration configuration = Mockito.mock(Configuration.class);
+    private final DistCp distCp = Mockito.mock(DistCp.class);
+    private final Job job = Mockito.mock(Job.class);
+
+    /** Test runner */
+    private final TestRunner runner = TestRunners.newTestRunner(new TestableDistCopyHDFS());
+
+    private final String fileEntry = "{\n" +
+            "\"name\": \"%s\"\n" +
+            "}";
+
+    /** Initialize instance variables */
+    @Before
+    public void setUp() throws Exception {
+        // Setup test runner
+        runner.setValidateExpressionUsage(false);
+
+        Mockito.when(distCp.execute()).thenReturn(job);
+    }
+
+    /** Verify required properties. */
+    @Test
+    public void testValidators() {
+        String destination = DistCopyHDFS.DESTINATION.getName();
+
+        // Test with no properties
+        Collection<ValidationResult> results = validate(runner);
+        Assert.assertEquals(1, results.size());
+        results.forEach((ValidationResult result) -> Assert.assertEquals(
+                String.format("'%s' is invalid because %s is required", destination, destination), result.toString()));
+
+        // Test with required properties present
+        runner.setProperty(DistCopyHDFS.DESTINATION, "/dropzone");
+        results = validate(runner);
+        Assert.assertEquals(0, results.size());
+
+        // Test with additional property SOURCE set
+        runner.setProperty(DistCopyHDFS.SOURCE, "/var");
+        results = validate(runner);
+        Assert.assertEquals(0, results.size());
+    }
+
+    @Test
+    public void testNoFilesNorSource() {
+
+        runner.setProperty(DistCopyHDFS.DESTINATION, "/dropzone");
+        runner.enqueue(new byte[0]);
+        runner.run();
+
+        // Check relationships
+        Assert.assertEquals(1, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_FAILURE).size());
+        Assert.assertEquals(0, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_SUCCESS).size());
+
+        // Check distCp call
+        InOrder inOrder = Mockito.inOrder(distCp);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    @Test
+    public void testSourceSet() throws Exception {
+        Path source = new Path("/var");
+        Path destination = new Path("/dropzone");
+
+        runner.setProperty(DistCopyHDFS.DESTINATION, destination.toString());
+        runner.setProperty(DistCopyHDFS.SOURCE, source.toString());
+        runner.enqueue(new byte[0]);
+        runner.run();
+
+        // Check relationships
+        Assert.assertEquals(0, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_FAILURE).size());
+        Assert.assertEquals(1, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_SUCCESS).size());
+
+        // Check distCp call
+        InOrder inOrder = Mockito.inOrder(distCp);
+        inOrder.verify(distCp).execute();
+        inOrder.verifyNoMoreInteractions();
+
+        // Check final paths provided to DistCp
+        TestableDistCopyHDFS proc = (TestableDistCopyHDFS) runner.getProcessor();
+        Assert.assertEquals(proc.TEST_destination, destination);
+        Assert.assertEquals(proc.TEST_pathsList, new ArrayList<>(Arrays.asList(source)));
+    }
+
+    @Test
+    public void testSourceAndFilesSet() throws Exception {
+        String source = "/var";
+        Path destination = new Path("/dropzone");
+        String file1 = "001";
+        String file2 = "002";
+        String files = String.format("[" + fileEntry + "," + fileEntry + "]", file1, file2);
+
+        runner.setProperty(DistCopyHDFS.DESTINATION, destination.toString());
+        runner.setProperty(DistCopyHDFS.SOURCE, source);
+        runner.setProperty(DistCopyHDFS.FILES, files);
+        runner.enqueue(new byte[0]);
+        runner.run();
+
+        // Check relationships
+        Assert.assertEquals(0, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_FAILURE).size());
+        Assert.assertEquals(1, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_SUCCESS).size());
+
+        // Check distCp call
+        InOrder inOrder = Mockito.inOrder(distCp);
+        inOrder.verify(distCp).execute();
+        inOrder.verifyNoMoreInteractions();
+
+        // Check final paths provided to DistCp
+        TestableDistCopyHDFS proc = (TestableDistCopyHDFS) runner.getProcessor();
+        Assert.assertEquals(proc.TEST_destination, destination);
+        Assert.assertEquals(proc.TEST_pathsList, new ArrayList<>(Arrays.asList(
+                new Path(source, file1), new Path(source, file2))));
+    }
+
+    @Test
+    public void testFilesSet() throws Exception {
+        Path destination = new Path("/dropzone");
+        String file1 = "/001";
+        String file2 = "/002";
+        String files = String.format("[" + fileEntry + "," + fileEntry + "]", file1, file2);
+
+        runner.setProperty(DistCopyHDFS.DESTINATION, destination.toString());
+        runner.setProperty(DistCopyHDFS.FILES, files);
+        runner.enqueue(new byte[0]);
+        runner.run();
+
+        // Check relationships
+        Assert.assertEquals(0, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_FAILURE).size());
+        Assert.assertEquals(1, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_SUCCESS).size());
+
+        // Check distCp call
+        InOrder inOrder = Mockito.inOrder(distCp);
+        inOrder.verify(distCp).execute();
+        inOrder.verifyNoMoreInteractions();
+
+        // Check final paths provided to DistCp
+        TestableDistCopyHDFS proc = (TestableDistCopyHDFS) runner.getProcessor();
+        Assert.assertEquals(proc.TEST_destination, destination);
+        Assert.assertEquals(proc.TEST_pathsList, new ArrayList<>(Arrays.asList(
+                new Path(file1), new Path(file2))));
+    }
+
+    @Test
+    public void testFilesListAttributeNotJSON() {
+
+        runner.setProperty(DistCopyHDFS.DESTINATION, "/dropzone");
+        runner.setProperty(DistCopyHDFS.FILES, "a");
+        runner.enqueue(new byte[0]);
+        runner.run();
+
+        // Check relationships
+        Assert.assertEquals(1, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_FAILURE).size());
+        Assert.assertEquals(0, runner.getFlowFilesForRelationship(DistCopyHDFS.REL_SUCCESS).size());
+
+        // Check file system calls
+        InOrder inOrder = Mockito.inOrder(fileSystem);
+        inOrder.verifyNoMoreInteractions();
+    }
+
+    /**
+     * Enqueues a {@code FlowFile} and validates its properties.
+     *
+     * @param runner the test runner
+     * @return the validation results
+     */
+    @Nonnull
+    private Collection<ValidationResult> validate(@Nonnull final TestRunner runner) {
+        runner.enqueue(new byte[0]);
+        return ((MockProcessContext)runner.getProcessContext()).validate();
+    }
+
+    /**
+     * A mock {@code DistCopyHDFS} for testing.
+     */
+    private class TestableDistCopyHDFS extends DistCopyHDFS {
+        private List<Path> TEST_pathsList;
+        private Path TEST_destination;
+
+        @Nullable
+        @Override
+        protected FileSystem getFileSystem(@Nonnull ProcessContext context) {
+            return fileSystem;
+        }
+
+        @Nullable
+        @Override
+        protected Configuration getConfiguration() {
+            return configuration;
+        }
+
+        @Override
+        protected DistCp getDistCp(List<Path> pathsList, Path destination) {
+            TEST_pathsList = pathsList;
+            TEST_destination = destination;
+            return distCp;
+        }
+
+        @Override
+        HdfsResources resetHDFSResources(String configResources, String dir, ProcessContext context) throws IOException {
+            return null;
+        }
+    }
+}
