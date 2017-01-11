d73e95a57107b56e110b522fc80c997210b6efc1
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2017-01-11 14:09:38 +0000

    PC-979 Abandon All button in Ops Manager - corrected file name for the stored procedure

--- install/setup/sql/mysql/thinkbig/0.6.0/update.sh
+++ install/setup/sql/mysql/thinkbig/0.6.0/update.sh
@@ -2,6 +2,6 @@
 
 MY_DIR=$(dirname $0)
 mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/schema-0.6.0-upgrade.sql
-mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/abandon_feed_jobs.sql
+mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/abandon-feed-jobs.sql
 echo "Updated to 0.6.0 release";
 
