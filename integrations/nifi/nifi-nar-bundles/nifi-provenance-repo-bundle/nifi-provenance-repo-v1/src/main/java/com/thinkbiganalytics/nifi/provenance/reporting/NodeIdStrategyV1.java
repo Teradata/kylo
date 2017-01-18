07bd6b73095f465f510e37d6839f759e4270d4be
Gregory Hart <greg.hart@thinkbiganalytics.com>
2017-01-18 09:42:04 -0800

    Fixed linking issues with NiFi v1.0.

--- integrations/nifi/nifi-nar-bundles/nifi-provenance-repo-bundle/nifi-provenance-repo-v1/src/main/java/com/thinkbiganalytics/nifi/provenance/reporting/NodeIdStrategyV1.java
+++ integrations/nifi/nifi-nar-bundles/nifi-provenance-repo-bundle/nifi-provenance-repo-v1/src/main/java/com/thinkbiganalytics/nifi/provenance/reporting/NodeIdStrategyV1.java
@@ -5,9 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-/**
- * Created by ru186002 on 18/01/2017.
- */
 @Component
 public class NodeIdStrategyV1 implements NodeIdStrategy {
 
@@ -16,8 +13,8 @@ public class NodeIdStrategyV1 implements NodeIdStrategy {
     @Override
     public String getNodeId(ReportingContext context) {
         final boolean isClustered = context.isClustered();
-        final String nodeId = context.getClusterNodeIdentifier();
-        if (nodeId == null && isClustered) {
+        final String nodeId = isClustered ? context.getClusterNodeIdentifier() : "non-clustered-node-id";
+        if (nodeId == null) {
             log.info("This instance of NiFi is configured for clustering, but the Cluster Node Identifier is not yet available. "
                     + "Will wait for Node Identifier to be established.");
         }
