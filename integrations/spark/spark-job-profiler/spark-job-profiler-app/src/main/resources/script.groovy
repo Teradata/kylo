6846c69191cfc2709a2ba86113f00d3da9342573
Tim Harsch <tim.harsch@thinkbiganalytics.com>
2017-01-24 15:13:56 -0800

    PC-1065: remove copy from groovy script

--- integrations/spark/spark-job-profiler/spark-job-profiler-app/src/main/resources/script.groovy
+++ integrations/spark/spark-job-profiler/spark-job-profiler-app/src/main/resources/script.groovy
@@ -1,6 +1,3 @@
-/*
- * Copyright (c) 2016. Teradata Inc.
- */
 def myFunc =  [ call: { val -> return val.substring(1,5) }] as org.apache.spark.sql.api.java.UDF1
 
         //hiveContext.udf().register("myFunc", new UDF1() { (x: Integer) => x + 1})
