fb1985620b4f4efb24e8f1c5c48f7378fe93d9c4
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-01-25 10:01:48 -0800

    PC-1143 Code cleanup

--- commons/commons-util/src/main/java/com/thinkbiganalytics/Formatters.java
+++ commons/commons-util/src/main/java/com/thinkbiganalytics/Formatters.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.thinkbiganalytics;
 
 /*-
@@ -23,10 +20,6 @@ package com.thinkbiganalytics;
  * #L%
  */
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 import org.joda.time.ReadableInstant;
@@ -36,36 +29,42 @@ import org.joda.time.format.ISODateTimeFormat;
 import org.joda.time.format.PeriodFormat;
 import org.joda.time.format.PeriodFormatter;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
- * Defines common time-related formatters and parsing functionality.
- * 
- * @author Sean Felten
+ * Defines common time-related formatter's and parsing functionality.
  */
 public interface Formatters {
 
     static final PeriodFormatter PERIOD_FORMATTER = PeriodFormat.getDefault();
-    static final DateTimeFormatter ISO_DATE_TIME_FORMATTER = ISODateTimeFormat.dateTimeParser();  // No print
+    static final DateTimeFormatter ISO_DATE_TIME_FORMATTER = ISODateTimeFormat.dateTimeParser();
     static final DateTimeFormatter ISO_DATE_TIME_FORMATTER_NO_MILLIS = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ssZZ");
-//    static final DateTimeFormatter ISO_DATE_TIME_FORMATTER_NO_MILLIS = ISODateTimeFormat.dateTimeNoMillis();
-    static final DateTimeFormatter ISO_TIME_FORMATTER = ISODateTimeFormat.timeParser();  // No print
-    static final DateTimeFormatter ISO_DATE_FORMATTER_NO_MILLIS = ISODateTimeFormat.dateOptionalTimeParser(); // No print
-    static final DateTimeFormatter ISO_LOCAL_DATE_FORMATTER_NO_MILLIS = ISODateTimeFormat.localDateOptionalTimeParser(); // No print
+    static final DateTimeFormatter ISO_TIME_FORMATTER = ISODateTimeFormat.timeParser();
+    static final DateTimeFormatter ISO_DATE_FORMATTER_NO_MILLIS = ISODateTimeFormat.dateOptionalTimeParser();
+    static final DateTimeFormatter ISO_LOCAL_DATE_FORMATTER_NO_MILLIS = ISODateTimeFormat.localDateOptionalTimeParser();
     static final DateTimeFormatter SHORT_TIME_FORMATTER = DateTimeFormat.shortTime();
     static final DateTimeFormatter HMS_TIME_FORMATTER = DateTimeFormat.forPattern("HH:MM");
     static final DateTimeFormatter HM_TIME_FORMATTER = DateTimeFormat.forPattern("HH:MM");
     static final DateTimeFormatter MILLIS_FORMATTER = new MillisDateTimeFormat();
-    
-    /** A list of date/time-related formatters in order of most specific to least specific */
-    static final DateTimeFormatter[] DATE_TIME_FORMATTERS 
-        = new DateTimeFormatter[] { 
-                                    ISO_DATE_TIME_FORMATTER_NO_MILLIS, 
-                                    ISO_DATE_TIME_FORMATTER,
-                                    SHORT_TIME_FORMATTER,
-                                    HMS_TIME_FORMATTER,
-                                    HM_TIME_FORMATTER,
-                                    MILLIS_FORMATTER
-                                    };
-    
+
+    /**
+     * A list of date/time-related formatters in order of most specific to least specific
+     */
+    static final DateTimeFormatter[] DATE_TIME_FORMATTERS
+        = new DateTimeFormatter[]{
+        ISO_DATE_TIME_FORMATTER_NO_MILLIS,
+        ISO_DATE_TIME_FORMATTER,
+        SHORT_TIME_FORMATTER,
+        HMS_TIME_FORMATTER,
+        HM_TIME_FORMATTER,
+        MILLIS_FORMATTER
+    };
+
+    /**
+     * convert the String to a DateTime field using the defined formatters
+     */
     static DateTime parseDateTime(String timeStr) {
         for (DateTimeFormatter formatter : DATE_TIME_FORMATTERS) {
             try {
@@ -73,33 +72,42 @@ public interface Formatters {
             } catch (IllegalArgumentException e) {
             }
         }
-        
+
         throw new IllegalArgumentException("Date/time cannot be parsed - acceptable examples: " + getDateTimeExamples());
     }
-    
+
+    /**
+     * Return the DateTime as a String in ISO format, formatting the string in the format: "yyyy-MM-dd'T'HH:mm:ssZZ"
+     *
+     * @return the string format as "yyyy-MM-dd'T'HH:mm:ssZZ" of the DateTime object passed in
+     */
     static String print(DateTime time) {
         return ISO_DATE_TIME_FORMATTER_NO_MILLIS.print(time);
     }
-    
+
+    /**
+     * Return a list of example DateTime formatted Strings
+     *
+     * @return a list of strings with various formatting
+     */
     static Collection<String> getDateTimeExamples() {
         DateTime time = DateTime.now();
         List<String> examples = new ArrayList<>();
         examples.add(ISO_DATE_TIME_FORMATTER_NO_MILLIS.print(time));
         examples.add(SHORT_TIME_FORMATTER.print(time));
         examples.add(HMS_TIME_FORMATTER.print(time));
-        examples.add(HM_TIME_FORMATTER.print(time)); 
-        examples.add(MILLIS_FORMATTER.print(time)); 
+        examples.add(HM_TIME_FORMATTER.print(time));
+        examples.add(MILLIS_FORMATTER.print(time));
         return examples;
     }
-    
-    
-    
+
+
     class MillisDateTimeFormat extends DateTimeFormatter {
 
         public MillisDateTimeFormat() {
             super(null, null);
         }
-        
+
         @Override
         public DateTime parseDateTime(String text) {
             try {
@@ -108,7 +116,7 @@ public interface Formatters {
                 throw new IllegalArgumentException(e);
             }
         }
-        
+
         @Override
         public String print(ReadableInstant instant) {
             return Long.toString(instant.getMillis());
