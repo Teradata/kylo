c10828ad823b52b6fd225220de03d966be251cb0
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-01-30 09:02:22 -0800

    PC-1143 Code cleanup classname-change module

--- core/classname-change/classname-change-core/src/main/java/com/thinkbiganalytics/classnameregistry/ClassNameChangeSpringConfiguration.java
+++ core/classname-change/classname-change-core/src/main/java/com/thinkbiganalytics/classnameregistry/ClassNameChangeSpringConfiguration.java
@@ -24,7 +24,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Created by sr186054 on 7/22/16.
+ * Spring configuration for the ClassNameChange bean
  */
 @Configuration
 public class ClassNameChangeSpringConfiguration {
