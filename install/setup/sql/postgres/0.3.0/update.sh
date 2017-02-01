f33dd2ee6de35009900fe7b6f7e643ca94f401d8
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2017-02-01 11:17:56 -0800

    removed postgres sql folder

--- install/setup/sql/postgres/0.3.0/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $# -gt 0 ]] ; then
-    echo "Usage is: sudo ./setup-postgres.sh"
-    exit 1
-fi
-
-sudo -u postgres psql -f alter_tables.sql
-echo "Updated to 0.3.0 release";
--- install/setup/sql/postgres/kylo/create-database.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE kylo owner=pguser;
-\connect kylo;
-CREATE CAST (varchar as bigint) with inout as implicit;
