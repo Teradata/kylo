f2679973f63baed96bdb30e81ea3f12eac00e86d
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-02-02 16:10:03 -0800

    indexes and foreign keys to speed up queries and event/job inserts

---
+++ install/setup/sql/mysql/kylo/0.7.0/update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MY_DIR=$(dirname $0)
+mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/add_table_indexes_and_foreign_keys.sql
+mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/remove_unused_tables.sql
+echo "Updated to 0.7.0 release";
+
