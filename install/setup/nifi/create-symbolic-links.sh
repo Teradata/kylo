da98152f93c5e026f4092f3a5207a39b28edb157
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2017-02-09 11:47:26 -0800

    PC-1218 - Remove nifi 0.x references from RPM build

--- install/setup/nifi/create-symbolic-links.sh
+++ install/setup/nifi/create-symbolic-links.sh
@@ -3,21 +3,12 @@
 ln -f -s /opt/nifi/data/lib/kylo-nifi-core-service-nar-*.nar /opt/nifi/current/lib/kylo-nifi-core-service-nar.nar
 ln -f -s /opt/nifi/data/lib/kylo-nifi-standard-services-nar-*.nar /opt/nifi/current/lib/kylo-nifi-standard-services-nar.nar
 
-if grep -q "nifi.version=0." /opt/nifi/current/conf/nifi.properties; then
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-core-v0-nar-*.nar /opt/nifi/current/lib/kylo-nifi-core-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-spark-v0-nar-*.nar /opt/nifi/current/lib/kylo-nifi-spark-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-hadoop-v0-nar-*.nar /opt/nifi/current/lib/kylo-nifi-hadoop-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-hadoop-service-v0-nar-*.nar /opt/nifi/current/lib/kylo-nifi-hadoop-service-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-provenance-repo-v0-nar-*.nar /opt/nifi/current/lib/kylo-nifi-provenance-repo-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-elasticsearch-v0-nar-*.nar /opt/nifi/current/lib/kylo-nifi-elasticsearch-nar.nar
-else
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-core-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-core-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-spark-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-spark-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-hadoop-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-hadoop-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-hadoop-service-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-hadoop-service-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-provenance-repo-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-provenance-repo-nar.nar
-    ln -f -s /opt/nifi/data/lib/kylo-nifi-elasticsearch-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-elasticsearch-nar.nar
-fi
+ln -f -s /opt/nifi/data/lib/kylo-nifi-core-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-core-nar.nar
+ln -f -s /opt/nifi/data/lib/kylo-nifi-spark-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-spark-nar.nar
+ln -f -s /opt/nifi/data/lib/kylo-nifi-hadoop-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-hadoop-nar.nar
+ln -f -s /opt/nifi/data/lib/kylo-nifi-hadoop-service-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-hadoop-service-nar.nar
+ln -f -s /opt/nifi/data/lib/kylo-nifi-provenance-repo-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-provenance-repo-nar.nar
+ln -f -s /opt/nifi/data/lib/kylo-nifi-elasticsearch-v1-nar-*.nar /opt/nifi/current/lib/kylo-nifi-elasticsearch-nar.nar
 
 SPARK_PROFILE="spark-v"$(spark-submit --version 2>&1 | grep -o "version [0-9]" | grep -o "[0-9]")
 ln -f -s /opt/nifi/data/lib/app/kylo-spark-validate-cleanse-${SPARK_PROFILE}-*-jar-with-dependencies.jar /opt/nifi/current/lib/app/kylo-spark-validate-cleanse-jar-with-dependencies.jar
