f8884ade635b96f91482c55ef7d4c57a87b7caf0
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-02-08 17:53:14 -0800

    fix delete error in stored procedure

--- install/setup/sql/mysql/kylo/0.7.0/update.sh
+++ install/setup/sql/mysql/kylo/0.7.0/update.sh
@@ -3,5 +3,6 @@
 MY_DIR=$(dirname $0)
 mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/add_table_indexes_and_foreign_keys.sql
 mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/remove_unused_tables.sql
+mysql -f -h $1 -u$2 --password=$3 < ${MY_DIR}/delete_feed_jobs.sql
 echo "Updated to 0.7.0 release";
 
