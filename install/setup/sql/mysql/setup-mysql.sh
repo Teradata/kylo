88cb4b03fc43c5953f5aeded78f4fd88c2f0eef4
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-03-13 14:12:42 -0700

    KYLO-308 add the 0.7.1 update script to the master setup script

--- install/setup/sql/mysql/setup-mysql.sh
+++ install/setup/sql/mysql/setup-mysql.sh
@@ -11,6 +11,7 @@ $MYSQL_DIR/kylo/0.4.2/update.sh $1 $2 $3
 $MYSQL_DIR/kylo/0.5.0/update.sh $1 $2 $3
 $MYSQL_DIR/kylo/0.6.0/update.sh $1 $2 $3
 $MYSQL_DIR/kylo/0.7.0/update.sh $1 $2 $3
+$MYSQL_DIR/kylo/0.7.1/update.sh $1 $2 $3
 
 mysql -h $1 -u$2 --password=$3 -e 'show databases;'
 
