94373c59a9567f35ebe59d4d13fdd713b6a205f6
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2017-03-28 11:33:43 +0100

    KYLO-390 Support for other databases, 0.8.0 database script will mark 0.8.0 as initial Liquibase upgrade and from there on everyone can simply start using Liquibase

---
+++ install/setup/sql/liquibase/changelog/0.8.0/procedures/mysql/abandon-feed-jobs.sql
@@ -0,0 +1,21 @@
+DROP PROCEDURE IF EXISTS abandon_feed_jobs;
+
+CREATE PROCEDURE abandon_feed_jobs(in feed varchar(255), in exitMessage varchar(255), out res integer)
+BEGIN
+
+UPDATE BATCH_JOB_EXECUTION
+  INNER JOIN BATCH_JOB_INSTANCE ON BATCH_JOB_INSTANCE.JOB_INSTANCE_ID = BATCH_JOB_EXECUTION.JOB_INSTANCE_ID
+SET BATCH_JOB_EXECUTION.STATUS = 'ABANDONED',
+  BATCH_JOB_EXECUTION.EXIT_MESSAGE = CONCAT_WS('\n',BATCH_JOB_EXECUTION.EXIT_MESSAGE,exitMessage)
+WHERE BATCH_JOB_INSTANCE.JOB_NAME in ( SELECT checkFeed.NAME
+    FROM FEED_CHECK_DATA_FEEDS c
+    inner join FEED f on f.name = feed and c.FEED_ID = f.id
+    inner join FEED checkFeed on checkFeed.id = c.CHECK_DATA_FEED_ID
+    UNION
+    SELECT feed from dual )
+  AND BATCH_JOB_EXECUTION.STATUS = 'FAILED';
+
+  --   need to return a value for this procedure calls to work on postgresql and with spring-data-jpa repositories and named queries
+set res = 1;
+
+END
---
+++ install/setup/sql/liquibase/changelog/0.8.0/procedures/postgresql/abandon-feed-jobs.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE FUNCTION abandon_feed_jobs(in feed varchar(255), in exitMessage varchar(255))
+  RETURNS integer AS $$
+BEGIN
+
+UPDATE BATCH_JOB_EXECUTION
+SET
+  STATUS = 'ABANDONED',
+  EXIT_MESSAGE = CONCAT_WS(chr(10),EXIT_MESSAGE,exitMessage)
+FROM BATCH_JOB_INSTANCE
+WHERE BATCH_JOB_INSTANCE.JOB_INSTANCE_ID = BATCH_JOB_EXECUTION.JOB_INSTANCE_ID
+AND BATCH_JOB_INSTANCE.JOB_NAME in ( SELECT checkFeed.NAME
+                                       FROM FEED_CHECK_DATA_FEEDS c
+                                         inner join FEED f on f.name = feed and c.FEED_ID = f.id
+                                         inner join FEED checkFeed on checkFeed.id = c.CHECK_DATA_FEED_ID
+                                       UNION
+                                       SELECT feed)
+      AND STATUS = 'FAILED';
+
+--   need to return a value for this procedure calls to work with spring-data-jpa repositories and named queries
+return 1;
+
+END;
+$$ LANGUAGE plpgsql;
