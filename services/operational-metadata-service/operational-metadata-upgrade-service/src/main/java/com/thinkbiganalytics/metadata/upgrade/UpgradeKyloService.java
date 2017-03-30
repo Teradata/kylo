0c6d7f4a0fe28e1ef747ed057363632b8fb6ee66
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-03-30 15:53:20 -0700

    add license headers, fix initial check on new database

--- services/operational-metadata-service/operational-metadata-upgrade-service/src/main/java/com/thinkbiganalytics/metadata/upgrade/UpgradeKyloService.java
+++ services/operational-metadata-service/operational-metadata-upgrade-service/src/main/java/com/thinkbiganalytics/metadata/upgrade/UpgradeKyloService.java
@@ -1,5 +1,25 @@
 package com.thinkbiganalytics.metadata.upgrade;
 
+/*-
+ * #%L
+ * kylo-operational-metadata-upgrade-service
+ * %%
+ * Copyright (C) 2017 ThinkBig Analytics
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.lang.reflect.InvocationTargetException;
 
 /*-
@@ -105,6 +125,9 @@ public class UpgradeKyloService {
 //    };
     public KyloVersion getCurrentVersion() {
         KyloVersion version = kyloVersionProvider.getCurrentVersion();
+        if(version == null){
+            version = kyloVersionProvider.getLatestVersion();
+        }
         return new Version(version.getMajorVersion(), version.getMinorVersion());
     }
     
