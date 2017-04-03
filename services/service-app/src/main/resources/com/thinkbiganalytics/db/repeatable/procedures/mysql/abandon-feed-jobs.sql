df9304d45afa458ff862e800afaff447190bf704
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2017-04-03 13:07:17 +0100

    KYLO-390 Support for other databases, workaround for generating single valid sql update script and also being valid for automatic updates

--- services/service-app/src/main/resources/com/thinkbiganalytics/db/repeatable/procedures/mysql/abandon-feed-jobs.sql
+++ services/service-app/src/main/resources/com/thinkbiganalytics/db/repeatable/procedures/mysql/abandon-feed-jobs.sql
@@ -17,13 +17,14 @@
 -- limitations under the License.
 -- #L%
 -- -
-CREATE PROCEDURE abandon_feed_jobs(in feed varchar(255), in exitMessage varchar(255))
+CREATE PROCEDURE abandon_feed_jobs(in feed varchar(255), in exitMessage varchar(255), out res integer)
 BEGIN
 
 UPDATE BATCH_JOB_EXECUTION
-  INNER JOIN BATCH_JOB_INSTANCE ON BATCH_JOB_INSTANCE.JOB_INSTANCE_ID = BATCH_JOB_EXECUTION.JOB_INSTANCE_ID
+  INNER JOIN BATCH_JOB_INSTANCE
+    ON BATCH_JOB_INSTANCE.JOB_INSTANCE_ID = BATCH_JOB_EXECUTION.JOB_INSTANCE_ID
 SET BATCH_JOB_EXECUTION.STATUS = 'ABANDONED',
-  BATCH_JOB_EXECUTION.EXIT_MESSAGE = CONCAT_WS('\n',BATCH_JOB_EXECUTION.EXIT_MESSAGE,exitMessage)
+  BATCH_JOB_EXECUTION.EXIT_MESSAGE = CONCAT_WS('\n', BATCH_JOB_EXECUTION.EXIT_MESSAGE, exitMessage)
 WHERE BATCH_JOB_INSTANCE.JOB_NAME in ( SELECT checkFeed.NAME
     FROM FEED_CHECK_DATA_FEEDS c
     inner join FEED f on f.name = feed and c.FEED_ID = f.id
@@ -32,4 +33,7 @@ WHERE BATCH_JOB_INSTANCE.JOB_NAME in ( SELECT checkFeed.NAME
     SELECT feed from dual )
   AND BATCH_JOB_EXECUTION.STATUS = 'FAILED';
 
-END
+  --   need to return a value for this procedure calls to work on postgresql and with spring-data-jpa repositories and named queries
+set res = 1;
+
+END;
