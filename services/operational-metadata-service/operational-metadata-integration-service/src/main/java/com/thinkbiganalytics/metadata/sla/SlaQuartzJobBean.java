a7b9737633ba736e8cadd07f334ff5bc0598d04f
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-05-04 16:22:26 -0700

    KYLO-420 SLA Scheduler Cluster Aware (DB persistence)

--- services/operational-metadata-service/operational-metadata-integration-service/src/main/java/com/thinkbiganalytics/metadata/sla/SlaQuartzJobBean.java
+++ services/operational-metadata-service/operational-metadata-integration-service/src/main/java/com/thinkbiganalytics/metadata/sla/SlaQuartzJobBean.java
@@ -1,5 +1,25 @@
 package com.thinkbiganalytics.metadata.sla;
 
+/*-
+ * #%L
+ * thinkbig-operational-metadata-integration-service
+ * %%
+ * Copyright (C) 2017 ThinkBig Analytics
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.thinkbiganalytics.metadata.api.MetadataAccess;
 import com.thinkbiganalytics.metadata.sla.api.ServiceLevelAgreement;
 import com.thinkbiganalytics.metadata.sla.spi.ServiceLevelAgreementChecker;
@@ -16,7 +36,7 @@ import java.util.Map;
 import javax.inject.Inject;
 
 /**
- * Created by sr186054 on 5/4/17.
+ * Quartz Scheduled Job Bean that will run the SLA code
  */
 public class SlaQuartzJobBean extends QuartzJobBean{
     public static final String SLA_ID_PARAM = "SLA_ID";
