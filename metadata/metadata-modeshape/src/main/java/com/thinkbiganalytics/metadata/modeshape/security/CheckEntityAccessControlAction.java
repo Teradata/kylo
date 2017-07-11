1e3f5a8ffd7d82704135cdf8c54436eecbe6b2a5
Sean Felten <sean.felten@thinkbiganalytics.com>
2017-07-11 08:17:53 -0700

    KYLO-893: Ensure role setup when transitioning from EAC disabled to enabled

--- metadata/metadata-modeshape/src/main/java/com/thinkbiganalytics/metadata/modeshape/security/CheckEntityAccessControlAction.java
+++ metadata/metadata-modeshape/src/main/java/com/thinkbiganalytics/metadata/modeshape/security/CheckEntityAccessControlAction.java
@@ -1,17 +1,20 @@
-package com.thinkbiganalytics.metadata.upgrade.v081;
+/**
+ * 
+ */
+package com.thinkbiganalytics.metadata.modeshape.security;
 
 /*-
  * #%L
- * kylo-operational-metadata-upgrade-service
+ * kylo-metadata-modeshape
  * %%
  * Copyright (C) 2017 ThinkBig Analytics
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +23,24 @@ package com.thinkbiganalytics.metadata.upgrade.v081;
  * #L%
  */
 
-import com.thinkbiganalytics.KyloVersion;
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import javax.jcr.Node;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+
+import com.thinkbiganalytics.metadata.api.MetadataAccess;
+import com.thinkbiganalytics.metadata.api.PostMetadataConfigAction;
 import com.thinkbiganalytics.metadata.api.category.Category;
 import com.thinkbiganalytics.metadata.api.category.CategoryProvider;
 import com.thinkbiganalytics.metadata.api.category.security.CategoryAccessControl;
@@ -33,11 +53,12 @@ import com.thinkbiganalytics.metadata.api.template.FeedManagerTemplate;
 import com.thinkbiganalytics.metadata.api.template.FeedManagerTemplateProvider;
 import com.thinkbiganalytics.metadata.api.template.security.TemplateAccessControl;
 import com.thinkbiganalytics.metadata.modeshape.JcrMetadataAccess;
-import com.thinkbiganalytics.metadata.modeshape.MetadataRepositoryException;
 import com.thinkbiganalytics.metadata.modeshape.category.JcrCategory;
+import com.thinkbiganalytics.metadata.modeshape.common.SecurityPaths;
 import com.thinkbiganalytics.metadata.modeshape.feed.JcrFeed;
 import com.thinkbiganalytics.metadata.modeshape.security.action.JcrAllowedActions;
 import com.thinkbiganalytics.metadata.modeshape.support.JcrPropertyUtil;
+import com.thinkbiganalytics.metadata.modeshape.support.JcrUtil;
 import com.thinkbiganalytics.metadata.modeshape.template.JcrFeedTemplate;
 import com.thinkbiganalytics.security.AccessController;
 import com.thinkbiganalytics.security.action.Action;
@@ -46,119 +67,74 @@ import com.thinkbiganalytics.security.action.AllowedActions;
 import com.thinkbiganalytics.security.action.AllowedEntityActionsProvider;
 import com.thinkbiganalytics.security.role.SecurityRole;
 import com.thinkbiganalytics.security.role.SecurityRoleProvider;
-import com.thinkbiganalytics.server.upgrade.KyloUpgrader;
-import com.thinkbiganalytics.server.upgrade.UpgradeState;
-
-import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
-import java.security.Principal;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
-
-import javax.annotation.Nonnull;
-import javax.inject.Inject;
-import javax.jcr.ItemNotFoundException;
 
 /**
- * This action is upgraded both on upgrade to v0.8.1 and during a fresh install.
+ * An action invoked early post-metadata configuration that checks if access control has been enabled or disabled.
+ * If it was enabled when previously disabled then roles are setup.  If it was disabled when previously enabled then
+ * an exception is thrown to fail startup.
  */
-@Component("upgradeAction081")
-@Profile(KyloUpgrader.KYLO_UPGRADE)
-public class SecurityRolesUpgradeAction implements UpgradeState {
+@Order(PostMetadataConfigAction.EARLY_ORDER)
+public class CheckEntityAccessControlAction implements PostMetadataConfigAction {
 
-    private static final Logger log = LoggerFactory.getLogger(SecurityRolesUpgradeAction.class);
+    private static final Logger log = LoggerFactory.getLogger(CheckEntityAccessControlAction.class);
+    
+    @Inject
+    private MetadataAccess metadata;
 
-    @Inject
-    private CategoryProvider categoryProvider;
-    @Inject
-    private FeedProvider feedProvider;
-    @Inject
-    private FeedManagerTemplateProvider feedManagerTemplateProvider;
-    @Inject
-    private SecurityRoleProvider roleProvider;
-    @Inject
-    private AllowedEntityActionsProvider actionsProvider;
     @Inject
     private AccessController accessController;
     
-    private boolean upgraded = false;
+    @Inject
+    private SecurityRoleProvider roleProvider;
     
+    @Inject
+    private AllowedEntityActionsProvider actionsProvider;
+
+    @Inject
+    private CategoryProvider categoryProvider;
+    
+    @Inject
+    private FeedProvider feedProvider;
+    
+    @Inject
+    private FeedManagerTemplateProvider feedManagerTemplateProvider;
+
     
     @Override
-    public boolean isTargetFreshInstall() {
-        return true;
-    }
-
-    @Override
-    public boolean isTargetVersion(KyloVersion version) {
-        return version.matches("0.8", "1", "");
-    }
-
-    /* (non-Javadoc)
-     * @see com.thinkbiganalytics.metadata.upgrade.UpgradeState#upgradeFrom(com.thinkbiganalytics.metadata.api.app.KyloVersion)
-     */
-    @Override
-    public void upgradeTo(KyloVersion startingVersion) {
-        // Only invoke this action once; either during upgrade or a fresh install.
-        // This method may be upgraded twice if a fresh install is being performed for build v0.8.1 specifically.
-        if (! this.upgraded) {
-            log.info("Upgrading from version: " + startingVersion);
+    public void run() {
+        metadata.commit(() -> {
+            Node securityNode = JcrUtil.getNode(JcrMetadataAccess.getActiveSession(), SecurityPaths.SECURITY.toString());
+            boolean propertyEnabled = accessController.isEntityAccessControlled();
+            boolean metadataEnabled = wasAccessControllEnabled(securityNode);
             
-            ensureFeedTemplateFeedRelationships();
-            if (accessController.isEntityAccessControlled()) {
+            if (metadataEnabled == true && propertyEnabled == false) {
+                log.error(  "\n*************************************************************************************************************************************\n"
+                            + "Kylo has previously been started with entity access control enabled and the current configuration is attempting to set it as disabled\n"
+                            + "*************************************************************************************************************************************");
+                throw new IllegalStateException("Entity access control is configured as disabled when it was previously enabled");
+            } else if (metadataEnabled == false && propertyEnabled == true) {
                 ensureDefaultEntityRoles();
-            } 
-            
-            this.upgraded = true;
+            }
+
+            JcrPropertyUtil.setProperty(securityNode, SecurityPaths.ENTITY_ACCESS_CONTROL_ENABLED, propertyEnabled);
+        }, MetadataAccess.SERVICE);
+    }
+
+    private boolean wasAccessControllEnabled(Node securityNode) {
+        if (JcrPropertyUtil.hasProperty(securityNode, SecurityPaths.ENTITY_ACCESS_CONTROL_ENABLED)) {
+            return JcrPropertyUtil.getBoolean(securityNode, SecurityPaths.ENTITY_ACCESS_CONTROL_ENABLED);
+        } else {
+            return roleProvider.getRoles().values().stream().anyMatch(roles -> roles.size() > 0);
         }
     }
-    
-    private void ensureFeedTemplateFeedRelationships() {
-        //ensure the templates have the feed relationships
-        List<Feed> feeds = feedProvider.findAll();
-        if (feeds != null) {
-            feeds.stream().forEach(feed -> {
-                FeedManagerTemplate template = feed.getTemplate();
-                if (template != null) {
-                    //ensure the template has feeds.
-                    List<Feed> templateFeeds = null;
-                    try {
-                        templateFeeds = template.getFeeds();
-                    } catch (MetadataRepositoryException e) {
-                        //templateFeeds are weak references.
-                        //if the template feeds return itemNotExists we need to reset it
-                        Throwable rootCause = ExceptionUtils.getRootCause(e);
-                        if (rootCause != null && rootCause instanceof ItemNotFoundException) {
-                            //reset the reference collection.  It will be rebuilt in the subsequent call
-                            JcrPropertyUtil.removeAllFromSetProperty(((JcrFeedTemplate) template).getNode(), JcrFeedTemplate.FEEDS);
-                        }
-                    }
-                    if (templateFeeds == null || !templateFeeds.contains(feed)) {
-                        template.addFeed(feed);
-                        feedManagerTemplateProvider.update(template);
-                    }
-                }
-            });
 
-        }
-
-        feedProvider.populateInverseFeedDependencies();
-    }
 
     private void ensureDefaultEntityRoles() {
         createDefaultRoles();
 
-        ensureTemplateRoles();
-        ensureCategoryRoles();
-        ensureFeedRoles();
+        ensureTemplateAccessControl();
+        ensureCategoryAccessControl();
+        ensureFeedAccessControl();
     }
 
     private void createDefaultRoles() {
@@ -248,7 +224,7 @@ public class SecurityRolesUpgradeAction implements UpgradeState {
         }
     }
 
-    private void ensureFeedRoles() {
+    private void ensureFeedAccessControl() {
         List<Feed> feeds = feedProvider.findAll();
         if (feeds != null) {
             List<SecurityRole> roles = this.roleProvider.getEntityRoles(SecurityRole.FEED);
@@ -260,7 +236,7 @@ public class SecurityRolesUpgradeAction implements UpgradeState {
         }
     }
 
-    private void ensureCategoryRoles() {
+    private void ensureCategoryAccessControl() {
         List<Category> categories = categoryProvider.findAll();
         if (categories != null) {
             List<SecurityRole> roles = this.roleProvider.getEntityRoles(SecurityRole.CATEGORY);
@@ -272,7 +248,7 @@ public class SecurityRolesUpgradeAction implements UpgradeState {
         }
     }
 
-    private void ensureTemplateRoles() {
+    private void ensureTemplateAccessControl() {
         List<FeedManagerTemplate> templates = feedManagerTemplateProvider.findAll();
         if (templates != null) {
             List<SecurityRole> roles = this.roleProvider.getEntityRoles(SecurityRole.TEMPLATE);
@@ -283,4 +259,5 @@ public class SecurityRolesUpgradeAction implements UpgradeState {
             });
         }
     }
+
 }
