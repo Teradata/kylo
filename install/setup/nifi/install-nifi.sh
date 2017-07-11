ac30f86f1cecf40ec355921aef829c47fb6ae2c5
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2017-07-11 08:47:09 -0700

    correctly setup nifi path in nifi.properties file when installing in places other than /opt/nifi

--- install/setup/nifi/install-nifi.sh
+++ install/setup/nifi/install-nifi.sh
@@ -53,16 +53,18 @@ chown -R $NIFI_USER:$NIFI_GROUP $NIFI_INSTALL_HOME
 echo "NiFi installation complete"
 
 echo "Modifying the nifi.properties file"
-sed -i 's/nifi.flow.configuration.file=.\/conf\/flow.xml.gz/nifi.flow.configuration.file=\/opt\/nifi\/data\/conf\/flow.xml.gz/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.flow.configuration.archive.dir=.\/conf\/archive\//nifi.flow.configuration.archive.dir=\/opt\/nifi\/data\/conf\/archive\//' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.authorizer.configuration.file=.\/conf\/authorizers.xml/nifi.authorizer.configuration.file=\/opt\/nifi\/data\/conf\/authorizers.xml/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.templates.directory=.\/conf\/templates/nifi.templates.directory=\/opt\/nifi\/data\/conf\/templates/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.flowfile.repository.directory=.\/flowfile_repository/nifi.flowfile.repository.directory=\/opt\/nifi\/data\/flowfile_repository/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.content.repository.directory.default=.\/content_repository/nifi.content.repository.directory.default=\/opt\/nifi\/data\/content_repository/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.content.repository.archive.enabled=true/nifi.content.repository.archive.enabled=false/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.provenance.repository.directory.default=.\/provenance_repository/nifi.provenance.repository.directory.default=\/opt\/nifi\/data\/provenance_repository/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.web.http.port=8080/nifi.web.http.port=8079/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
-sed -i 's/nifi.provenance.repository.implementation=org.apache.nifi.provenance.PersistentProvenanceRepository/nifi.provenance.repository.implementation=com.thinkbiganalytics.nifi.provenance.repo.KyloPersistentProvenanceEventRepository/' $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.flow.configuration.file=.\/conf\/flow.xml.gz|nifi.flow.configuration.file=$NIFI_INSTALL_HOME\/data\/conf\/flow.xml.gz|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.flow.configuration.archive.dir=.\/conf\/archive\/|nifi.flow.configuration.archive.dir=$NIFI_INSTALL_HOME\/data\/conf\/archive\/|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.authorizer.configuration.file=.\/conf\/authorizers.xml|nifi.authorizer.configuration.file=$NIFI_INSTALL_HOME\/data\/conf\/authorizers.xml|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.templates.directory=.\/conf\/templates|nifi.templates.directory=$NIFI_INSTALL_HOME\/data\/conf\/templates|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.flowfile.repository.directory=.\/flowfile_repository|nifi.flowfile.repository.directory=$NIFI_INSTALL_HOME\/data\/flowfile_repository|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.content.repository.directory.default=.\/content_repository|nifi.content.repository.directory.default=$NIFI_INSTALL_HOME\/data\/content_repository|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.content.repository.archive.enabled=true|nifi.content.repository.archive.enabled=false|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.provenance.repository.directory.default=.\/provenance_repository|nifi.provenance.repository.directory.default=$NIFI_INSTALL_HOME\/data\/provenance_repository|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.web.http.port=8080|nifi.web.http.port=8079|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+sed -i "s|nifi.provenance.repository.implementation=org.apache.nifi.provenance.PersistentProvenanceRepository|nifi.provenance.repository.implementation=com.thinkbiganalytics.nifi.provenance.repo.KyloPersistentProvenanceEventRepository|" $NIFI_INSTALL_HOME/current/conf/nifi.properties
+
+# sed -i "s|=/opt/nifi|=/apps/nifi|" /apps/nifi/current/conf/nifi.properties
 
 echo "Updating the log file path"
 sed -i 's/NIFI_LOG_DIR=\".*\"/NIFI_LOG_DIR=\"\/var\/log\/nifi\"/' $NIFI_INSTALL_HOME/current/bin/nifi-env.sh
