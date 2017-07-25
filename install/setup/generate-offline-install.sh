3a1dedcb5d680b3bca4abe0343cf16aa1fe207ac
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2017-07-25 11:41:35 -0700

    KYLO-906 - Upgraded ES installation to 5.5.0

--- install/setup/generate-offline-install.sh
+++ install/setup/generate-offline-install.sh
@@ -5,8 +5,8 @@ cd /opt/kylo/setup
 wget https://archive.apache.org/dist/activemq/5.13.3/apache-activemq-5.13.3-bin.tar.gz -P ./activemq
 
 # Modify to DEB file if necessary
-# wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/2.3.0/elasticsearch-2.3.0.deb -P ./elasticsearch/
-wget https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/rpm/elasticsearch/2.3.0/elasticsearch-2.3.0.rpm -P ./elasticsearch/
+# wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.5.0.deb -P ./elasticsearch/
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.5.0.rpm -P ./elasticsearch/
 
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -P ./java
 
