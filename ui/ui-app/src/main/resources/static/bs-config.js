d5992f0b52baa47a99a82a53a18dcc28756e6cc5
Gregory Hart <greg.hart@thinkbiganalytics.com>
2017-08-07 12:14:55 -0700

    KYLO-762 Added login and logout paths to Browser Sync.

--- ui/ui-app/src/main/resources/static/bs-config.js
+++ ui/ui-app/src/main/resources/static/bs-config.js
@@ -1,11 +1,13 @@
 var proxy = require("http-proxy-middleware");
+var proxyFilter = function (pathName, req) {
+    return pathName.match(/^\/api|^\/login$|^\/logout$|^\/proxy/);
+};
 
 module.exports = {
     server: {
         baseDir: "src/main/resources/static",
         middleware: [
-            proxy("/api", {target: "http://localhost:8400/"}),
-            proxy("/proxy", {target: "http://localhost:8400/"})
+            proxy(proxyFilter, {target: "http://localhost:8400/"})
         ]
     }
 };
