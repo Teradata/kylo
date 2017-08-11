35779f14ca3bc6b9685a5e1e317f4505c54085ee
Jagrut Sharma <jagrut.sharma@thinkbiganalytics.com>
2017-08-11 13:55:53 -0700

    KYLO-1001 Helper script to create kylo indexes for Elasticsearch v5

---
+++ plugins/search-elasticsearch-rest/scripts/es-v5/create-kylo-indexes-esv5.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#################################################################################################
+#	Run this script before starting Kylo services for the 1st time, for Elasticsearch v5        #
+#################################################################################################
+
+ELASTIC_SEARCH_HOST=$1
+ELASTIC_SEARCH_REST_PORT=$2
+NUMBER_OF_SHARDS=$3
+NUMBER_OF_REPLICAS=$4
+
+if [ $# -eq 4 ]
+then
+	echo "Setting up kylo indexes in Elasticsearch (v5): host="$ELASTIC_SEARCH_HOST", port="$ELASTIC_SEARCH_REST_PORT", num-shards="$NUMBER_OF_SHARDS", num-replicas="$NUMBER_OF_REPLICAS
+else
+	echo "Usage: <command> <host> <rest-port> <num-shards> <num-replicas>"
+	echo "Examples values:"
+	echo " host: localhost"
+	echo " rest-port: 9200"
+	echo " num-shards: 1"
+	echo " num-replicas: 1"
+	echo " Note: num-shards and num-replicas can be set to 1 for development environment" 
+	exit 1
+fi
+
+kylo_indexes=kylo-data,kylo-datasources,kylo-categories-metadata,kylo-categories-default,kylo-feeds-metadata,kylo-feeds-default
+
+BODY="{
+		\"settings\" : {
+			\"index\" : {
+				\"number_of_shards\" : "$NUMBER_OF_SHARDS", 
+				\"number_of_replicas\" : "$NUMBER_OF_REPLICAS"
+				}
+		}
+	}"
+	
+for KYLO_INDEX in $(echo $kylo_indexes | sed "s/,/ /g")
+do
+	curl -XPUT $ELASTIC_SEARCH_HOST:$ELASTIC_SEARCH_REST_PORT/$KYLO_INDEX?pretty -H 'Content-Type: application/json' -d"$BODY"
+done
+
+echo "Done"
