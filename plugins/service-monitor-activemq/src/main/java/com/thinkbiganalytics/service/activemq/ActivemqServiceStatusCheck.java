8738cf5893d5652d5c922f4526d16e388833532a
shashivish <shashi.vish@yahoo.in>
2017-08-13 17:26:27 +0100

    Adding Activemq configurations and kylo service monitor dependencies

---
+++ plugins/service-monitor-activemq/src/main/java/com/thinkbiganalytics/service/activemq/ActivemqServiceStatusCheck.java
@@ -0,0 +1,35 @@
+package com.thinkbiganalytics.service.activemq;
+
+/*-
+ * #%L
+ * thinkbig-service-monitor-nifi
+ * %%
+ * Copyright (C) 2017 ThinkBig Analytics
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import com.thinkbiganalytics.servicemonitor.check.ServiceStatusCheck;
+import com.thinkbiganalytics.servicemonitor.model.DefaultServiceComponent;
+import com.thinkbiganalytics.servicemonitor.model.DefaultServiceStatusResponse;
+import com.thinkbiganalytics.servicemonitor.model.ServiceComponent;
+import com.thinkbiganalytics.servicemonitor.model.ServiceStatusResponse;
+
+
+public class ActivemqServiceStatusCheck {
+
+
+
+}
