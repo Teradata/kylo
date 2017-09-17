baca5675c0d9c2dffebf8b5532e17849b3235b9f
claudiu stanciu <claudiu.a.stanciu@gmail.com>
2017-09-17 14:10:47 +0200

    KYLO-994 Fix properties issues with sla-email. Refactor Message type and multiple address send for sla-email

--- plugins/sla-email/src/main/java/com/thinkbiganalytics/metadata/sla/EmailServiceLevelAgreementAction.java
+++ plugins/sla-email/src/main/java/com/thinkbiganalytics/metadata/sla/EmailServiceLevelAgreementAction.java
@@ -50,18 +50,13 @@ public class EmailServiceLevelAgreementAction implements ServiceLevelAgreementAc
         String desc = ServiceLevelAssessmentAlertUtil.getDescription(assessment);
         String slaName = assessment.getAgreement().getName();
         String emails = actionConfiguration.getEmailAddresses();
-        sendToAddresses(desc, slaName, emails);
+        sendToAddresses(emails, slaName, desc);
         return true;
     }
 
-    void sendToAddresses(String desc, String slaName, String emails) {
-        String[] addresses = emails.split(",");
-        Arrays.stream(addresses).forEach(address -> sendToAddress(desc, slaName, address.trim()));
-    }
-
-    private void sendToAddress(String desc, String slaName, String address) {
-        log.info("Responding to SLA violation.  About to send an email for SLA {} ", slaName);
-        emailService.sendMail(address, "SLA Violated: " + slaName, desc);
+    public void sendToAddresses(String addresses, String slaName, String desc) {
+        log.info("Responding to SLA violation.  About to send an email for SLA: {} ", slaName);
+        emailService.sendMail(addresses, "SLA Violated: " + slaName, desc);
     }
 
     /**
