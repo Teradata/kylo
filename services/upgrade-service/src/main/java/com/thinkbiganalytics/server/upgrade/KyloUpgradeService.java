11036334c1408bd7554a962275eda091250f4054
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-10-16 08:49:35 -0700

    add missing kylo upgrade versions

--- services/upgrade-service/src/main/java/com/thinkbiganalytics/server/upgrade/KyloUpgradeService.java
+++ services/upgrade-service/src/main/java/com/thinkbiganalytics/server/upgrade/KyloUpgradeService.java
@@ -56,13 +56,18 @@ public class KyloUpgradeService {
               "0.6.3",
               "0.6.4",
               "0.7.0",
+              "0.7.1",
+              "0.7.2",
+              "0.7.2.1",
               "0.8.0",
               "0.8.0.1",
               "0.8.1",
               "0.8.2",
               "0.8.2.2",
               "0.8.2.3",
-              "0.8.2.4"
+              "0.8.2.4",
+              "0.8.2.5",
+              "0.8.2.6"
         };
         
         UPGRADE_SEQUENCE = Collections.unmodifiableList(Arrays.stream(versions)
