8463af61cfb596fefb928137e8e5608772bfa8f1
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2017-11-02 20:06:57 -0700

    Updated HDF NiFi install script to support older versions of NiFI

--- install/setup/nifi/hdf/install-kylo-hdf-components.sh
+++ install/setup/nifi/hdf/install-kylo-hdf-components.sh
@@ -15,18 +15,48 @@ mkdir $HDF_NIFI_HOME_FOLDER/lib/app
 chown $NIFI_USER:$NIFI_GROUP $HDF_NIFI_HOME_FOLDER/lib/app
 
 echo "Creating symbolic links"
+
+# Expected path something like /usr/hdf/current/nifi
+framework_name=$(find $NIFI_HOME/lib/ -name "nifi-framework-api*.jar")
+prefix="$NIFI_HOME/current/lib/nifi-framework-api-"
+len=${#prefix}
+ver=${framework_name:$len}
+
+ln -f -s $NIFI_HOME/data/lib/kylo-nifi-elasticsearch-v1-nar-*.nar $NIFI_HOME/current/lib/kylo-nifi-elasticsearch-nar.nar
+
+if [[ $ver == 1.0* ]] || [[ $ver == 1.1* ]] ;
+then
+
+echo "Creating symlinks for NiFi version $ver compatible nars"
+
 ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-core-service-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-core-service-nar.nar
 ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-standard-services-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-standard-services-nar.nar
 ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-core-v1-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-core-nar.nar
 ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-spark-v1-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-spark-nar.nar
 ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-hadoop-v1-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-hadoop-nar.nar
 ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-hadoop-service-v1-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-hadoop-service-nar.nar
-ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-provenance-repo-v1-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-provenance-repo-nar.nar
+ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-provenance-repo-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-provenance-repo-nar.nar
 ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-core-service-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-core-service-nar.nar
+
+elif  [[ $ver == 1.2* ]] || [[ $ver == 1.3* ]] || [[ $ver == 1.4* ]] ;
+then
+   echo "Creating symlinks for NiFi version $ver compatible nars"
+  ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-provenance-repo-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-provenance-repo-nar.nar
+
+  ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-core-service-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-core-service-nar.nar
+  ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-standard-services-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-standard-services-nar.nar
+  ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-core-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-core-nar.nar
+  ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-spark-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-spark-nar.nar
+  ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-spark-service-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-spark-service-nar.nar
+  ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-hadoop-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-hadoop-nar.nar
+  ln -f -s $NIFI_KYLO_FOLDER/lib/kylo-nifi-hadoop-service-v1.2-nar-*.nar $HDF_NIFI_HOME_FOLDER/lib/kylo-nifi-hadoop-service-nar.nar
+
+fi
 
 ln -f -s $NIFI_KYLO_FOLDER/lib/app/kylo-spark-validate-cleanse-spark-v2-*-jar-with-dependencies.jar $HDF_NIFI_HOME_FOLDER/lib/app/kylo-spark-validate-cleanse-jar-with-dependencies.jar
 ln -f -s $NIFI_KYLO_FOLDER/lib/app/kylo-spark-job-profiler-spark-v2-*-jar-with-dependencies.jar $HDF_NIFI_HOME_FOLDER/lib/app/kylo-spark-job-profiler-jar-with-dependencies.jar
 ln -f -s $NIFI_KYLO_FOLDER/lib/app/kylo-spark-interpreter-spark-v2-*-jar-with-dependencies.jar $HDF_NIFI_HOME_FOLDER/lib/app/kylo-spark-interpreter-jar-with-dependencies.jar
+
 
 echo "Updating permissions for the nifi sym links"
 chown -h nifi:nifi $HDF_NIFI_HOME_FOLDER/lib/kylo*.nar
