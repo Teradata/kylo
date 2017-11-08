5f557273946293d011011989652d7917eb01c0c2
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2017-11-07 17:49:16 -0800

    fix feed cache filter

--- services/operational-metadata-service/operational-metadata-integration-service/src/main/java/com/thinkbiganalytics/metadata/cache/FeedHealthSummaryCache.java
+++ services/operational-metadata-service/operational-metadata-integration-service/src/main/java/com/thinkbiganalytics/metadata/cache/FeedHealthSummaryCache.java
@@ -279,7 +279,7 @@ public class FeedHealthSummaryCache implements TimeBasedCache<FeedSummary> {
     private Predicate<FeedSummary> filter(FeedSummaryFilter filter, RoleSetExposingSecurityExpressionRoot userContext) {
         return s -> {
             try {
-                return feedAclCache.hasAccess(userContext, s.getFeedId().toString()) && fixedFilter(s, filter) && (filter.containsFeed(s.getFeedName()) || filter
+                return feedAclCache.hasAccess(userContext, s.getFeedId().toString()) && fixedFilter(s, filter) && (filter.containsFeed(s.getFeedName()) && filter
                     .containsState(s.getRunStatus().name().toLowerCase()));
             } catch (Exception e) {
                 return false;
