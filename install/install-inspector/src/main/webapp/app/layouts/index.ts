25c273782ae887162594cc3caf4542fd22733d5c
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2018-01-18 15:04:56 +0000

    KYLO-1393 Java app to verify configuration, remove loading profiles

--- install/install-inspector/src/main/webapp/app/layouts/index.ts
+++ install/install-inspector/src/main/webapp/app/layouts/index.ts
@@ -5,6 +5,3 @@ export * from './footer/footer.component';
 export * from './navbar/navbar.component';
 export * from './navbar/navbar.route';
 export * from './navbar/active-menu.directive';
-export * from './profiles/page-ribbon.component';
-export * from './profiles/profile.service';
-export * from './profiles/profile-info.model';
