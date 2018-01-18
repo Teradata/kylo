04637a43ce3d53337469caca92899eb6adcc4060
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2018-01-18 16:04:04 +0000

    KYLO-1393 Java app to verify configuration, remove redundant functionality

--- install/install-inspector/src/main/webapp/app/app.module.ts
+++ install/install-inspector/src/main/webapp/app/app.module.ts
@@ -4,12 +4,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Ng2Webstorage } from 'ngx-webstorage';
 
-import { InspectorSharedModule, UserRouteAccessService } from './shared';
+import { InspectorSharedModule } from './shared';
 import { InspectorAppRoutingModule} from './app-routing.module';
 import { InspectorHomeModule } from './home/home.module';
-import { InspectorAdminModule } from './admin/admin.module';
-import { InspectorAccountModule } from './account/account.module';
-import { InspectorEntityModule } from './entities/entity.module';
 import { customHttpProvider } from './blocks/interceptor/http.provider';
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
 import {MaterialModule} from './material.module';
@@ -34,9 +31,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
         Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
         InspectorSharedModule,
         InspectorHomeModule,
-        InspectorAdminModule,
-        InspectorAccountModule,
-        InspectorEntityModule,
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
     declarations: [
@@ -49,7 +43,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     providers: [
         customHttpProvider(),
         PaginationConfig,
-        UserRouteAccessService
     ],
     bootstrap: [ JhiMainComponent ]
 })
