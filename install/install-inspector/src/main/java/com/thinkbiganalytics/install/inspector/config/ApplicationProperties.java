5b613989a04ce1c1772dee48b0e3adc5a4406ca1
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2018-01-19 15:41:24 +0000

    KYLO-1393 Java app to verify configuration, revert to standard licence, disable trailing space check in comments for .ts files

--- install/install-inspector/src/main/java/com/thinkbiganalytics/install/inspector/config/ApplicationProperties.java
+++ install/install-inspector/src/main/java/com/thinkbiganalytics/install/inspector/config/ApplicationProperties.java
@@ -6,28 +6,23 @@ package com.thinkbiganalytics.install.inspector.config;
  * %%
  * Copyright (C) 2017 - 2018 ThinkBig Analytics
  * %%
- * %% Licensed under the Apache License, Version 2.0 (the "License");
- * %% you may not use this file except in compliance with the License.
- * %% You may obtain a copy of the License at
- * %%
- * %%     http://www.apache.org/licenses/LICENSE-2.0
- * %%
- * %% Unless required by applicable law or agreed to in writing, software
- * %% distributed under the License is distributed on an "AS IS" BASIS,
- * %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * %% See the License for the specific language governing permissions and
- * %% limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * #L%
  */
 
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-/**
- * Properties specific to Inspector.
- * <p>
- * Properties are configured in the application.yml file.
- * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
- */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
 
