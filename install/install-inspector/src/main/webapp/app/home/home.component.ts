4ac628239cac3a21846e54b2486d56a915315f11
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2018-01-22 15:14:34 +0000

    KYLO-1393 Java app to verify configuration, fix security config, add download button

--- install/install-inspector/src/main/webapp/app/home/home.component.ts
+++ install/install-inspector/src/main/webapp/app/home/home.component.ts
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,6 +36,7 @@ export class HomeComponent implements OnInit {
     selectedCheckId = -1;
     path = new FormControl('', [Validators.required]);
     devMode = new FormControl(false, [Validators.required]);
+    selectAll = new FormControl(false, [Validators.required]);
 
     constructor(private configService: ConfigService) {
     }
@@ -91,6 +92,10 @@ export class HomeComponent implements OnInit {
             });
     }
 
+    downloadReport() {
+        console.log('download report');
+    }
+
     loadChecks() {
         console.log('loading checks');
         return this.configService.loadChecks();
@@ -125,5 +130,11 @@ export class HomeComponent implements OnInit {
 
     getErrorMessage() {
         return this.path.hasError('required') ? 'You must enter a value' : '';
+    }
+
+    onSelectAllChange() {
+        if (this.checks) {
+            this.checks.forEach((check) => check.enabled.setValue(this.selectAll.value));
+        }
     }
 }
