636ffecc8594bf8ef342d1cd8c537efe3795fbfb
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-01-31 12:03:57 +0500

    Conversion of (module-name.ts + module.ts) under Admin

--- ui/ui-app/src/main/resources/static/js/admin/module-name.js
+++ ui/ui-app/src/main/resources/static/js/admin/module-name.js
@@ -1,3 +1,6 @@
-define([], function () {
-    return 'kylo.admin';
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.moduleName = "kylo.admin";
 });
+//# sourceMappingURL=module-name.js.map
--- ui/ui-app/src/main/resources/static/js/admin/module.js
+++ ui/ui-app/src/main/resources/static/js/admin/module.js
@@ -1,65 +1,68 @@
-define(['angular', 'admin/module-name','kylo-utils/LazyLoadUtil','constants/AccessConstants','codemirror-require/module','kylo-common', 'kylo-services','jquery'], function (angular,moduleName,lazyLoadUtil,AccessConstants) {
-    var module = angular.module(moduleName, []);
-
-    /**
-     * LAZY loaded in from /app.js
-     */
-    module.config(['$stateProvider',function ($stateProvider) {
-        $stateProvider.state('jcr-query',{
-            url:'/admin/jcr-query',
-            views: {
-                'content': {
-                    templateUrl: 'js/admin/jcr/jcr-query.html',
-                    controller:"JcrQueryController",
-                    controllerAs:"vm"
-                }
-            },
-            resolve: {
-                loadMyCtrl: lazyLoadController(['admin/jcr/JcrQueryController'])
-            },
-            data:{
-                breadcrumbRoot:false,
-                displayName:'JCR Admin',
-                module:moduleName,
-                permissions:AccessConstants.UI_STATES.JCR_ADMIN.permissions
-            }
-        });
-
-        $stateProvider.state('cluster',{
-            url:'/admin/cluster',
-            views: {
-                'content': {
-                    templateUrl: 'js/admin/cluster/cluster-test.html',
-                    controller:"ClusterController",
-                    controllerAs:"vm"
-                }
-            },
-            resolve: {
-                loadMyCtrl: lazyLoadController(['admin/cluster/ClusterController'])
-            },
-            data:{
-                breadcrumbRoot:false,
-                displayName:'Kylo Cluster',
-                module:moduleName,
-                permissions:[]
-            }
-        })
-
-        function lazyLoadController(path){
-            return lazyLoadUtil.lazyLoadController(path,'admin/module-require');
+define(["require", "exports", "angular", "./module-name"], function (require, exports, angular, module_name_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var AccessConstants = require('../constants/AccessConstants');
+    var lazyLoadUtil = require('../kylo-utils/LazyLoadUtil');
+    var codeMirrorRequire = require('../codemirror-require/module');
+    var ModuleFactory = /** @class */ (function () {
+        function ModuleFactory() {
+            this.module = angular.module(module_name_1.moduleName, []);
+            this.module.config(['$stateProvider', this.configFn.bind(this)]);
+            this.module.run(['$ocLazyLoad', this.runFn.bind(this)]);
         }
-
-    }]);
-
-
-    module.run(['$ocLazyLoad', function ($ocLazyLoad) {
-        $ocLazyLoad.load({
-            name: 'kylo', files: ['bower_components/angular-ui-grid/ui-grid.css', 'assets/ui-grid-material.css'
-            ], serie: true
-        })
-    }]);
-    return module;
+        ModuleFactory.prototype.configFn = function ($stateProvider) {
+            $stateProvider.state('jcr-query', {
+                url: '/admin/jcr-query',
+                views: {
+                    'content': {
+                        templateUrl: 'js/admin/jcr/jcr-query.html',
+                        controller: "JcrQueryController",
+                        controllerAs: "vm"
+                    }
+                },
+                resolve: {
+                    loadMyCtrl: this.lazyLoadController(['admin/jcr/JcrQueryController'])
+                },
+                data: {
+                    breadcrumbRoot: false,
+                    displayName: 'JCR Admin',
+                    module: module_name_1.moduleName,
+                    permissions: AccessConstants.UI_STATES.JCR_ADMIN.permissions
+                }
+            });
+            $stateProvider.state('cluster', {
+                url: '/admin/cluster',
+                views: {
+                    'content': {
+                        templateUrl: 'js/admin/cluster/cluster-test.html',
+                        controller: "ClusterController",
+                        controllerAs: "vm"
+                    }
+                },
+                resolve: {
+                    loadMyCtrl: this.lazyLoadController(['admin/cluster/ClusterController'])
+                },
+                data: {
+                    breadcrumbRoot: false,
+                    displayName: 'Kylo Cluster',
+                    module: module_name_1.moduleName,
+                    permissions: []
+                }
+            });
+        };
+        ModuleFactory.prototype.runFn = function ($ocLazyLoad) {
+            $ocLazyLoad.load({
+                name: 'kylo',
+                files: ['bower_components/angular-ui-grid/ui-grid.css', 'assets/ui-grid-material.css'],
+                serie: true
+            });
+        };
+        ModuleFactory.prototype.lazyLoadController = function (path) {
+            return lazyLoadUtil.lazyLoadController(path, "admin/module-require");
+        };
+        return ModuleFactory;
+    }());
+    var module = new ModuleFactory();
+    exports.default = module;
 });
-
-
-
+//# sourceMappingURL=module.js.map
