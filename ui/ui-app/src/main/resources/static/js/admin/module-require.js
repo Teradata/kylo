2ea096adb8c4d792416b111f04c59cbaa3d202ec
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-01-31 17:07:01 +0500

    module-require.js->ts (Admin)

--- ui/ui-app/src/main/resources/static/js/admin/module-require.js
+++ ui/ui-app/src/main/resources/static/js/admin/module-require.js
@@ -1,9 +1,9 @@
-define(['angular','kylo-services','kylo-common',
-        'admin/jcr/JcrQueryController',
-'admin/cluster/ClusterController'],function() {
+define(["require", "exports", "../services/services.module", "../common/common.module", "./jcr/JcrQueryController", "./cluster/ClusterController"], function (require, exports, services_module_1, common_module_1, JcrQueryController_1, ClusterController_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.KyloServicesModule = services_module_1.KyloServicesModule;
+    exports.KyloCommonModule = common_module_1.KyloCommonModule;
+    exports.JcrQueryController = JcrQueryController_1.JcrQueryController;
+    exports.ClusterController = ClusterController_1.ClusterController;
 });
-
-
-
-
-
+//# sourceMappingURL=module-require.js.map
