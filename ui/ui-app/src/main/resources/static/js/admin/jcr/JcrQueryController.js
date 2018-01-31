efa9c1b2ace112889e23b97f7026a2c10a8ec857
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-01-31 12:05:24 +0500

    JcrQueryController.js->.ts Conversion (Admin/jcr)

--- ui/ui-app/src/main/resources/static/js/admin/jcr/JcrQueryController.js
+++ ui/ui-app/src/main/resources/static/js/admin/jcr/JcrQueryController.js
@@ -1,103 +1,94 @@
-define(["angular", "admin/module-name"], function (angular, moduleName) {
-
-    var JcrQueryController = function ($scope, $http, $mdDialog, $mdToast,AccessControlService) {
-        var self = this;
-
-        var init = function() {
-            self.sql = 'SELECT fs.[jcr:title], fd.[tba:state], c.[tba:systemName] \n'
-                       + 'FROM [tba:feed] as e \n'
-                       + 'JOIN [tba:feedSummary] AS fs ON ISCHILDNODE(fs, e) \n'
-                       + 'JOIN [tba:feedData] AS fd ON ISCHILDNODE(fd, e) \n'
-                       +'JOIN [tba:categoryDetails] AS cd ON ISCHILDNODE(e, cd) \n'
-                       + 'JOIN [tba:category] as c on ISCHILDNODE(cd,c)';
-
-            getIndexes();
-
-        };
-        this.loading = false;
-        self.errorMessage = null;
-        this.queryTime = null;
-
-
+define(["require", "exports", "angular", "../module-name", "underscore"], function (require, exports, angular, module_name_1, _) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var JcrQueryController = /** @class */ (function () {
+        function JcrQueryController($scope, $http, $mdDialog, $mdToast, AccessControlService) {
+            this.$scope = $scope;
+            this.$http = $http;
+            this.$mdDialog = $mdDialog;
+            this.$mdToast = $mdToast;
+            this.AccessControlService = AccessControlService;
+            // sql: string;
+            this.sql = "";
+            this.loading = false;
+            this.errorMessage = null;
+            this.queryTime = null;
+            this.indexesErrorMessage = "";
             this.codemirrorOptions = {
-            lineWrapping: true,
-            indentWithTabs: true,
-            smartIndent: true,
-            lineNumbers: true,
-            matchBrackets: true,
-            autofocus: true,
-            mode: 'text/x-hive'
-        };
-
-        this.gridOptions = {
-            columnDefs: [],
-            data: null,
-            enableColumnResizing: true,
-            enableGridMenu: true,
-            flatEntityAccess: true
-        };
-
-        self.resultSize = 0;
-
-        self.indexes = [];
-
-
-        self.index = {
-            indexName:'',
-            nodeType:'',
-            propertyName:'',
-            propertyType:''
-        };
-
-        self.indexTable = {
-            currentPage:1,
-            rowsPerPage:5
+                lineWrapping: true,
+                indentWithTabs: true,
+                smartIndent: true,
+                lineNumbers: true,
+                matchBrackets: true,
+                autofocus: true,
+                mode: 'text/x-hive'
+            };
+            this.gridOptions = {
+                columnDefs: [],
+                data: null,
+                enableColumnResizing: true,
+                enableGridMenu: true,
+                flatEntityAccess: true
+            };
+            this.resultSize = 0;
+            this.indexes = [];
+            this.index = {
+                indexName: '',
+                nodeType: '',
+                propertyName: '',
+                propertyType: ''
+            };
+            this.indexTable = {
+                currentPage: 1,
+                rowsPerPage: 5
+            };
+            this.previousQueries = [];
+            this.previousQuery = '';
+            this.explainPlan = null;
+            this.propertyTypes = [{ name: "String", value: 1 },
+                { name: "Binary", value: 2 },
+                { name: "Long", value: 3 },
+                { name: "Double", value: 4 },
+                { name: "Date", value: 5 },
+                { name: "Boolean", value: 6 },
+                { name: "Name", value: 7 },
+                { name: "Path", value: 8 },
+                { name: "Reference", value: 9 },
+                { name: "Weak Referebce", value: 10 },
+                { name: "URI", value: 11 },
+                { name: "Decimal", value: 12 }];
+            this.indexKinds = ["VALUE", "ENUMERATED_VALUE", "UNIQUE_VALUE", "TEXT", "NODE_TYPE"];
+            this.ngOnInit();
         }
-
-        self.previousQueries = [];
-        self.previousQuery = '';
-
-        self.explainPlan = null;
-
-
-        this.executeQuery = function(){
-            query();
-        }
-
-        this.propertyTypes = [{name:"String",value:1},
-        {name:"Binary",value:2},
-        {name:"Long",value:3},
-        {name:"Double",value:4},
-        {name:"Date",value:5},
-        {name:"Boolean",value:6},
-        {name:"Name",value:7},
-        {name:"Path",value:8},
-        {name:"Reference",value:9},
-        {name:"Weak Referebce",value:10},
-        {name:"URI",value:11},
-        {name:"Decimal",value:12}]
-
-        this.indexKinds = ["VALUE","ENUMERATED_VALUE","UNIQUE_VALUE","TEXT","NODE_TYPE"]
-
-        this.registerIndex = function(){
-            showDialog("Adding Index", "Adding index. Please wait...");
+        JcrQueryController.prototype.ngOnInit = function () {
+            this.sql = 'SELECT fs.[jcr:title], fd.[tba:state], c.[tba:systemName] \n'
+                + 'FROM [tba:feed] as e \n'
+                + 'JOIN [tba:feedSummary] AS fs ON ISCHILDNODE(fs, e) \n'
+                + 'JOIN [tba:feedData] AS fd ON ISCHILDNODE(fd, e) \n'
+                + 'JOIN [tba:categoryDetails] AS cd ON ISCHILDNODE(e, cd) \n'
+                + 'JOIN [tba:category] as c on ISCHILDNODE(cd,c)';
+            this.getIndexes();
+        };
+        JcrQueryController.prototype.executeQuery = function () {
+            this.query();
+        };
+        JcrQueryController.prototype.registerIndex = function () {
+            var _this = this;
+            this.showDialog("Adding Index", "Adding index. Please wait...");
             var successFn = function (response) {
                 if (response.data) {
-                    hideDialog();
-                    getIndexes();
-                    $mdToast.show(
-                        $mdToast.simple()
-                            .textContent('Added the index')
-                            .hideDelay(3000)
-                    );
+                    _this.hideDialog();
+                    _this.getIndexes();
+                    _this.$mdToast.show(_this.$mdToast.simple()
+                        .textContent('Added the index')
+                        .hideDelay(3000));
                 }
-            }
+            };
             var errorFn = function (err) {
-                hideDialog();
-            }
-
-            var indexCopy = angular.extend({},self.index);
-            var promise = $http({
+                _this.hideDialog();
+            };
+            var indexCopy = angular.extend({}, this.index);
+            var promise = this.$http({
                 url: "/proxy/v1/metadata/debug/jcr-index/register",
                 method: "POST",
                 data: angular.toJson(indexCopy),
@@ -105,26 +96,21 @@ define(["angular", "admin/module-name"], function (angular, moduleName) {
                     'Content-Type': 'application/json; charset=UTF-8'
                 }
             }).then(successFn, errorFn);
-        }
-
-        this.unregisterIndex = function(indexName){
-
-            if(angular.isDefined(indexName)) {
+        };
+        JcrQueryController.prototype.unregisterIndex = function (indexName) {
+            var _this = this;
+            if (angular.isDefined(indexName)) {
                 var successFn = function (response) {
                     if (response.data) {
-                        getIndexes();
-                        $mdToast.show(
-                            $mdToast.simple()
-                                .textContent('Removed the index '+indexName)
-                                .hideDelay(3000)
-                        );
+                        _this.getIndexes();
+                        _this.$mdToast.show(_this.$mdToast.simple()
+                            .textContent('Removed the index ' + indexName)
+                            .hideDelay(3000));
                     }
-                }
+                };
                 var errorFn = function (err) {
-
-                }
-
-                var promise = $http({
+                };
+                var promise = this.$http({
                     url: "/proxy/v1/metadata/debug/jcr-index/" + indexName + "/unregister",
                     method: "POST",
                     headers: {
@@ -132,142 +118,118 @@ define(["angular", "admin/module-name"], function (angular, moduleName) {
                     }
                 }).then(successFn, errorFn);
             }
-        }
-
-        this.changePreviousQuery = function(){
-            self.sql = self.previousQuery;
-        }
-
-        function showDialog(title,message){
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .parent(angular.element(document.body))
-                    .clickOutsideToClose(false)
-                    .title(title)
-                    .textContent(message)
-                    .ariaLabel(title)
-            );
-        }
-
-        function hideDialog(){
-            $mdDialog.hide();
-        }
-
-        this.reindex = function(){
-                showDialog("Reindexing", "Reindexing. Please wait...");
-
-
-
-                var successFn = function (response) {
-                    hideDialog();
-                    if (response.data) {
-                        $mdToast.show(
-                            $mdToast.simple()
-                                .textContent('Successfully reindexed')
-                                .hideDelay(3000)
-                        );
-                    }
+        };
+        JcrQueryController.prototype.changePreviousQuery = function () {
+            this.sql = this.previousQuery;
+        };
+        JcrQueryController.prototype.showDialog = function (title, message) {
+            this.$mdDialog.show(this.$mdDialog.alert()
+                .parent(angular.element(document.body))
+                .clickOutsideToClose(false)
+                .title(title)
+                .textContent(message)
+                .ariaLabel(title));
+        };
+        JcrQueryController.prototype.hideDialog = function () {
+            this.$mdDialog.hide();
+        };
+        JcrQueryController.prototype.reindex = function () {
+            var _this = this;
+            this.showDialog("Reindexing", "Reindexing. Please wait...");
+            var successFn = function (response) {
+                _this.hideDialog();
+                if (response.data) {
+                    _this.$mdToast.show(_this.$mdToast.simple()
+                        .textContent('Successfully reindexed')
+                        .hideDelay(3000));
                 }
-                var errorFn = function (err) {
-                    hideDialog();
-                    $mdToast.show(
-                        $mdToast.simple()
-                            .textContent('Error reindexing ')
-                            .hideDelay(3000)
-                    );
-                }
-
-                var promise = $http({
-                    url: "/proxy/v1/metadata/debug/jcr-index/reindex",
-                    method: "POST",
-                    headers: {
-                        'Content-Type': 'application/json; charset=UTF-8'
-                    }
-                }).then(successFn, errorFn);
-        }
-
-        function query() {
-            self.loading = true;
-            self.errorMessage = null;
-            self.explainPlan = null;
-            var sql = self.sql;
-            var successFn = function(response) {
-                if(_.indexOf(self.previousQueries,sql) == -1) {
-                    self.previousQueries.push(sql);
-                }
-                self.loading = false;
-                transformResults(response.data);
             };
-            var errorFn = function(err) {
-                self.resultSize = 0;
-                self.loading = false;
-                if(err && err.data && err.data.developerMessage){
-                    self.errorMessage = err.data.developerMessage;
+            var errorFn = function (err) {
+                _this.hideDialog();
+                _this.$mdToast.show(_this.$mdToast.simple()
+                    .textContent('Error reindexing ')
+                    .hideDelay(3000));
+            };
+            var promise = this.$http({
+                url: "/proxy/v1/metadata/debug/jcr-index/reindex",
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
+            }).then(successFn, errorFn);
+        };
+        JcrQueryController.prototype.query = function () {
+            var _this = this;
+            this.loading = true;
+            this.errorMessage = null;
+            this.explainPlan = null;
+            var sql = this.sql;
+            var successFn = function (response) {
+                if (_.indexOf(_this.previousQueries, sql) == -1) {
+                    _this.previousQueries.push(sql);
+                }
+                _this.loading = false;
+                _this.transformResults(response.data);
+            };
+            var errorFn = function (err) {
+                _this.resultSize = 0;
+                _this.loading = false;
+                if (err && err.data && err.data.developerMessage) {
+                    _this.errorMessage = err.data.developerMessage;
                 }
                 else {
-                    self.errorMessage = 'Error performing query ';
+                    _this.errorMessage = 'Error performing query ';
                 }
-
             };
-            var promise = $http.get('/proxy/v1/metadata/debug/jcr-sql',{params:{query:self.sql}});
+            var promise = this.$http.get('/proxy/v1/metadata/debug/jcr-sql', { params: { query: sql } });
             promise.then(successFn, errorFn);
             return promise;
-        }
-
-        function getIndexes(){
-            var successFn = function(response) {
-                self.indexes = response.data;
+        };
+        JcrQueryController.prototype.getIndexes = function () {
+            var _this = this;
+            var successFn = function (response) {
+                _this.indexes = response.data;
             };
-            var errorFn = function(err) {
-                self.indexes = [];
-                self.indexesErrorMessage = 'Error getting indexes '+err
+            var errorFn = function (err) {
+                _this.indexes = [];
+                _this.indexesErrorMessage = 'Error getting indexes ' + err;
             };
-            var promise = $http.get('/proxy/v1/metadata/debug/jcr-index');
+            var promise = this.$http.get('/proxy/v1/metadata/debug/jcr-index');
             promise.then(successFn, errorFn);
             return promise;
-        }
-
-
-        function transformResults(result) {
-
+        };
+        JcrQueryController.prototype.transformResults = function (result) {
             var data = {};
             var rows = [];
             var columns = [];
-           self.queryTime = result.queryTime;
-           self.explainPlan = result.explainPlan;
-
-
-            angular.forEach(result.columns,function(col,i){
-                        columns.push({
-                            displayName: col.name,
-                            headerTooltip: col.name,
-                            minWidth: 150,
-                            name: 'col_'+i
-                        });
+            this.queryTime = result.queryTime;
+            this.explainPlan = result.explainPlan;
+            angular.forEach(result.columns, function (col, i) {
+                columns.push({
+                    displayName: col.name,
+                    headerTooltip: col.name,
+                    minWidth: 150,
+                    name: 'col_' + i
+                });
             });
-
-            angular.forEach(result.rows,function(row,i){
-
-              var rowObj = {}
-               _.each(row.columnValues,function(col,i){
-                   rowObj['col_'+i] = col.value;
-               });
-              rows.push(rowObj);
+            angular.forEach(result.rows, function (row, i) {
+                var rowObj = {};
+                _.each(row.columnValues, function (col, i) {
+                    rowObj['col_' + i] = col.value;
+                });
+                rows.push(rowObj);
             });
-
             data.columns = columns;
             data.rows = rows;
-            self.gridOptions.columnDefs = columns;
-            self.gridOptions.data = rows;
-            self.resultSize = data.rows.length;
+            this.gridOptions.columnDefs = columns;
+            this.gridOptions.data = rows;
+            this.resultSize = data.rows.length;
             return data;
         };
-
-
-        init();
-
-
-    };
-
-    angular.module(moduleName).controller("JcrQueryController", ["$scope", "$http","$mdDialog", "$mdToast","AccessControlService",JcrQueryController]);
+        ;
+        return JcrQueryController;
+    }());
+    exports.JcrQueryController = JcrQueryController;
+    angular.module(module_name_1.moduleName).controller("JcrQueryController", ["$scope", "$http", "$mdDialog", "$mdToast", "AccessControlService", JcrQueryController]);
 });
+//# sourceMappingURL=JcrQueryController.js.map
