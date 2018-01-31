0284350655334a1569bde9f8c73ae4ab21a1e4a3
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-01-31 12:04:35 +0500

    ClusterController.js->ts Conversion

--- ui/ui-app/src/main/resources/static/js/admin/cluster/ClusterController.js
+++ ui/ui-app/src/main/resources/static/js/admin/cluster/ClusterController.js
@@ -1,101 +1,91 @@
-define(["angular", "admin/module-name"], function (angular, moduleName) {
-
-    var ClusterController = function ($scope, $http, $mdDialog, $mdToast,$interval,AccessControlService) {
-        var self = this;
-
-
-        this.simpleMessage;
-
-        this.latestSimpleMessage = {}
-        this.sentMessages = [];
-        this.receivedMessages = [];
-        this.receivedMessageIds = [];
-        self.messageCheckerInterval = null;
-
-        self.members = [];
-        self.isClustered = false;
-
-
-        this.sendMessage = function(){
-            var simpleMessage = self.simpleMessage;
+define(["require", "exports", "angular", "../module-name", "underscore"], function (require, exports, angular, module_name_1, _) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    //const moduleName = require('../module-name');
+    var ClusterController = /** @class */ (function () {
+        function ClusterController($scope, $http, $mdDialog, $mdToast, $interval, AccessControlService) {
+            var _this = this;
+            this.$scope = $scope;
+            this.$http = $http;
+            this.$mdDialog = $mdDialog;
+            this.$mdToast = $mdToast;
+            this.$interval = $interval;
+            this.AccessControlService = AccessControlService;
+            this.latestSimpleMessage = {};
+            this.sentMessages = [];
+            this.receivedMessages = [];
+            this.receivedMessageIds = [];
+            this.messageCheckerInterval = null;
+            this.members = [];
+            this.isClustered = false;
+            this.getMembers = function () {
+                _this.$http.get("/proxy/v1/admin/cluster/members").then(function (response) {
+                    if (response.data) {
+                        _this.members = response.data;
+                    }
+                });
+            };
+            this.ngOnInit();
+        }
+        ClusterController.prototype.sendMessage = function () {
+            var _this = this;
+            var simpleMessage = this.simpleMessage;
             var successFn = function (response) {
                 if (response.data && response.data.status == 'success') {
-                    $mdToast.show(
-                        $mdToast.simple()
-                            .textContent('Sent the message')
-                            .hideDelay(3000)
-                    );
-                    self.sentMessages.push(simpleMessage);
+                    _this.$mdToast.show(_this.$mdToast.simple()
+                        .textContent('Sent the message')
+                        .hideDelay(3000));
+                    _this.sentMessages.push(simpleMessage);
                 }
-            }
+            };
             var errorFn = function (err) {
-                $mdToast.show(
-                    $mdToast.simple()
-                        .textContent('Error sending the message')
-                        .hideDelay(3000)
-                );
-            }
-
-            var promise = $http({
+                _this.$mdToast.show(_this.$mdToast.simple()
+                    .textContent('Error sending the message')
+                    .hideDelay(3000));
+            };
+            var promise = this.$http({
                 url: "/proxy/v1/admin/cluster/simple",
                 method: "POST",
-                data: self.simpleMessage
+                data: this.simpleMessage
             }).then(successFn, errorFn);
-        }
-
-        function messageChecker() {
-            $http.get("/proxy/v1/admin/cluster/simple").then(function(response){
-                if(response.data){
-                    self.latestSimpleMessage = response.data;
-                    if(response.data.type != "NULL" && _.indexOf(self.receivedMessageIds,response.data.id) < 0){
-                        self.receivedMessages.push(response.data);
-                        self.receivedMessageIds.push(response.data.id);
+        };
+        ClusterController.prototype.messageChecker = function () {
+            var _this = this;
+            this.$http.get("/proxy/v1/admin/cluster/simple").then(function (response) {
+                if (response.data) {
+                    _this.latestSimpleMessage = response.data;
+                    if (response.data.type != "NULL" && _.indexOf(_this.receivedMessageIds, response.data.id) < 0) {
+                        _this.receivedMessages.push(response.data);
+                        _this.receivedMessageIds.push(response.data.id);
                     }
                 }
             });
-        }
-
-        self.getMembers = function() {
-            $http.get("/proxy/v1/admin/cluster/members").then(function(response){
-                if(response.data){
-                   self.members = response.data;
-                }
-            });
-        }
-
-        function isClustered() {
-            $http.get("/proxy/v1/admin/cluster/is-clustered").then(function(response){
-                if(response.data && response.data.status == 'success'){
-                    self.isClustered = true;
+        };
+        ClusterController.prototype.setIsClustered = function () {
+            var _this = this;
+            this.$http.get("/proxy/v1/admin/cluster/is-clustered").then(function (response) {
+                if (response.data && response.data.status == 'success') {
+                    _this.isClustered = true;
                 }
                 else {
-                    self.isClustered = false;
+                    _this.isClustered = false;
                 }
             });
-        }
-
-        function startMessageChecker(){
-         self.messageCheckerInterval =   $interval(function(){
-                messageChecker();
-            },2000);
-        }
-
-        $scope.$on('$destroy', function () {
-            if(self.messageCheckerInterval != null) {
-                $interval.cancel(self.messageCheckerInterval);
-            }
-        });
-
-        function init() {
-            startMessageChecker();
-            isClustered();
-            self.getMembers();
-        }
-
-        init();
-
-
-    };
-
-    angular.module(moduleName).controller("ClusterController", ["$scope", "$http","$mdDialog", "$mdToast","$interval","AccessControlService",ClusterController]);
+        };
+        ClusterController.prototype.startMessageChecker = function () {
+            var _this = this;
+            this.messageCheckerInterval = this.$interval(function () {
+                _this.messageChecker();
+            }, 2000);
+        };
+        ClusterController.prototype.ngOnInit = function () {
+            this.startMessageChecker();
+            this.setIsClustered();
+            this.getMembers();
+        };
+        return ClusterController;
+    }());
+    exports.ClusterController = ClusterController;
+    angular.module(module_name_1.moduleName).controller("ClusterController", ["$scope", "$http", "$mdDialog", "$mdToast", "$interval", "AccessControlService", ClusterController]);
 });
+//# sourceMappingURL=ClusterController.js.map
