6cb463ac1cd495bd0f477a964610b7f88c931c1f
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2018-02-01 08:52:59 -0800

    KYLO-1399 - supporting code for this jira.  remove unused class

--- integrations/nifi/nifi-nar-bundles/nifi-core-bundle/nifi-savepoint-processor-v1.2/src/test/java/com/thinkbiganalytics/nifi/v2/savepoint/MockSpringContextService.java
+++
@@ -1,8 +0,0 @@
-package com.thinkbiganalytics.nifi.v2.savepoint;
-
-import com.thinkbiganalytics.nifi.core.api.cleanup.CleanupEventConsumer;
-import com.thinkbiganalytics.nifi.core.api.spring.SpringContextService;
-
-import org.apache.nifi.controller.AbstractControllerService;
-import org.springframework.beans.BeansException;
-
