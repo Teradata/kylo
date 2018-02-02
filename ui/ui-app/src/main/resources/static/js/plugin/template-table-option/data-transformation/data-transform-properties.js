6c174eb86045a436e80ca408ca9eda2e5650d3b5
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-02-02 10:53:13 +0500

    data-transform-properties.js->.ts
    Slight Fixing

--- ui/ui-app/src/main/resources/static/js/plugin/template-table-option/data-transformation/data-transform-properties.js
+++ ui/ui-app/src/main/resources/static/js/plugin/template-table-option/data-transformation/data-transform-properties.js
@@ -1,13 +1,3 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 define(["require", "exports", "angular"], function (require, exports, angular) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
@@ -45,33 +35,20 @@ define(["require", "exports", "angular"], function (require, exports, angular) {
             /**
              * Navigates to the Edit Feed page for the current feed.
              */
-            navigateToEditFeedInStepper = function () { return function () {
-                _this.VisualQueryService.resetModel();
-                _this.StateService.FeedManager().Feed().navigateToEditFeedInStepper(_this.model.feedId);
-            }; };
+            this.navigateToEditFeedInStepper();
             //Apply the entity access permissions
             this.$q.when(this.AccessControlService.hasPermission(this.AccessControlService.FEEDS_EDIT, this.model, this.AccessControlService.ENTITY_ACCESS.FEED.EDIT_FEED_DETAILS))
                 .then(function (access) {
                 _this.allowEdit = access;
             });
         }
+        DataTransformPropertiesController.prototype.navigateToEditFeedInStepper = function () {
+            this.VisualQueryService.resetModel();
+            this.StateService.FeedManager().Feed().navigateToEditFeedInStepper(this.model.feedId);
+        };
         return DataTransformPropertiesController;
     }());
     exports.DataTransformPropertiesController = DataTransformPropertiesController;
-    var kyloDataTransformProperties = /** @class */ (function (_super) {
-        __extends(kyloDataTransformProperties, _super);
-        function kyloDataTransformProperties() {
-            var _this = this;
-            return {
-                controller: "DataTransformPropertiesController",
-                controllerAs: "vm",
-                restrict: "E",
-                templateUrl: "js/plugin/template-table-option/data-transformation/data-transform-properties.html"
-            };
-        }
-        return kyloDataTransformProperties;
-    }(ng.IDirective));
-    exports.kyloDataTransformProperties = kyloDataTransformProperties;
     angular.module("kylo.plugin.template-table-option.data-transformation", [])
         .controller('DataTransformPropertiesController', ["$scope",
         "$q",
@@ -80,6 +57,14 @@ define(["require", "exports", "angular"], function (require, exports, angular) {
         "StateService",
         "VisualQueryService",
         DataTransformPropertiesController])
-        .directive('kyloDataTransformProperties', kyloDataTransformProperties);
+        .directive('kyloDataTransformProperties', [function () {
+            return {
+                controller: "DataTransformPropertiesController",
+                controllerAs: "vm",
+                restrict: "E",
+                templateUrl: "js/plugin/template-table-option/data-transformation/data-transform-properties.html"
+            };
+        }
+    ]);
 });
 //# sourceMappingURL=data-transform-properties.js.map
--- ui/ui-app/src/main/resources/static/js/plugin/template-table-option/data-transformation/data-transform-properties.ts
+++ ui/ui-app/src/main/resources/static/js/plugin/template-table-option/data-transformation/data-transform-properties.ts
@@ -20,19 +20,21 @@ export class DataTransformPropertiesController implements ng.IComponentControlle
         /**
          * Navigates to the Edit Feed page for the current feed.
          */
-        navigateToEditFeedInStepper = => () {
-            this.VisualQueryService.resetModel();
-            this.StateService.FeedManager().Feed().navigateToEditFeedInStepper(this.model.feedId);
-        };
-
+        
+        this.navigateToEditFeedInStepper();
         //Apply the entity access permissions
         this.$q.when(this.AccessControlService.hasPermission(this.AccessControlService.FEEDS_EDIT, 
                                                              this.model, 
                                                              this.AccessControlService.ENTITY_ACCESS.FEED.EDIT_FEED_DETAILS))
-                                              .then((access)=>{
+                                              .then((access: any)=>{
                                                          this.allowEdit = access;
                                                         });
                  }
+
+        navigateToEditFeedInStepper(){
+            this.VisualQueryService.resetModel();
+            this.StateService.FeedManager().Feed().navigateToEditFeedInStepper(this.model.feedId);
+        }
 
         /**
          * Indicates if the feed NiFi properties may be edited.
@@ -53,17 +55,6 @@ export class DataTransformPropertiesController implements ng.IComponentControlle
         model: any = this.FeedService.editFeedModel;
 
 }
-export class kyloDataTransformProperties extends ng.IDirective
-{
-    constructor(){
-          return {
-            controller: "DataTransformPropertiesController",
-            controllerAs: "vm",
-            restrict: "E",
-            templateUrl: "js/plugin/template-table-option/data-transformation/data-transform-properties.html"
-        };
-        }
-}
 
   angular.module("kylo.plugin.template-table-option.data-transformation", [])
         .controller('DataTransformPropertiesController', 
@@ -74,4 +65,13 @@ export class kyloDataTransformProperties extends ng.IDirective
                     "StateService", 
                     "VisualQueryService", 
                     DataTransformPropertiesController])
-        .directive('kyloDataTransformProperties', kyloDataTransformProperties);
+        .directive('kyloDataTransformProperties', [()=>
+                               { return {
+                                    controller: "DataTransformPropertiesController",
+                                    controllerAs: "vm",
+                                    restrict: "E",
+                                    templateUrl: "js/plugin/template-table-option/data-transformation/data-transform-properties.html"
+                                };
+                                }
+        ]
+        );
