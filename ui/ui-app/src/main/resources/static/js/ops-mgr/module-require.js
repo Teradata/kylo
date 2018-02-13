cdd1e7e6a9b63d9d74aba765d48ea8cd42568aba
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-02-13 12:27:20 +0500

    ops-mgr/*.js- > .ts
    module.ts+ module-name.ts+ module-require.ts

--- ui/ui-app/src/main/resources/static/js/ops-mgr/module-require.js
+++ ui/ui-app/src/main/resources/static/js/ops-mgr/module-require.js
@@ -1,22 +1,5 @@
-/**
- * Include all common services/directives are are used for the kylo.opsmgr module
- */
-define(['angular',
-        'ops-mgr/services/AlertsServiceV2',
-        'ops-mgr/services/EventService',
-        'ops-mgr/services/OpsManagerFeedService',
-        'ops-mgr/services/ChartJobStatusService',
-        'ops-mgr/services/IconStatusService',
-        'ops-mgr/services/Nvd3ChartService',
-        'ops-mgr/services/OpsManagerJobService',
-        'ops-mgr/services/OpsManagerRestUrlService',
-        'ops-mgr/services/ProvenanceEventStatsService',
-        'ops-mgr/services/ServicesStatusService',
-        'ops-mgr/services/TabService',
-        'ops-mgr/services/OpsManagerDashboardService'
-       ],function() {
-
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
 });
-
-
-
+//# sourceMappingURL=module-require.js.map
