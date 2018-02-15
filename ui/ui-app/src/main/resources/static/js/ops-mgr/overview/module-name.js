bcae0918991dca126d007a3a81c14653a15cc2d8
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-02-15 17:11:38 +0500

    ops-mgr/overview/.js -> .ts
    OverviewController.ts + module.ts + module-name.ts + module-require.ts

--- ui/ui-app/src/main/resources/static/js/ops-mgr/overview/module-name.js
+++ ui/ui-app/src/main/resources/static/js/ops-mgr/overview/module-name.js
@@ -1,3 +1,6 @@
-define([], function () {
-    return "kylo.opsmgr.overview";
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.moduleName = "kylo.opsmgr.overview";
 });
+//# sourceMappingURL=module-name.js.map
---
+++ ui/ui-app/src/main/resources/static/js/ops-mgr/overview/module-require.ts
@@ -0,0 +1,9 @@
+import * as angular from 'angular';
+import {moduleName} from "./module-name";
+import OverviewController from "./OverviewController";
+import controller from "./alerts/AlertsDirective";
+import * as DataConfidenceIndicatorDirective from "./data-confidence-indicator/DataConfidenceIndicatorDirective";
+import FeedHealthTableCardController from "./feed-health/FeedHealthTableCardDirective";
+import * as FeedStatusIndicatorController from "./feed-status-indicator/FeedsStatusIndicatorDirective";
+import * as JobStatusIndicatorController from "./job-status-indicator/JobStatusIndicatorDirective";
+import * as ServicesIndicatorDirective from  "./services-indicator/ServicesIndicatorDirective";
