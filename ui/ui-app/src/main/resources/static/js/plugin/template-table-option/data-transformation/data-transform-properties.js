21291641c34f3ea868d42b1e065df083d5858ea7
Gregory Hart <greg.hart@thinkbiganalytics.com>
2018-02-15 12:09:31 -0800

    KYLO-1623 Fixed feed details page redirecting to edit feed page

--- ui/ui-app/src/main/resources/static/js/plugin/template-table-option/data-transformation/data-transform-properties.js
+++ ui/ui-app/src/main/resources/static/js/plugin/template-table-option/data-transformation/data-transform-properties.js
@@ -26,45 +26,36 @@ define(["require", "exports", "angular"], function (require, exports, angular) {
              */
             this.model = this.FeedService.editFeedModel;
             // Watch for model changes
-            this.$scope.$watch(function () { return FeedService.editFeedModel; }, function () {
+            this.$scope.$watch(function () {
+                return FeedService.editFeedModel;
+            }, function () {
                 //only update the model if it is not set yet
                 if (_this.model == null) {
                     _this.model = angular.copy(FeedService.editFeedModel);
                 }
             });
-            /**
-             * Navigates to the Edit Feed page for the current feed.
-             */
-            this.navigateToEditFeedInStepper();
             //Apply the entity access permissions
             this.$q.when(this.AccessControlService.hasPermission(this.AccessControlService.FEEDS_EDIT, this.model, this.AccessControlService.ENTITY_ACCESS.FEED.EDIT_FEED_DETAILS))
                 .then(function (access) {
                 _this.allowEdit = access;
             });
         }
+        /**
+         * Navigates to the Edit Feed page for the current feed.
+         */
         DataTransformPropertiesController.prototype.navigateToEditFeedInStepper = function () {
             this.VisualQueryService.resetModel();
             this.StateService.FeedManager().Feed().navigateToEditFeedInStepper(this.model.feedId);
         };
+        DataTransformPropertiesController.$inject = ["$scope", "$q", "AccessControlService", "FeedService", "StateService", "VisualQueryService"];
         return DataTransformPropertiesController;
     }());
     exports.DataTransformPropertiesController = DataTransformPropertiesController;
     angular.module("kylo.plugin.template-table-option.data-transformation", [])
-        .controller('DataTransformPropertiesController', ["$scope",
-        "$q",
-        "AccessControlService",
-        "FeedService",
-        "StateService",
-        "VisualQueryService",
-        DataTransformPropertiesController])
-        .directive('kyloDataTransformProperties', [function () {
-            return {
-                controller: "DataTransformPropertiesController",
-                controllerAs: "vm",
-                restrict: "E",
-                templateUrl: "js/plugin/template-table-option/data-transformation/data-transform-properties.html"
-            };
-        }
-    ]);
+        .component("kyloDataTransformProperties", {
+        controller: DataTransformPropertiesController,
+        controllerAs: "vm",
+        templateUrl: "js/plugin/template-table-option/data-transformation/data-transform-properties.html"
+    });
 });
 //# sourceMappingURL=data-transform-properties.js.map
--- ui/ui-app/src/main/resources/static/js/plugin/template-table-option/data-transformation/data-transform-properties.ts
+++ ui/ui-app/src/main/resources/static/js/plugin/template-table-option/data-transformation/data-transform-properties.ts
@@ -1,77 +1,58 @@
 import * as angular from 'angular';
 
-export class DataTransformPropertiesController implements ng.IComponentController{
+export class DataTransformPropertiesController implements ng.IComponentController {
 
-    constructor(private $scope: any,
-                private $q: any, 
-                private AccessControlService: any, 
-                private FeedService: any,
-                private StateService: any,
-                private VisualQueryService: any){
+    /**
+     * Indicates if the feed NiFi properties may be edited.
+     * @type {boolean}
+     */
+    allowEdit: boolean = false;
+
+    /**
+     * Indicates that the editable section is visible.
+     * @type {boolean}
+     */
+    editableSection: boolean = false;
+
+    /**
+     * Feed model.
+     * @type {Object}
+     */
+    model: any = this.FeedService.editFeedModel;
+
+    static readonly $inject = ["$scope", "$q", "AccessControlService", "FeedService", "StateService", "VisualQueryService"];
+
+    constructor(private $scope: any, private $q: any, private AccessControlService: any, private FeedService: any, private StateService: any, private VisualQueryService: any) {
         // Watch for model changes
-        this.$scope.$watch( ()=> { return FeedService.editFeedModel;},
-                            ()=>{
-                                //only update the model if it is not set yet
-                                if (this.model == null) {
-                                    this.model = angular.copy(FeedService.editFeedModel);
-                                }
-        });
+        this.$scope.$watch(() => {
+                return FeedService.editFeedModel;
+            },
+            () => {
+                //only update the model if it is not set yet
+                if (this.model == null) {
+                    this.model = angular.copy(FeedService.editFeedModel);
+                }
+            });
 
-        /**
-         * Navigates to the Edit Feed page for the current feed.
-         */
-        
-        this.navigateToEditFeedInStepper();
         //Apply the entity access permissions
-        this.$q.when(this.AccessControlService.hasPermission(this.AccessControlService.FEEDS_EDIT, 
-                                                             this.model, 
-                                                             this.AccessControlService.ENTITY_ACCESS.FEED.EDIT_FEED_DETAILS))
-                                              .then((access: any)=>{
-                                                         this.allowEdit = access;
-                                                        });
-                 }
+        this.$q.when(this.AccessControlService.hasPermission(this.AccessControlService.FEEDS_EDIT, this.model, this.AccessControlService.ENTITY_ACCESS.FEED.EDIT_FEED_DETAILS))
+            .then((access: any) => {
+                this.allowEdit = access;
+            });
+    }
 
-        navigateToEditFeedInStepper(){
-            this.VisualQueryService.resetModel();
-            this.StateService.FeedManager().Feed().navigateToEditFeedInStepper(this.model.feedId);
-        }
-
-        /**
-         * Indicates if the feed NiFi properties may be edited.
-         * @type {boolean}
-         */
-        allowEdit: boolean = false;
-
-        /**
-         * Indicates that the editable section is visible.
-         * @type {boolean}
-         */
-        editableSection: boolean = false;
-
-        /**
-         * Feed model.
-         * @type {Object}
-         */
-        model: any = this.FeedService.editFeedModel;
-
+    /**
+     * Navigates to the Edit Feed page for the current feed.
+     */
+    navigateToEditFeedInStepper() {
+        this.VisualQueryService.resetModel();
+        this.StateService.FeedManager().Feed().navigateToEditFeedInStepper(this.model.feedId);
+    }
 }
 
-  angular.module("kylo.plugin.template-table-option.data-transformation", [])
-        .controller('DataTransformPropertiesController', 
-                    ["$scope", 
-                    "$q", 
-                    "AccessControlService", 
-                    "FeedService", 
-                    "StateService", 
-                    "VisualQueryService", 
-                    DataTransformPropertiesController])
-        .directive('kyloDataTransformProperties', [()=>
-                               { return {
-                                    controller: "DataTransformPropertiesController",
-                                    controllerAs: "vm",
-                                    restrict: "E",
-                                    templateUrl: "js/plugin/template-table-option/data-transformation/data-transform-properties.html"
-                                };
-                                }
-        ]
-        );
+angular.module("kylo.plugin.template-table-option.data-transformation", [])
+    .component("kyloDataTransformProperties", {
+        controller: DataTransformPropertiesController,
+        controllerAs: "vm",
+        templateUrl: "js/plugin/template-table-option/data-transformation/data-transform-properties.html"
+    });
