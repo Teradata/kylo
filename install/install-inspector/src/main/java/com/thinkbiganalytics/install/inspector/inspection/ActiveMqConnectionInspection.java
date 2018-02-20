241b7797ce3576ac41218b3a32d02a62069b5f11
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2018-02-20 14:02:06 +0000

    KYLO-1393 Java app to verify configuration, ActiveMQ inspection

--- install/install-inspector/src/main/java/com/thinkbiganalytics/install/inspector/inspection/ActiveMqConnectionInspection.java
+++ install/install-inspector/src/main/java/com/thinkbiganalytics/install/inspector/inspection/ActiveMqConnectionInspection.java
@@ -9,9 +9,9 @@ package com.thinkbiganalytics.install.inspector.inspection;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,14 +22,21 @@ package com.thinkbiganalytics.install.inspector.inspection;
 
 
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.jms.Connection;
+import javax.jms.JMSException;
+
 @Component
 public class ActiveMqConnectionInspection extends AbstractInspection {
 
-    private final Logger log = LoggerFactory.getLogger(ActiveMqConnectionInspection.class);
+    private static final String JMS_ACTIVEMQ_BROKER_USERNAME = "jms.activemq.broker.username";
+    private static final String JMS_ACTIVEMQ_BROKER_PASSWORD = "jms.activemq.broker.password";
+    private static final String JMS_ACTIVEMQ_BROKER_URL = "jms.activemq.broker.url";
+    private final Logger LOG = LoggerFactory.getLogger(ActiveMqConnectionInspection.class);
 
     @Override
     public String getName() {
@@ -41,4 +48,31 @@ public class ActiveMqConnectionInspection extends AbstractInspection {
         return "Checks whether Kylo Services can connect to ActiveMQ";
     }
 
+    @Override
+    public InspectionStatus inspect(Configuration configuration) {
+        LOG.debug("ActiveMqConnectionInspection.inspect");
+        String brokerUrl = configuration.getServicesProperty(JMS_ACTIVEMQ_BROKER_URL);
+        String username = configuration.getServicesProperty(JMS_ACTIVEMQ_BROKER_USERNAME);
+        String password = configuration.getServicesProperty(JMS_ACTIVEMQ_BROKER_PASSWORD);
+        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(brokerUrl);
+        Connection connection = null;
+        try {
+            connection = factory.createConnection(username, password);
+            connection.setClientID("kylo-install-inspector"); //this will fail if connection is not set up correctly
+            return new InspectionStatus(true);
+        } catch (JMSException e) {
+            InspectionStatus status = new InspectionStatus(false);
+            status.setError(String.format("Failed to connect to ActiveMQ at %s. Following error occurred: %s", brokerUrl, e.getMessage()));
+            return status;
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    LOG.error("An error occurred while closing JMS connection", e);
+                }
+            }
+        }
+
+    }
 }
