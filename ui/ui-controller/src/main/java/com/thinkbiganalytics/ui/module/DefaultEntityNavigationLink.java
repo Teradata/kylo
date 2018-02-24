f53618bcdcb3b1d88dd1d328e44fd3c40fd12460
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2018-02-23 16:45:25 -0800

    KYLO-1716 UI Plugins as Feed/Template links
    (cherry picked from commit cde84e6)

---
+++ ui/ui-controller/src/main/java/com/thinkbiganalytics/ui/module/DefaultEntityNavigationLink.java
@@ -0,0 +1,52 @@
+package com.thinkbiganalytics.ui.module;
+/*-
+ * #%L
+ * kylo-ui-controller
+ * %%
+ * Copyright (C) 2017 ThinkBig Analytics
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+import com.thinkbiganalytics.ui.api.module.EntityNavigationLink;
+
+public class DefaultEntityNavigationLink implements EntityNavigationLink {
+
+    private String linkText;
+    private String sref;
+    private String icon;
+
+    public String getLinkText() {
+        return linkText;
+    }
+
+    public void setLinkText(String linkText) {
+        this.linkText = linkText;
+    }
+
+    public String getSref() {
+        return sref;
+    }
+
+    public void setSref(String sref) {
+        this.sref = sref;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
