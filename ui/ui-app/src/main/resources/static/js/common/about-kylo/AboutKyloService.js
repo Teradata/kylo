7a9b4892f19ff25934e764f49b7979788f5585df
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-02-26 16:44:28 +0500

    AboutKyloService.ts

--- ui/ui-app/src/main/resources/static/js/common/about-kylo/AboutKyloService.js
+++ ui/ui-app/src/main/resources/static/js/common/about-kylo/AboutKyloService.js
@@ -1,49 +1,53 @@
-/*
- * Service to display 'about kylo' popup page, and get kylo version
- */
-define(['angular','common/module-name'], function (angular,moduleName) {
-    angular.module(moduleName).service('AboutKyloService', function ($mdDialog) {
-        var self = this;
-
-        self.showAboutDialog = function () {
-            $mdDialog.show({
-                controller: 'AboutKyloDialogController',
-                templateUrl: 'js/common/about-kylo/about.html',
-                parent: angular.element(document.body),
-                clickOutsideToClose: false,
-                escapeToClose: true,
-                fullscreen: false,
-                locals: {}
-            }).then(function (msg) {
-                //callback (success)
-            }, function () {
-                //callback (failure)
-            });
-        }
-    });
-
-        var controller = function ($scope, $mdDialog, $http) {
-
-            var self = this;
-
+define(["require", "exports", "angular", "../module-name"], function (require, exports, angular, module_name_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var controller = /** @class */ (function () {
+        function controller($scope, $mdDialog, $http) {
+            this.$scope = $scope;
+            this.$mdDialog = $mdDialog;
+            this.$http = $http;
             $http({
                 method: "GET",
                 url: "/proxy/v1/about/version"
             }).then(function callSuccess(response) {
                 $scope.version = response.data;
             }, function callFailure(response) {
-                $scope.version = "Not Available"
+                $scope.version = "Not Available";
             });
-
             $scope.hide = function () {
                 $mdDialog.hide();
             };
-
             $scope.cancel = function () {
                 $mdDialog.cancel();
             };
-
-        };
-        angular.module(moduleName).controller('AboutKyloDialogController', controller);
-
+        }
+        return controller;
+    }());
+    exports.default = controller;
+    var AboutKyloService = /** @class */ (function () {
+        function AboutKyloService($mdDialog) {
+            var _this = this;
+            this.$mdDialog = $mdDialog;
+            this.showAboutDialog = function () {
+                _this.$mdDialog.show({
+                    controller: 'AboutKyloDialogController',
+                    templateUrl: 'js/common/about-kylo/about.html',
+                    parent: angular.element(document.body),
+                    clickOutsideToClose: false,
+                    escapeToClose: true,
+                    fullscreen: false,
+                    locals: {}
+                }).then(function (msg) {
+                    //callback (success)
+                }, function () {
+                    //callback (failure)
+                });
+            };
+        }
+        return AboutKyloService;
+    }());
+    exports.AboutKyloService = AboutKyloService;
+    angular.module(module_name_1.moduleName).service('AboutKyloService', AboutKyloService);
+    angular.module(module_name_1.moduleName).controller('AboutKyloDialogController', controller);
 });
+//# sourceMappingURL=AboutKyloService.js.map
