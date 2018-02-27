a4a2250973c86d97635342d9ffcab0be4dbc0e24
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2018-02-27 14:12:06 +0000

    KYLO-1702 Hive Connection Inspection

--- install/install-inspector/src/main/java/com/thinkbiganalytics/install/inspector/inspection/HiveConnectionInspection.java
+++ install/install-inspector/src/main/java/com/thinkbiganalytics/install/inspector/inspection/HiveConnectionInspection.java
@@ -9,9 +9,9 @@ package com.thinkbiganalytics.install.inspector.inspection;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,14 +21,19 @@ package com.thinkbiganalytics.install.inspector.inspection;
  */
 
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 @Component
 public class HiveConnectionInspection extends AbstractInspection {
 
-    private final Logger log = LoggerFactory.getLogger(HiveConnectionInspection.class);
+    private static final Logger LOG = LoggerFactory.getLogger(HiveConnectionInspection.class);
 
     @Override
     public String getName() {
@@ -40,4 +45,30 @@ public class HiveConnectionInspection extends AbstractInspection {
         return "Checks whether Hive connection is setup";
     }
 
+
+    @Override
+    public InspectionStatus inspect(Configuration configuration) {
+        InspectionStatus status = new InspectionStatus(false);
+
+        DataSource ds;
+        try {
+            ds = configuration.getServicesBean(HiveConnectionInspectionConfiguration.class, DataSource.class);
+        } catch (Exception e) {
+            String msg = String.format("Failed to check Hive connection: %s", ExceptionUtils.getRootCause(e).getMessage());
+            LOG.error(msg, e);
+            status.addError(msg);
+            return status;
+        }
+        try {
+            ((com.thinkbiganalytics.hive.service.RefreshableDataSource) ds).testConnection();
+        } catch (SQLException e) {
+            String msg = String.format("Failed to connect to Hive: %s", e.getMessage());
+            LOG.error(msg, e);
+            status.addError(msg);
+            return status;
+        }
+
+        status.setValid(true);
+        return status;
+    }
 }
