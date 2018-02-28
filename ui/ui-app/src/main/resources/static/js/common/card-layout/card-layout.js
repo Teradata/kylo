0317c02dfac7a942e1f0712e397044a11ff54f9c
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-02-28 10:34:04 +0500

    card-layout.ts

--- ui/ui-app/src/main/resources/static/js/common/card-layout/card-layout.js
+++ ui/ui-app/src/main/resources/static/js/common/card-layout/card-layout.js
@@ -1,19 +1,20 @@
-define(['angular','common/module-name'], function (angular,moduleName) {
-
-    return angular.module(moduleName).directive("cardLayout", function ($compile) {
+define(["require", "exports", "angular", "../module-name"], function (require, exports, angular, module_name_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    angular.module(module_name_1.moduleName).directive("cardLayout", function ($compile) {
         return {
-            scope: {headerCss: "@", bodyCss: "@", cardCss: '@',cardToolbar: "=?"},
+            scope: { headerCss: "@", bodyCss: "@", cardCss: '@', cardToolbar: "=?" },
             transclude: {
                 'header1': '?headerSection',
                 'body1': '?bodySection'
             },
             templateUrl: 'js/common/card-layout/card-layout.html',
             link: function (scope, iElem, iAttrs, ctrl, transcludeFn) {
-                if(angular.isUndefined(scope.cardToolbar)){
+                if (angular.isUndefined(scope.cardToolbar)) {
                     scope.cardToolbar = true;
                 }
             }
         };
     });
 });
-
+//# sourceMappingURL=card-layout.js.map
