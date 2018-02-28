826374874171014d66e8d2eb5b6c21570ea3e1d1
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-02-28 10:27:05 +0500

    modue + module-name + module-require

--- ui/ui-app/src/main/resources/static/js/common/module-require.js
+++ ui/ui-app/src/main/resources/static/js/common/module-require.js
@@ -1,44 +1,5 @@
-define(['angular',
-        'common/module-name',
-        'common/ng-if-permission/ng-if-permission',
-        'common/card-layout/card-layout',
-        'common/card-filter-header/card-filter-header',
-        'common/custom-icons/custom-icons',
-        'common/view-type-selection/view-type-selection',
-        'common/stepper/step-buttons',
-        'common/stepper/StepperService',
-        'common/stepper/stepper',
-        'common/file-upload/file-upload',
-        'common/about-kylo/AboutKyloService',
-        'common/filters/filters',
-        'common/icon-picker-dialog/icon-picker-dialog',
-        'common/draggable/draggable',
-        'common/sticky/sticky',
-        'common/sticky-tabs/sticky-tabs',
-        'common/add-button/add-button',
-        'common/fixed-top-right/fixed-top-right',
-        'common/ui-router-breadcrumbs/ui-router-breadcrumbs',
-        'common/kylo-options/kylo-options',
-        'common/vertical-section-layout/vertical-section-layout-directive',
-        'common/dir-pagination/dirPagination-arrows',
-        'common/browser-height/browser-height',
-        'common/options-menu/OptionsMenu',
-        'common/string-to-number/string-to-number',
-        'common/utils/IDGenerator',
-        'common/utils/StringUtils',
-        'common/utils/ArrayUtils',
-        'common/utils/DateTimeUtils',
-        'common/accordion-menu/AccordionMenuService',
-        'common/accordion-menu/accordion-menu',
-        'common/accordion-menu/menu_toggle',
-        'common/accordion-menu/menulink',
-        'common/timer/kylo-timer',
-        'common/multiple-email-validator/multiple-email-validator.js',
-        'common/lazy-load-retain-order/lazy-load-retain-order',
-        'common/notifications/angular1',
-        'jquery'],function(angular, moduleName){
-    return angular.module(moduleName);
+define(["require", "exports", "angular", "./module-name", "./ng-if-permission/ng-if-permission", "./card-layout/card-layout", "./card-filter-header/card-filter-header", "./custom-icons/custom-icons", "./view-type-selection/view-type-selection", "./stepper/step-buttons", "./stepper/StepperService", "./stepper/stepper", "./file-upload/file-upload", "./about-kylo/AboutKyloService", "./filters/filters", "./icon-picker-dialog/icon-picker-dialog", "./draggable/draggable", "./sticky/sticky", "./sticky-tabs/sticky-tabs", "./add-button/add-button", "./fixed-top-right/fixed-top-right", "./ui-router-breadcrumbs/ui-router-breadcrumbs", "./kylo-options/kylo-options", "./vertical-section-layout/vertical-section-layout-directive", "./dir-pagination/dirPagination-arrows", "./browser-height/browser-height", "./options-menu/OptionsMenu", "./string-to-number/string-to-number", "./utils/IDGenerator", "./utils/StringUtils", "./utils/ArrayUtils", "./utils/DateTimeUtils", "./accordion-menu/AccordionMenuService", "./accordion-menu/accordion-menu", "./accordion-menu/menu_toggle", "./accordion-menu/menulink", "./timer/kylo-timer", "./multiple-email-validator/multiple-email-validator.js", "./lazy-load-retain-order/lazy-load-retain-order", "./notifications/angular1", "jquery"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
 });
-
-
-
+//# sourceMappingURL=module-require.js.map
