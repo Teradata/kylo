0397e1baebbee0984769a71dfd550ab458d69920
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-02-28 10:38:07 +0500

    kylo-options.ts

--- ui/ui-app/src/main/resources/static/js/common/kylo-options/kylo-options.ts
+++ ui/ui-app/src/main/resources/static/js/common/kylo-options/kylo-options.ts
@@ -1,9 +1,12 @@
-define(["require", "exports", "angular", "../module-name"], function (require, exports, angular, module_name_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    angular.module(module_name_1.moduleName).directive('kyloOptions', ['$http', '$mdDialog', '$window', 'AboutKyloService',
-        function ($http, $mdDialog, $window, AboutKyloService) {
-            return {
+import * as angular from "angular";
+import {moduleName} from "../module-name";
+
+
+   angular.module(moduleName).directive('kyloOptions', 
+   ['$http', '$mdDialog', '$window', 'AboutKyloService', 
+   ($http, $mdDialog, $window, AboutKyloService)=>
+   {
+  return {
                 restrict: "E",
                 scope: {
                     selectedOption: "&?",
@@ -11,37 +14,40 @@ define(["require", "exports", "angular", "../module-name"], function (require, e
                     menuIcon: "@?"
                 },
                 templateUrl: 'js/common/kylo-options/kylo-options.html',
-                link: function ($scope) {
+                link: function ($scope: any) {
+
                     //default the icon to be more_vert
                     if (!angular.isDefined($scope.menuIcon)) {
                         $scope.menuIcon = 'more_vert';
                     }
+
                     // Get user name
                     $scope.username = "User";
-                    this.$http.get("/proxy/v1/about/me").then(function (response) {
+                    this.$http.get("/proxy/v1/about/me").then(function (response: any) {
                         $scope.username = response.data.systemName;
                     });
-                    $scope.openMenu = function ($mdOpenMenu, ev) {
+
+                    $scope.openMenu = function ($mdOpenMenu: any, ev: any) {
                         //callback
                         if ($scope.openedMenu) {
                             $scope.openedMenu();
                         }
                         $mdOpenMenu(ev);
                     };
+
                     $scope.aboutKylo = function () {
                         this.AboutKyloService.showAboutDialog();
                         if ($scope.selectedOption) {
                             $scope.selectedOption()('aboutKylo');
                         }
                     };
+
                     /**
                      * Redirects the user to the logout page.
                      */
                     $scope.logout = function () {
                         this.$window.location.href = "/logout";
-                    };
+                    }
                 }
-            };
-        }]);
-});
-//# sourceMappingURL=kylo-options.js.map
+            }
+   }]);
