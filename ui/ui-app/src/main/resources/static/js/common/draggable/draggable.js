818347583075c4ba880a93fd4d110a4f4ed7b601
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-02-28 10:35:20 +0500

    draggable.ts

--- ui/ui-app/src/main/resources/static/js/common/draggable/draggable.js
+++ ui/ui-app/src/main/resources/static/js/common/draggable/draggable.js
@@ -1,18 +1,20 @@
-define(['angular','common/module-name','draggabilly'], function (angular,moduleName, Draggabilly) {
-
-    angular.module(moduleName).directive('ngDraggable', function () {
-        return {
-            restrict: 'A',
-            scope: {
-                dragOptions: '=ngDraggable'
-            },
-            link: function (scope, elem, attr) {
-                if (scope.dragOptions == undefined) {
-                    scope.dragOptions = {};
+define(["require", "exports", "angular", "../module-name"], function (require, exports, angular, module_name_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Draggabilly = require("../../../bower_components/draggabilly/dist/draggabilly.pkgd");
+    angular.module(module_name_1.moduleName).directive('ngDraggable', [function () {
+            return {
+                restrict: 'A',
+                scope: {
+                    dragOptions: '=ngDraggable'
+                },
+                link: function (scope, elem, attr) {
+                    if (scope.dragOptions == undefined) {
+                        scope.dragOptions = {};
+                    }
+                    var draggable = new Draggabilly(elem[0], scope.dragOptions);
                 }
-                var draggable = new Draggabilly(elem[0], scope.dragOptions);
-            }
-        }
-
-    })
+            };
+        }]);
 });
+//# sourceMappingURL=draggable.js.map
