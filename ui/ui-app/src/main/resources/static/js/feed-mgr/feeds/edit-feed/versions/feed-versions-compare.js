077247b4b03571d9fa42ad151724dbce0f6d727d
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-03-21 13:32:10 +0500

    feed-version-compare.ts

--- ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/edit-feed/versions/feed-versions-compare.js
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/edit-feed/versions/feed-versions-compare.js
@@ -1,83 +1,78 @@
-define(['angular', 'feed-mgr/feeds/edit-feed/module-name'], function (angular, moduleName) {
-
-    var directive = function () {
-        return {
-            restrict: "EA",
-            bindToController: {
-            },
-            controllerAs: 'vm',
-            scope: {},
-            templateUrl: 'js/feed-mgr/feeds/edit-feed/versions/feed-versions-compare.html',
-            controller: "FeedVersionsCompareController",
-            link: function ($scope, element, attrs, controller) {
-
-            }
-
-        };
-    }
-
-    var controller = function ($scope, $http, $q, $filter, RestUrlService, FeedService) {
-
-        var self = this;
-
-        this.model = FeedService.editFeedModel;
-        FeedService.versionFeedModel = {};
-        FeedService.versionFeedModelDiff = [];
-        this.current = $filter('translate')('views.feed-versions-compare.Current');
-        this.leftVersion = this.current;
-        this.rightVersion = {};
-        this.versions = [];
-        this.loading = false;
-
-
-        this.loadVersions = function () {
-            FeedService.getFeedVersions(this.model.feedId).then(function(result) {
-                self.versions = result.versions;
-                self.leftVersion = self.current + " (" + getCurrentVersion().name + ")";
-            }, function(err) {
-
-            });
-        };
-
-        function getCurrentVersion() {
-            return self.versions[0];
-        }
-
-        this.changeRightVersion = function() {
-            var version = _.find(self.versions, function(v){
-                return v.id === self.rightVersion;
-            });
-            self.loading = true;
-            var diff = FeedService.diffFeedVersions(this.model.feedId, self.rightVersion, getCurrentVersion().id).then(function(result) {
-                // console.log('diff', result.difference);
-                FeedService.versionFeedModelDiff = [];
-                _.each(result.difference.patch, function(patch) {
-                    FeedService.versionFeedModelDiff[patch.path] = patch;
+define(["require", "exports", "angular", "underscore"], function (require, exports, angular, _) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var moduleName = require('feed-mgr/feeds/module-name');
+    var controller = /** @class */ (function () {
+        function controller($scope, $http, $q, $filter, RestUrlService, FeedService) {
+            var _this = this;
+            this.$scope = $scope;
+            this.$http = $http;
+            this.$q = $q;
+            this.$filter = $filter;
+            this.RestUrlService = RestUrlService;
+            this.FeedService = FeedService;
+            this.getCurrentVersion = function () {
+                return _this.versions[0];
+            };
+            this.model = FeedService.editFeedModel;
+            FeedService.versionFeedModel = {};
+            FeedService.versionFeedModelDiff = [];
+            this.current = $filter('translate')('views.feed-versions-compare.Current');
+            this.leftVersion = this.current;
+            this.rightVersion = {};
+            this.versions = [];
+            this.loading = false;
+            this.loadVersions = function () {
+                FeedService.getFeedVersions(_this.model.feedId).then(function (result) {
+                    _this.versions = result.versions;
+                    _this.leftVersion = _this.current + " (" + _this.getCurrentVersion().name + ")";
+                }, function (err) {
                 });
-            }, function (err) {
-
-            });
-
-            var versionedFeed = FeedService.getFeedVersion(this.model.feedId, self.rightVersion).then(function(result) {
-                self.rightFeed = result.entity;
-                FeedService.versionFeedModel = self.rightFeed;
-                FeedService.versionFeedModel.version = version;
-            }, function (err) {
-
-            });
-
-            Promise.all([diff, versionedFeed]).then(function(result) {
-                self.loading = false;
-            }).catch(function(err) {
-                self.loading = false;
-            });
-
-        };
-
-        self.loadVersions();
-    };
-
+            };
+            this.changeRightVersion = function () {
+                var version = _.find(_this.versions, function (v) {
+                    return v.id === _this.rightVersion;
+                });
+                _this.loading = true;
+                var diff = FeedService.diffFeedVersions(_this.model.feedId, _this.rightVersion, _this.getCurrentVersion().id).then(function (result) {
+                    // console.log('diff', result.difference);
+                    FeedService.versionFeedModelDiff = [];
+                    _.each(result.difference.patch, function (patch) {
+                        FeedService.versionFeedModelDiff[patch.path] = patch;
+                    });
+                }, function (err) {
+                });
+                var versionedFeed = FeedService.getFeedVersion(_this.model.feedId, _this.rightVersion).then(function (result) {
+                    _this.rightFeed = result.entity;
+                    FeedService.versionFeedModel = _this.rightFeed;
+                    FeedService.versionFeedModel.version = version;
+                }, function (err) {
+                });
+                Promise.all([diff, versionedFeed]).then(function (result) {
+                    _this.loading = false;
+                }).catch(function (err) {
+                    _this.loading = false;
+                });
+            };
+            this.loadVersions();
+        }
+        return controller;
+    }());
+    exports.default = controller;
     angular.module(moduleName).controller('FeedVersionsCompareController', ["$scope", "$http", "$q", "$filter", "RestUrlService", "FeedService", controller]);
-
-    angular.module(moduleName).directive('thinkbigFeedVersionsCompare', directive);
+    angular.module(moduleName).directive('thinkbigFeedVersionsCompare', [
+        function () {
+            return {
+                restrict: "EA",
+                bindToController: {},
+                controllerAs: 'vm',
+                scope: {},
+                templateUrl: 'js/feed-mgr/feeds/edit-feed/versions/feed-versions-compare.html',
+                controller: "FeedVersionsCompareController",
+                link: function ($scope, element, attrs, controller) {
+                }
+            };
+        }
+    ]);
 });
+//# sourceMappingURL=feed-versions-compare.js.map
