3b9022685d212098555cefc3d26c880f2193ab3a
Myra Batool <35554338+syedamyrabatool@users.noreply.github.com>
2018-03-29 15:51:47 +0500

    Code Cleanup

--- ui/ui-app/src/main/resources/static/js/ops-mgr/module-require.js
+++ ui/ui-app/src/main/resources/static/js/ops-mgr/module-require.js
@@ -1,4 +1,4 @@
-define(["require", "exports", "angular", "./services/AlertsServiceV2", "./services/EventService", "./services/OpsManagerFeedService", "./services/ChartJobStatusService", "./services/IconStatusService", "./services/Nvd3ChartService", "./services/OpsManagerJobService", "./services/OpsManagerRestUrlService", "./services/ProvenanceEventStatsService", "./services/ServicesStatusService", "./services/TabService", "./services/OpsManagerDashboardService"], function (require, exports) {
+define(["require", "exports", "angular", "./services/AlertsService", "./services/AlertsServiceV2", "./services/EventService", "./services/OpsManagerFeedService", "./services/ChartJobStatusService", "./services/IconStatusService", "./services/Nvd3ChartService", "./services/OpsManagerJobService", "./services/OpsManagerRestUrlService", "./services/ProvenanceEventStatsService", "./services/ServicesStatusService", "./services/TabService", "./services/OpsManagerDashboardService"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
 });
