157227892dcff5e31f75b15ea8f87516e4cc97a9
Ahmad Hassan <ahmad.hassan@teradata.com>
2018-04-05 12:40:42 +0500

    Feed Access Control Directive Converted to a Component

--- ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/define-feed/feed-details/DefineFeedAccessControlDirective.ts
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/define-feed/feed-details/DefineFeedAccessControlDirective.ts
@@ -2,105 +2,77 @@ import * as angular from 'angular';
 import * as _ from "underscore";
 const moduleName = require('feed-mgr/feeds/define-feed/module-name');
 
-var directive = function () {
-    return {
-        restrict: "EA",
-        bindToController: {
-            stepIndex: '@'
-        },
-        scope: {},
-        controllerAs: 'vm',
-        require:['thinkbigDefineFeedAccessControl','^thinkbigStepper'],
-        templateUrl: 'js/feed-mgr/feeds/define-feed/feed-details/define-feed-access-control.html',
-        controller: "DefineFeedAccessControlController",
-        link: function ($scope:any, element:any, attrs:any, controllers:any) {
-            var thisController = controllers[0];
-            var stepperController = controllers[1];
-            thisController.stepperController = stepperController;
-            thisController.totalSteps = stepperController.totalSteps;
-        }
-    };
-};
-
 export class DefineFeedAccessControlController {
 
-    transformChip:any;
-    securityGroupsEnabled:any;
-    securityGroupChips:any;
+    /**
+         * Flag to indicate if hadoop groups are enabled or not
+         * @type {boolean}
+    */
+    securityGroupsEnabled:any = false;
+    /**
+         * Hadoop security groups chips model
+         * @type {{}}
+    */
+    securityGroupChips:any = {};
+    /**
+         * Service to access the Hadoop security groups
+    */
     feedSecurityGroups:any;
+    /**
+         * The feed model
+         * @type {*}
+    */
     model:any;
-    feedAccessControlForm:any;
-    stepNumber:any;
+    feedAccessControlForm:any = {};
+    stepNumber:number;
     stepIndex:any;
+    stepperController: { totalSteps : number };
+    totalSteps:number;
+    
+    $onInit(){
+        this.totalSteps = this.stepperController.totalSteps;
+        this.stepNumber = parseInt(this.stepIndex)+1;
+    }
+
+    
+
+    static readonly $inject = ["$scope","FeedService","FeedSecurityGroups"];
 
     constructor($scope:any,FeedService:any, FeedSecurityGroups:any) {
 
-
-        this.stepNumber = parseInt(this.stepIndex)+1
-
-        /**
-         * ref back to this controller
-         * @type {DefineFeedAccessControlController}
-         */
-        var self = this;
-
-        /**
-         * The angular form
-         * @type {{}}
-         */
-        this.feedAccessControlForm = {};
-
-        /**
-         * The feed model
-         * @type {*}
-         */
         this.model = FeedService.createFeedModel;
-
-        /**
-         * Service to access the Hadoop security groups
-         */
-        self.feedSecurityGroups = FeedSecurityGroups;
-
-        /**
-         * Hadoop security groups chips model
-         * @type {{}}
-         */
-        self.securityGroupChips = {};
-        self.securityGroupChips.selectedItem = null;
-        self.securityGroupChips.searchText = null;
-
-        /**
-         * Flag to indicate if hadoop groups are enabled or not
-         * @type {boolean}
-         */
-        self.securityGroupsEnabled = false;
-
-
-
-
-
+        
+        this.feedSecurityGroups = FeedSecurityGroups;
+        
+        this.securityGroupChips.selectedItem = null;
+        this.securityGroupChips.searchText = null;
 
         FeedSecurityGroups.isEnabled().then(function(isValid:any) {
-            self.securityGroupsEnabled = isValid;
+            this.securityGroupsEnabled = isValid;
         });
-
-
-
-        self.transformChip = function(chip:any) {
-            // If it is an object, it's already a known chip
-            if (angular.isObject(chip)) {
-                return chip;
-            }
-            // Otherwise, create a new one
-            return {name: chip}
-        };
-
-
-
     }
-
+    
+    transformChip(chip:any){
+        // If it is an object, it's already a known chip
+        if (angular.isObject(chip)) {
+            return chip;
+        }
+        // Otherwise, create a new one
+        return {name: chip}
+    }
 }
 
-angular.module(moduleName).controller("DefineFeedAccessControlController",["$scope","FeedService","FeedSecurityGroups", DefineFeedAccessControlController]);
 
-angular.module(moduleName).directive("thinkbigDefineFeedAccessControl", directive);
+angular.module(moduleName).
+    component("thinkbigDefineFeedAccessControl", {
+        bindings: {
+            stepIndex: '@'
+        },
+        require:{
+            stepperController: "^thinkbigStepper"
+
+        },
+        controllerAs: 'vm',
+        controller: DefineFeedAccessControlController,
+        templateUrl: 'js/feed-mgr/feeds/define-feed/feed-details/define-feed-access-control.html',
+    });
