485fa82dbc4b2afcac1e47000a8a76262b99836f
Sean Felten <sean.felten@thinkbiganalytics.com>
2018-04-10 07:42:04 -0700

    KYLO-1887: License headers and dependency cleanup.

--- integrations/spark/spark-merge-table/spark-merge-table-app/src/test/java/com/thinkbiganalytics/spark/mergetable/TableMergeArgumentsTest.java
+++ integrations/spark/spark-merge-table/spark-merge-table-app/src/test/java/com/thinkbiganalytics/spark/mergetable/TableMergeArgumentsTest.java
@@ -3,6 +3,26 @@
  */
 package com.thinkbiganalytics.spark.mergetable;
 
+/*-
+ * #%L
+ * kylo-spark-merge-table-app
+ * %%
+ * Copyright (C) 2017 - 2018 ThinkBig Analytics, a Teradata Company
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
@@ -24,7 +44,7 @@ public class TableMergeArgumentsTest {
                                                 "-s", "source.table", 
                                                 "-t", "target.table", 
                                                 "-v", "123", 
-                                                "-m", "dedupe_merge"};
+                                                "-m", "dedupe_and_merge"};
     private static final String[] LONG_ARGS = {"--partition-keys", "field1|type1|formula(column1),field2|type2|formula(column2)", 
                                                "--column-specs", "column1|type1|comment|1|1|0", 
                                                "--source-table", "source.table", 
