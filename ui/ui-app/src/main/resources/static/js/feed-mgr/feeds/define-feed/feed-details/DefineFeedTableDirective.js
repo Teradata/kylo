12a05a2c84ef8db75e087e81a4eda031740118df
Tim Harsch <tim.harsch@thinkbiganalytics.com>
2018-04-16 13:30:33 -0700

    KYLO-1746: remove unneeded logging

--- ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/define-feed/feed-details/DefineFeedTableDirective.js
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/define-feed/feed-details/DefineFeedTableDirective.js
@@ -129,7 +129,7 @@ define(["require", "exports", "angular", "underscore"], function (require, expor
                 self.expandSchemaPanel();
             }
             this.calcTableState = function () {
-                console.log("calcTableState");
+                // console.log("calcTableState");
                 self.tableLocked = angular.isDefined(self.tableLocked) && (self.tableLocked === true || self.tableLocked === "true");
                 self.dataTypeLocked = angular.isDefined(self.dataTypeLocked) && (self.typeLocked === true || self.typeLocked === "true");
                 self.canRemoveFields = angular.isUndefined(self.canRemoveFields) || self.canRemoveFields === true || self.canRemoveFields === "true";
