3f0fbf0bab04a8a5f2b96c3ddf89fbfc0cf47802
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2018-04-24 08:45:05 -0700

    KYLO-373 Error importing Feed with required properties

--- ui/ui-app/src/main/resources/static/js/feed-mgr/services/ImportService.js
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/services/ImportService.js
@@ -11,6 +11,8 @@ define(["require", "exports", "angular", "underscore"], function (require, expor
         ImportComponentType["REMOTE_INPUT_PORT"] = "REMOTE_INPUT_PORT";
         ImportComponentType["USER_DATASOURCES"] = "USER_DATASOURCES";
         ImportComponentType["TEMPLATE_CONNECTION_INFORMATION"] = "TEMPLATE_CONNECTION_INFORMATION";
+        ImportComponentType[ImportComponentType["FEED_CATEGORY_USER_FIELDS"] = ImportComponentOptionTypes_1.Import.ImportComponentType.FEED_CATEGORY_USER_FIELDS] = "FEED_CATEGORY_USER_FIELDS";
+        ImportComponentType[ImportComponentType["FEED_USER_FIELDS"] = ImportComponentOptionTypes_1.Import.ImportComponentType.FEED_USER_FIELDS] = "FEED_USER_FIELDS";
     })(ImportComponentType = exports.ImportComponentType || (exports.ImportComponentType = {}));
     var DefaultImportService = /** @class */ (function () {
         function DefaultImportService() {
@@ -61,6 +63,12 @@ define(["require", "exports", "angular", "underscore"], function (require, expor
         };
         DefaultImportService.prototype.newUserDatasourcesImportOption = function () {
             return this.newImportComponentOption("USER_DATASOURCES" /* USER_DATASOURCES */);
+        };
+        DefaultImportService.prototype.newFeedCategoryUserFieldsImportOption = function () {
+            return this.newImportComponentOption(ImportComponentOptionTypes_1.Import.ImportComponentType.FEED_CATEGORY_USER_FIELDS);
+        };
+        DefaultImportService.prototype.newFeedUserFieldsImportOption = function () {
+            return this.newImportComponentOption(ImportComponentOptionTypes_1.Import.ImportComponentType.FEED_USER_FIELDS);
         };
         DefaultImportService.prototype.newUploadKey = function () {
             return _.uniqueId("upload_") + new Date().getTime() + this.guid();
