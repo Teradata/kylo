285dfb8d34389cbe91100d1f60588a0ef21a8723
Aakifa <aakifa.ishtiaq@teradata.com>
2018-05-14 17:28:15 +0500

    overview folder conversion

--- ui/ui-app/src/main/resources/static/js/ops-mgr/overview/data-confidence-indicator/DataConfidenceIndicatorDirective.ts
+++ ui/ui-app/src/main/resources/static/js/ops-mgr/overview/data-confidence-indicator/DataConfidenceIndicatorDirective.ts
@@ -4,76 +4,77 @@ import "pascalprecht.translate";
 import * as _ from 'underscore';
 
 export class controller implements ng.IComponentController{
-refreshing: any;
-dataLoaded: any;
-dataMap: any;
-allData: any;
-chartApi: any;
+refreshing: boolean = false;
+dataLoaded: boolean = false;
+dataMap: any = {'Healthy':{count:0, color:'#5cb85c'},'Unhealthy':{count:0,color:'#a94442'}};
+allData: any = null;
+chartApi: any = {};
 chartOptions: any;
 chartData: any[];
-constructor(private $scope: any,
-            private $element: any, 
-            private $http: any,
-            private $interval: any,
-            private $mdDialog: any,
+
+static readonly $inject = ["$scope","$element","$http","$interval","$mdDialog","OpsManagerJobService","OpsManagerDashboardService","BroadcastService", "$filter"];
+
+$onInit() {
+    this.ngOnInit();
+}
+
+ngOnInit() {
+
+    this.chartOptions = {
+        chart: {
+            type: 'pieChart',
+            x: (d: any)=>{return d.key;},
+            y: (d: any)=>{return d.value;},
+            showLabels: false,
+            duration: 100,
+            height:150,
+            labelThreshold: 0.01,
+            labelSunbeamLayout: false,
+            "margin":{"top":10,"right":10,"bottom":10,"left":10},
+            donut:true,
+            donutRatio:0.65,
+            showLegend:false,
+            refreshDataOnly: false,
+            valueFormat: (d: any)=>{
+                return parseInt(d);
+            },
+            color:(d: any)=>{
+                if(d.key == 'Healthy'){
+                    return '#009933';
+                }
+                else if( d.key== 'Unhealthy'){
+                    return '#FF0000';
+                }
+            },
+            pie: {
+                dispatch: {
+                    'elementClick': (e: any)=>{
+                        this.openDetailsDialog(e.data.key);
+                    }
+                }
+            },
+            dispatch: {
+
+            }
+        }
+    };
+    this.chartData = [];
+
+    this.initializePieChart();
+    this.watchDashboard();
+    
+}
+
+constructor(private $scope: IScope,
+            private $element: JQuery, 
+            private $http: angular.IHttpService,
+            private $interval: angular.IIntervalService,
+            private $mdDialog: angular.material.IDialogService,
             private OpsManagerJobService: any,
             private OpsManagerDashboardService: any,
             private BroadcastService: any,
-            private $filter: any){
-                this.refreshing = false;
-                this.dataLoaded = false;
-
-                this.dataMap = {'Healthy':{count:0, color:'#5cb85c'},'Unhealthy':{count:0,color:'#a94442'}};
-
-                /** All the data returned from the rest call
-                 *
-                 * @type {null}
-                 */
-                this.allData = null;
-                this.chartApi = {};
-
-                this.chartOptions = {
-                    chart: {
-                        type: 'pieChart',
-                        x: (d: any)=>{return d.key;},
-                        y: (d: any)=>{return d.value;},
-                        showLabels: false,
-                        duration: 100,
-                        height:150,
-                        labelThreshold: 0.01,
-                        labelSunbeamLayout: false,
-                        "margin":{"top":10,"right":10,"bottom":10,"left":10},
-                        donut:true,
-                        donutRatio:0.65,
-                        showLegend:false,
-                        refreshDataOnly: false,
-                        valueFormat: (d: any)=>{
-                            return parseInt(d);
-                        },
-                        color:(d: any)=>{
-                            if(d.key == 'Healthy'){
-                                return '#009933';
-                            }
-                            else if( d.key== 'Unhealthy'){
-                                return '#FF0000';
-                            }
-                        },
-                        pie: {
-                            dispatch: {
-                                'elementClick': (e: any)=>{
-                                    this.openDetailsDialog(e.data.key);
-                                }
-                            }
-                        },
-                        dispatch: {
-
-                        }
-                    }
-                };
-                this.chartData = [];
-
-                this.init();
-
+            private $filter: angular.IFilterService){
+                
                 $scope.$on('$destroy',  ()=> {
                     //cleanup
                 });
@@ -81,7 +82,39 @@ constructor(private $scope: any,
        
       openDetailsDialog = (key: any)=>{
                     this.$mdDialog.show({
-                        controller:"DataConfidenceDetailsDialogController",
+                        controller:["$scope","$mdDialog","$interval","StateService","status","allChartData",
+                        ($scope: any,$mdDialog: any,$interval: any,StateService: any,status: any,allChartData:any)=>{
+            
+                            var jobs:any[];
+                            $scope.jobs = jobs;
+            
+                            if(status == 'Unhealthy') {
+                                $scope.css = "md-warn";
+                                $scope.jobs = allChartData.failedJobs;
+                            } else {
+                                $scope.css = "";
+                                $scope.jobs = _.filter(allChartData.latestCheckDataFeeds,(job: any)=> {
+                                    return job.status != 'FAILED';
+                                });
+                            }
+            
+                            $scope.allChartData = allChartData;
+                            $scope.status = status;
+            
+                            $scope.hide =  ()=> {
+                                $mdDialog.hide();
+            
+                            };
+            
+                            $scope.gotoJobDetails = (jobExecutionId: any)=>{
+                                $mdDialog.hide();
+                                StateService.OpsManager().Job().navigateToJobDetails(jobExecutionId);
+                            }
+            
+                            $scope.cancel =  ()=> {
+                                $mdDialog.cancel();
+                            };
+                        }],
                         templateUrl: 'js/ops-mgr/overview/data-confidence-indicator/data-confidence-details-dialog.html',
                         parent: angular.element(document.body),
                         clickOutsideToClose: true,
@@ -146,70 +179,13 @@ constructor(private $scope: any,
             });
         }
 
-
-        init = function () {
-            this.initializePieChart();
-            this.watchDashboard();
-        }
 }
 
-export class DataConfidenceDetailsDialogController implements ng.IComponentController{
-jobs: any[];
-    constructor(private $scope: any,
-                private $mdDialog: any,
-                private $interval: any,
-                private StateService: any,
-                private status: any,
-                private allChartData:any){
-
-                $scope.jobs = this.jobs;
-
-                if(status == 'Unhealthy') {
-                    $scope.css = "md-warn";
-                    $scope.jobs = allChartData.failedJobs;
-                } else {
-                    $scope.css = "";
-                    $scope.jobs = _.filter(allChartData.latestCheckDataFeeds,(job: any)=> {
-                        return job.status != 'FAILED';
-                    });
-                }
-
-                $scope.allChartData = allChartData;
-                $scope.status = status;
-
-                $scope.hide =  ()=> {
-                    $mdDialog.hide();
-
-                };
-
-                $scope.gotoJobDetails = (jobExecutionId: any)=>{
-                    $mdDialog.hide();
-                    StateService.OpsManager().Job().navigateToJobDetails(jobExecutionId);
-                }
-
-                $scope.cancel =  ()=> {
-                    $mdDialog.cancel();
-                };
-                }
-}
-
-
-angular.module(moduleName).controller('DataConfidenceDetailsDialogController', ["$scope","$mdDialog","$interval","StateService","status","allChartData",DataConfidenceDetailsDialogController]);
-angular.module(moduleName).controller('DataConfidenceIndicatorController',["$scope","$element","$http","$interval","$mdDialog","OpsManagerJobService","OpsManagerDashboardService","BroadcastService", "$filter", controller]);
-angular.module(moduleName)
-        .directive('tbaDataConfidenceIndicator',[ ()=> {
-                    return {
-                        restrict: "EA",
-                        scope: true,
-                        bindToController: {
-                            panelTitle: "@"
-                        },
-                        controllerAs: 'vm',
-                        templateUrl: 'js/ops-mgr/overview/data-confidence-indicator/data-confidence-indicator-template.html',
-                        controller: "DataConfidenceIndicatorController",
-                        link: function ($scope, element, attrs) {
-                            $scope.$on('$destroy', function () {
-
-                            });
-                        } //DOM manipulation\}
-                    }}]);
+angular.module(moduleName).component('tbaDataConfidenceIndicator',{
+    controller: controller,
+    bindings: {
+        panelTitle: "@"
+    },
+    controllerAs: "vm",
+    templateUrl: "js/ops-mgr/overview/data-confidence-indicator/data-confidence-indicator-template.html"
+});
