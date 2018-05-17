69f8f7637489a2cca2c1cf9eed57c4f693e711e5
Ahmad Hassan <ahmad.hassan@teradata.com>
2018-05-17 17:47:01 +0500

    conversion of datasources folder to component

--- ui/ui-app/src/main/resources/static/js/services/AddButtonService.ts
+++ ui/ui-app/src/main/resources/static/js/services/AddButtonService.ts
@@ -1,41 +1,40 @@
 import * as angular from 'angular';
-import {moduleName} from './module-name';
+import { moduleName } from './module-name';
 import BroadcastService from "./broadcast-service";
 
-export default class AddButtonService{
+export default class AddButtonService {
     addButtons: any = {};
-        NEW_ADD_BUTTON_EVENT: any = 'newAddButton';
-        HIDE_ADD_BUTTON_EVENT: any = 'hideAddButton';
-        SHOW_ADD_BUTTON_EVENT: any = 'showAddButton';
+    NEW_ADD_BUTTON_EVENT: any = 'newAddButton';
+    HIDE_ADD_BUTTON_EVENT: any = 'hideAddButton';
+    SHOW_ADD_BUTTON_EVENT: any = 'showAddButton';
+    static readonly $inject = ["BroadcastService"]
+    constructor(private broadcastService: BroadcastService) { }
+    AddButtonServiceTag() { }
 
- constructor(private broadcastService: BroadcastService){}
- AddButtonServiceTag() {}
+    //__tag = new AddButtonServiceTag();
+    __tag = this.AddButtonServiceTag();
 
-        //__tag = new AddButtonServiceTag();
-         __tag = this.AddButtonServiceTag();
-        
-        registerAddButton = (state: any, action: any)=> {
-            this.addButtons[state] = action;
-            this.broadcastService.notify(this.NEW_ADD_BUTTON_EVENT, state);
-        }
-        hideAddButton = ()=> {
-            this.broadcastService.notify(this.HIDE_ADD_BUTTON_EVENT);
-        }
-        showAddButton = ()=> {
-            this.broadcastService.notify(this.SHOW_ADD_BUTTON_EVENT);
-        }
-        isShowAddButton =  (state: any)=> {
-           return this.addButtons[state] != undefined;
-        }
-        unregisterAddButton =  (state: any) =>{
-            this.addButtons[state];
-        }
-        onClick = (state: any) =>{
-            var action = this.addButtons[state];
-            if (action) {
-                action();
-            }
+    registerAddButton = (state: any, action: any) => {
+        this.addButtons[state] = action;
+        this.broadcastService.notify(this.NEW_ADD_BUTTON_EVENT, state);
+    }
+    hideAddButton = () => {
+        this.broadcastService.notify(this.HIDE_ADD_BUTTON_EVENT);
+    }
+    showAddButton = () => {
+        this.broadcastService.notify(this.SHOW_ADD_BUTTON_EVENT);
+    }
+    isShowAddButton = (state: any) => {
+        return this.addButtons[state] != undefined;
+    }
+    unregisterAddButton = (state: any) => {
+        this.addButtons[state];
+    }
+    onClick = (state: any) => {
+        var action = this.addButtons[state];
+        if (action) {
+            action();
         }
     }
- angular.module(moduleName)
- .service('AddButtonService', ["BroadcastService", AddButtonService]);
+}
+angular.module(moduleName).service('AddButtonService', AddButtonService);
