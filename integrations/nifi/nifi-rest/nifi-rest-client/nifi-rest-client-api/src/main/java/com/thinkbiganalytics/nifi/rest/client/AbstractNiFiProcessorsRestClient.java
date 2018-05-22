856f5722eab396e2aa06cae4a8b3d1c1a3f51059
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2018-05-21 21:23:55 -0700

    fix spelling of a method

--- integrations/nifi/nifi-rest/nifi-rest-client/nifi-rest-client-api/src/main/java/com/thinkbiganalytics/nifi/rest/client/AbstractNiFiProcessorsRestClient.java
+++ integrations/nifi/nifi-rest/nifi-rest-client/nifi-rest-client-api/src/main/java/com/thinkbiganalytics/nifi/rest/client/AbstractNiFiProcessorsRestClient.java
@@ -76,7 +76,7 @@ public abstract class AbstractNiFiProcessorsRestClient implements NiFiProcessors
         return findById(processor.getParentGroupId(), processor.getId())
             .map(initState -> {
                 ProcessorDTO current = initState;
-                List<Function<ProcessorDTO, ProcessorDTO>> wakeSequence = genrateWakeSequence(current);
+                List<Function<ProcessorDTO, ProcessorDTO>> wakeSequence = generateWakeSequence(current);
                 
                 for (Function<ProcessorDTO, ProcessorDTO> transition : wakeSequence) {
                     current = transition.apply(createProcessor(current));
@@ -87,7 +87,7 @@ public abstract class AbstractNiFiProcessorsRestClient implements NiFiProcessors
             .orElseThrow(() -> new NifiComponentNotFoundException(processor.getId(), NifiConstants.NIFI_COMPONENT_TYPE.PROCESSOR, null));
     }
 
-    private List<Function<ProcessorDTO, ProcessorDTO>> genrateWakeSequence(ProcessorDTO originalProc) {
+    private List<Function<ProcessorDTO, ProcessorDTO>> generateWakeSequence(ProcessorDTO originalProc) {
         List<Function<ProcessorDTO, ProcessorDTO>> sequence = new ArrayList<>();
         PROCESS_STATE state = PROCESS_STATE.valueOf(originalProc.getState().toUpperCase());
         SCHEDULE_STRATEGIES strategy = SCHEDULE_STRATEGIES.valueOf(originalProc.getConfig().getSchedulingStrategy().toUpperCase());
