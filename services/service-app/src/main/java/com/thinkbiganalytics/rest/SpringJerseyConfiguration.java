1c3ded927aafdc9e1dc819da4dc8d3d745d5f01f
Jeremy Merrifield <jeremy.merrifield@thinkbiganalytics.com>
2018-05-22 09:45:21 -0700

    KYLO-1804 - Spring and Spring Boot upgrade refactoring

--- services/service-app/src/main/java/com/thinkbiganalytics/rest/SpringJerseyConfiguration.java
+++ services/service-app/src/main/java/com/thinkbiganalytics/rest/SpringJerseyConfiguration.java
@@ -22,7 +22,7 @@ package com.thinkbiganalytics.rest;
 
 import org.glassfish.jersey.servlet.ServletContainer;
 import org.glassfish.jersey.servlet.ServletProperties;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
