d2e081ce08faf0b7c86db23cd78133c6717eae44
Aakifa <aakifa.ishtiaq@teradata.com>
2018-05-24 10:36:06 +0500

    file-upload.ts

--- ui/ui-app/src/main/resources/static/js/common/file-upload/file-upload.ts
+++ ui/ui-app/src/main/resources/static/js/common/file-upload/file-upload.ts
@@ -4,28 +4,27 @@ import {moduleName} from "../module-name";
 
 export default class UploadFile {
 
+    uploadFileModel: any;
     isMultiple: any;
     buttonText: any;
     fileNames: any;
 
-    static readonly $inject = ["$scope", "$element", "$parse", "$attrs"];
+    $onInit() {
+        this.ngOnInit();
+    }
 
-    constructor(private $scope: IScope, 
-                private $element: JQuery,
-                private $parse: angular.IParseService,
-                private $attrs: any) {
+    ngOnInit() {
+        var input = $(this.$element[0].querySelector('#fileInput'));
+        var button = $(this.$element[0].querySelector('#uploadButton'));
+        var textInput = $(this.$element[0].querySelector('#textInput'));
 
-        var input = $($element[0].querySelector('#fileInput'));
-        var button = $($element[0].querySelector('#uploadButton'));
-        var textInput = $($element[0].querySelector('#textInput'));
-
-        this.isMultiple = 'multiple' in $attrs;
+        this.isMultiple = 'multiple' in this.$attrs;
         if (this.isMultiple) {
             input.prop('multiple', true);
         }
         this.buttonText = this.isMultiple ? 'Choose files' : 'Choose file'
 
-        var size = $attrs.inputSize;
+        var size = this.$attrs.inputSize;
         if (size != null) {
             try {
                 size = parseInt(size);
@@ -33,9 +32,14 @@ export default class UploadFile {
             } catch (e) {
             }
         }
-        var model = $parse($attrs.uploadFileModel);
-        var modelSetter = model.assign;
-        var isModelArray = _.isArray(model);
+        if(angular.isDefined(this.uploadFileModel) && this.uploadFileModel != null){
+            if(_.isArray(this.uploadFileModel)){
+                this.fileNames = this.uploadFileModel.map((f :any) => f.name).join(', ');
+            }
+            else {
+                this.fileNames = this.uploadFileModel.name;
+            }
+        }
 
         if (input.length && button.length && textInput.length) {
             button.click((e: any)=>{
@@ -49,32 +53,49 @@ export default class UploadFile {
         input.on('change', (e: any)=>{
             var files = _.values(e.target.files);
 
-            this.fileNames = files.map(f => f.name).join(', ');
+            this.fileNames = files.map((f:any) => f.name).join(', ');
 
             if (this.fileNames !== '') {
                 button.removeClass('md-primary')
             } else {
                 button.addClass('md-primary')
             }
-            $scope.$apply(()=>{
-                if(isModelArray) {
-                    modelSetter($scope, files);
+            if(files && files.length >0) {
+            
+                if (_.isArray(this.uploadFileModel)) {
+                    this.uploadFileModel = files;
                 }
                 else {
-                    modelSetter($scope, files[0]);
+                    this.uploadFileModel = files[0];
                 }
-            });
-        });
+            
+            }
+            else {
+                this.uploadFileModel = null
+            }
 
+        });
     }
 
+    static readonly $inject = ["$element", "$parse", "$attrs"];
+
+    constructor(private $element: JQuery,
+                private $parse: angular.IParseService,
+                private $attrs: any) {
+
+        
+        
+    }
 }
 
 angular.module(moduleName).component('uploadFile', {
     controller: UploadFile,
+	bindings: {
+		uploadFileModel: '='
+    },
     template: '<input id="fileInput" type="file" class="ng-hide">' +
-                '<md-button id="uploadButton" class="md-raised md-primary" aria-label="attach_file">{{"views.file-upload.btn-Choose" | translate}}</md-button>' +
-                '<md-input-container class="condensed-no-float" md-no-float  flex>' +
-                '<input id="textInput" size="40" ng-model="$ctrl.fileNames" type="text" placeholder="{{\'views.file-upload.placeholder\' | translate}}" ng-readonly="true" style="margin-top: 20px;">' +
-                '</md-input-container>'
+                      '<md-button id="uploadButton" class="md-raised md-primary" aria-label="attach_file">{{"views.file-upload.btn-Choose" | translate}}</md-button>' +
+                      '<md-input-container class="condensed-no-float" md-no-float  flex>' +
+                        '<input id="textInput" size="40" ng-model="$ctrl.fileNames" type="text" placeholder="{{\'views.file-upload.placeholder\' | translate}}" ng-readonly="true" style="margin-top: 20px;">' +
+                      '</md-input-container>'
 });
