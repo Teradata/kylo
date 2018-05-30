cc0d80b4c958fcbdc03a71dd299cba1ff196c40b
Gregory Hart <greg.hart@thinkbiganalytics.com>
2018-05-30 16:44:23 -0700

    Removed compiled JavaScript files

--- ui/ui-app/src/main/resources/static/js/services/module-require.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", "angular", "./module-name", "jquery", "./AccessControlService", "./StateService", "./SearchService", "./broadcast-service", "./CommonRestUrlService", "./PaginationDataService", "./SideNavService", "./TableOptionsService", "./ConfigurationService", "./AddButtonService", "./FileUploadService", "./WindowUnloadService", "./Utils", "./HttpService", "./notification.service", "./AngularHttpInterceptor", "./UserGroupService", "./AngularModuleExtensionService", "./BroadcastConstants"], function (require, exports, angular, module_name_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.moduleRequire = angular.module(module_name_1.moduleName);
-});
-//# sourceMappingURL=module-require.js.map
--- ui/ui-app/src/main/resources/static/js/services/module.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", "angular", "./module-name"], function (require, exports, angular, module_name_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.module = angular.module(module_name_1.moduleName, []);
-});
-//# sourceMappingURL=module.js.map
