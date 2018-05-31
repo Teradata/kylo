ddab91d8b7d8f90e4023bd4effd201a559898fa7
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2018-05-31 17:08:13 +0100

    KYLO-2088 Any to Any Database Browse UI Component, fix common selection dialog

---
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/catalog/datasource/api/browser.module.ts
@@ -0,0 +1,51 @@
+import {CommonModule} from "@angular/common";
+import {NgModule} from "@angular/core";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatListModule} from "@angular/material/list";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatSelectModule} from '@angular/material/select';
+import {CovalentFileModule} from "@covalent/core/file";
+import {CovalentSearchModule} from "@covalent/core/search";
+import {CovalentPagingModule} from '@covalent/core/paging';
+
+import {KyloCommonModule} from "../../../../common/common.module";
+import {CovalentDataTableModule} from '@covalent/core/data-table';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {CovalentNotificationsModule} from '@covalent/core/notifications';
+import {MatDialogModule} from '@angular/material/dialog';
+import {SelectionDialogComponent} from './dialog/selection-dialog.component';
+
+@NgModule({
+    declarations: [
+        SelectionDialogComponent,
+    ],
+    entryComponents: [
+        SelectionDialogComponent,
+    ],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        CovalentDataTableModule,
+        CommonModule,
+        CovalentFileModule,
+        CovalentSearchModule,
+        CovalentPagingModule,
+        CovalentNotificationsModule,
+        FlexLayoutModule,
+        KyloCommonModule,
+        MatButtonModule,
+        MatCardModule,
+        MatDividerModule,
+        MatListModule,
+        MatProgressBarModule,
+        MatSelectModule,
+        MatCheckboxModule,
+        MatDialogModule,
+    ]
+})
+export class BrowserModule {
+}
