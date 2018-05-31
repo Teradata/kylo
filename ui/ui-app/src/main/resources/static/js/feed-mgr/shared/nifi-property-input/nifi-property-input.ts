b7e8ff4e8fb2b4eecfd268362d1a324b591c1cf8
Ahmad Hassan <ahmad.hassan@teradata.com>
2018-05-31 20:43:07 +0500

    attribute directive which was reverted back by me

--- ui/ui-app/src/main/resources/static/js/feed-mgr/shared/nifi-property-input/nifi-property-input.ts
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/shared/nifi-property-input/nifi-property-input.ts
@@ -2,81 +2,91 @@ import * as angular from 'angular';
 import * as _ from "underscore";
 const moduleName = require('feed-mgr/module-name');
 
-var directive = function () {
-    return {
-        restrict: "EA",
-        scope: {
-            property:'=',
-            theForm:'=',
-            propertyDisabled:'=?',
-            onPropertyChange:'&?'
-        },
-        templateUrl: 'js/feed-mgr/shared/nifi-property-input/nifi-property-input.html',
-        link: function ($scope:any, element:any, attrs:any) {
-            element.addClass('nifi-property-input layout-padding-top-bottom')
-            if($scope.property.formKey == null) {
-                var formKey = $scope.property.key.split(' ').join('_')+$scope.property.processorName.split(' ').join('_')
-                $scope.property.formKey = formKey.toLowerCase();
-            }
-            if($scope.propertyDisabled == undefined){
-                $scope.propertyDisabled = false;
-            }
 
-            $scope.onPropertyChanged = function(){
-                if($scope.onPropertyChange != undefined){
-                    $scope.onPropertyChange($scope.property);
-                }
+class controller {
+
+    property:any;
+    theForm: any;
+    propertyDisabled: any;
+    onPropertyChange: any;
+
+    static readonly $inject = ["$scope", "element"];
+    constructor(private $scope: IScope, private element: angular.IAugmentedJQuery) {
+        element.addClass('nifi-property-input layout-padding-top-bottom')
+        if (this.property.formKey == null) {
+            var formKey = this.property.key.split(' ').join('_') + this.property.processorName.split(' ').join('_')
+            this.property.formKey = formKey.toLowerCase();
+        }
+        if (this.propertyDisabled == undefined) {
+            this.propertyDisabled = false;
+        }
+
+        
+
+        if (this.property.renderType == 'select' && this.property.value != null) {
+            if (this.onPropertyChange != undefined) {
+                this.onPropertyChange(this.property);
             }
+        }
 
-            if( $scope.property.renderType == 'select' && $scope.property.value != null) {
-                if($scope.onPropertyChange != undefined){
-                    $scope.onPropertyChange($scope.property);
-                }
+        if (this.property.renderType == 'select') {
+            if (this.property.renderOptions == null || this.property.renderOptions == undefined) {
+                this.property.renderOptions = {};
             }
-
-            if( $scope.property.renderType == 'select'){
-                if($scope.property.renderOptions == null || $scope.property.renderOptions == undefined){
-                    $scope.property.renderOptions ={};
+            if (this.property.renderOptions['selectCustom'] == 'true') {
+                if (this.property.renderOptions['selectOptions']) {
+                    this.property.selectOptions = angular.fromJson(this.property.renderOptions['selectOptions']);
                 }
-                if($scope.property.renderOptions['selectCustom'] == 'true' ) {
-                    if($scope.property.renderOptions['selectOptions']){
-                        $scope.property.selectOptions = angular.fromJson($scope.property.renderOptions['selectOptions']);
-                    }
-                    else {
-                        $scope.property.selectOptions = [];
-                    }
-                }
-            }
-
-
-            if( $scope.property.renderType == 'checkbox-custom' ) {
-                if($scope.property.renderOptions == null || $scope.property.renderOptions == undefined){
-                    $scope.property.renderOptions ={};
-                }
-                var trueValue =  $scope.property.renderOptions['trueValue'];
-                if(StringUtils.isBlank(trueValue)){
-                    $scope.property.renderOptions['trueValue'] = 'true';
-                }
-                var falseValue =  $scope.property.renderOptions['falseValue'];
-                if(StringUtils.isBlank(falseValue)){
-                    $scope.property.renderOptions['falseValue'] = 'false';
+                else {
+                    this.property.selectOptions = [];
                 }
             }
         }
 
-    };
-};
 
-var emptyStringToNull = function () {
+        if (this.property.renderType == 'checkbox-custom') {
+            if (this.property.renderOptions == null || this.property.renderOptions == undefined) {
+                this.property.renderOptions = {};
+            }
+            var trueValue = this.property.renderOptions['trueValue'];
+            if (StringUtils.isBlank(trueValue)) {
+                this.property.renderOptions['trueValue'] = 'true';
+            }
+            var falseValue = this.property.renderOptions['falseValue'];
+            if (StringUtils.isBlank(falseValue)) {
+                this.property.renderOptions['falseValue'] = 'false';
+            }
+        }
+    }
+    onPropertyChanged = (property:any) => {
+        if (this.onPropertyChange != undefined) {
+            this.onPropertyChange(this.property);
+        }
+    }
+}
+
+angular.module(moduleName).component('nifiPropertyInput', {
+    bindings: {
+        property: '=',
+        theForm: '=',
+        propertyDisabled: '=?',
+        onPropertyChange: '&?'
+    },
+    controller : controller,
+    controllerAs : "vm",
+    templateUrl: 'js/feed-mgr/shared/nifi-property-input/nifi-property-input.html',
+});
+
+var emptyStringToNull = () => {
     // There is a distinction in Nifi between empty string and null, e.g. some
     // processors consider empty string invalid where null is valid. Without
     // this directive it would be impossible to set existing values to null
     return {
         restrict: 'EA',
         require: '?ngModel',
-        link: function ($scope:any, element:any, attr:any, ngModel:any) {
+        link: ($scope: any, element: any, attr: any, ngModel: any) => {
             if (ngModel) {
-                const replaceEmptyStringWithNull = function (value:any) {
+                const replaceEmptyStringWithNull = function (value: any) {
                     return value === '' ? null : value;
                 };
                 ngModel.$parsers.push(replaceEmptyStringWithNull);
@@ -86,7 +96,5 @@ var emptyStringToNull = function () {
 };
 
 angular.module(moduleName).directive('emptyStringToNull', emptyStringToNull);
-
-angular.module(moduleName).directive('nifiPropertyInput', directive);
 
 
