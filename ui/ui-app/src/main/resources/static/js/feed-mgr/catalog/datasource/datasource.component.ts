54fc66d06a5a965a338e4e68ad6bfa7f6b546ae4
Ruslans Uralovs <ruslans.uralovs@thinkbiganalytics.com>
2018-06-11 11:08:48 +0100

    KYLO-2226 Any to Any Browser: Multiple Selection as Option, disable Preview until there is a selection

--- ui/ui-app/src/main/resources/static/js/feed-mgr/catalog/datasource/datasource.component.ts
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/catalog/datasource/datasource.component.ts
@@ -6,6 +6,7 @@ import {StateRegistry, StateService} from "@uirouter/angular";
 import {ConnectorTab} from "../api/models/connector-tab";
 import {DataSource} from '../api/models/datasource';
 import {SelectionService} from '../api/services/selection.service';
+import {Node} from '../api/models/node'
 
 /**
  * Displays tabs for configuring a data set (or connection).
@@ -29,12 +30,12 @@ export class DatasourceComponent implements OnInit {
      */
     tabs: ConnectorTab[] = [];
 
-    constructor(private state: StateService, private stateRegistry: StateRegistry, private selection: SelectionService) {
+    constructor(private state: StateService, private stateRegistry: StateRegistry, private selectionService: SelectionService) {
     }
 
     public ngOnInit() {
         // Add tabs and register router states
-        this.selection.reset(this.datasource.id);
+        this.selectionService.reset(this.datasource.id);
         if (this.datasource.connector.tabs) {
             this.tabs = angular.copy(this.datasource.connector.tabs);
             for (let tab of this.tabs) {
@@ -49,5 +50,15 @@ export class DatasourceComponent implements OnInit {
 
         // Go to the first tab
         this.state.go(this.tabs[0].sref, {}, {location: "replace"});
+    }
+
+    isDisabled(tab: ConnectorTab) {
+        let disabled = false;
+        if (tab.sref === ".preview") {
+            //disable preview until there is a selection
+            const root: Node = <Node>this.selectionService.get(this.datasource.id);
+            disabled = root ? root.countSelectedDescendants() === 0 : false;
+        }
+        return disabled;
     }
 }
