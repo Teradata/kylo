25eaa7cf16d72aa3c2eccc316a7636f6221371ce
Gregory Hart <greg.hart@thinkbiganalytics.com>
2018-08-17 16:01:24 -0700

    Remove extra files from KYLO-2244

--- ui/ui-app/src/main/resources/static/js/common/lazy-load/angular1.ts
+++
@@ -1,8 +0,0 @@
-import {downgradeComponent} from "@angular/upgrade/static";
-import * as angular from "angular";
-
-import {moduleName} from "../module-name";
-import {LazyLoadComponent} from "./lazy-load.component";
-
-angular.module(moduleName)
-    .directive("lazyLoad", downgradeComponent({component: LazyLoadComponent}) as angular.IDirectiveFactory);
