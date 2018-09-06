70ddbaf64fc4883c29f779e3a01a2554d8ecbf1d
Ahmad Hassan <ahmad.hassan@teradata.com>
2018-09-06 11:00:34 +0500

    fixed GDC-54 and made changes to filteredPaginatedTableView.component according to specifications

--- ui/ui-app/src/main/resources/static/js/common/filtered-paginated-table-view/filteredPaginatedTableView.component.ts
+++ ui/ui-app/src/main/resources/static/js/common/filtered-paginated-table-view/filteredPaginatedTableView.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ContentChildren, Input, QueryList, ViewChild} from "@angular/core";
+import {AfterViewInit, Component, ContentChildren, Input, QueryList, ViewChild, Output, EventEmitter} from "@angular/core";
 import {ITdDataTableColumn, ITdDataTableSortChangeEvent, TdDataTableComponent, TdDataTableSortingOrder, TdDataTableTemplateDirective} from "@covalent/core/data-table";
 import {IPageChangeEvent} from "@covalent/core/paging";
 
@@ -10,43 +10,48 @@ export class FilteredPaginatedTableViewComponent implements AfterViewInit {
 
     @ContentChildren(TdDataTableTemplateDirective) templates: QueryList<TdDataTableTemplateDirective>;
     @Input() cardTitle: string = "";
-    @Input() onSearchDebounce: any;
     @Input() filteredData: any[];
     @Input() columns: ITdDataTableColumn[];
     @Input() clickable: boolean = true;
     @Input() sortable: boolean = true;
     @Input() sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Ascending;
     @Input() sortBy: string = "";
-    @Input() onSortChange: any;
-    @Input() onRowClick: any;
     @Input() firstLast: boolean = false;
     @Input() pageSize: number = 5;
     @Input() filteredTotal: number = 0;
-    @Input() onPageSizeChange: any;
     @Input() allowedPageSize: number[] = [5, 10, 20, 50];
     @ViewChild(TdDataTableComponent) dataTable: TdDataTableComponent;
     @Input() showToolbar: boolean = true;
 
+    @Output() searchDebounced: EventEmitter<any> = new EventEmitter<string>();
+    @Output() sortChanged:  EventEmitter<any> = new EventEmitter<string>();
+    @Output() rowClicked:  EventEmitter<any> = new EventEmitter<string>();
+    @Output() pageSizeChanged:  EventEmitter<any> = new EventEmitter<string>();
+
     pageSizeChange(pagingEvent: IPageChangeEvent): void {
-        if(typeof this.onPageSizeChange === 'function' && pagingEvent){
-            this.onPageSizeChange(pagingEvent);
-        }
+        // if(typeof this.onPageSizeChange === 'function' && pagingEvent){
+        //     this.onPageSizeChange(pagingEvent);
+        // }
+        this.pageSizeChanged.emit(pagingEvent);
     }
 
     search(searchTerm: string): void {
-        this.onSearchDebounce(searchTerm);
+        // this.onSearchDebounce(searchTerm);
+        this.searchDebounced.emit(searchTerm);
     }
 
     sort(sortEvent: ITdDataTableSortChangeEvent): void {
-        if(typeof this.onSortChange === 'function' && sortEvent){
-            this.onSortChange(sortEvent);
-        }
+        // if(typeof this.onSortChange === 'function' && sortEvent){
+        //     this.onSortChange(sortEvent);
+        // }
+        this.sortChanged.emit(sortEvent);
     }
 
-    rowClicked(clickEvent: any) {
-        if(typeof this.onRowClick === 'function' && clickEvent){
-            this.onRowClick(clickEvent);
-        }
+    onRowClick(clickEvent: any) {
+        // if(typeof this.onRowClick === 'function' && clickEvent){
+        //     this.onRowClick(clickEvent);
+        // }
+        this.rowClicked.emit(clickEvent);
     }
 
     ngAfterViewInit() {
