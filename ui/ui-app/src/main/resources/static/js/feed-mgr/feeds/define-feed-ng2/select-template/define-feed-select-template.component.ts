21bc127df5b80f28bc8f4b3e2801f3cb50f89d2e
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2018-09-10 06:55:01 -0700

    KYLO-2523 style new feed dialog

--- ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/define-feed-ng2/select-template/define-feed-select-template.component.ts
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/define-feed-ng2/select-template/define-feed-select-template.component.ts
@@ -320,7 +320,7 @@ export class DefineFeedSelectTemplateComponent implements OnInit {
      * @returns the date format string
      */
     openNewFeedDialog(config: NewFeedDialogData): Observable<NewFeedDialogResponse> {
-        return this.dialog.open(NewFeedDialogComponent, {data: config, panelClass: "full-screen-dialog",height:'100%',width:'800px'})
+        return this.dialog.open(NewFeedDialogComponent, {data: config, panelClass: "full-screen-dialog",width:'800px'})
             .afterClosed()
             .filter(value => typeof value !== "undefined");
     }
