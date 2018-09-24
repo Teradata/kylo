7a5bddb8e280e13a70ce28528d88c610c2283203
Sean Felten <sean.felten@thinkbiganalytics.com>
2018-09-24 15:10:38 -0700

    Begin 0.9.1.3-SNAPSHOT

--- ui/ui-app/src/main/resources/static/js/systemjs.config.js
+++ ui/ui-app/src/main/resources/static/js/systemjs.config.js
@@ -2,7 +2,7 @@
 var systemLocate = System.locate;
 System.locate = function (load) {
     return Promise.resolve(systemLocate.call(this, load)).then(function (address) {
-        return address + "?ver=0.9.1-2"
+        return address + "?ver=0.9.1-3-SNAPSHOT"
     });
 };
 
