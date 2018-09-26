f58d8e74de051effb34d99e7585d3bbfa5a82e9e
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2018-09-26 06:03:52 -0700

    TBK-143 (color scheme update)

--- ui/ui-app/src/main/resources/static/assets/env.js
+++ ui/ui-app/src/main/resources/static/assets/env.js
@@ -1,20 +1,9 @@
 (function (window) {
     window.__env = window.__env || {};
     var themes = {
-        "definePalette":[ {name:'primaryBlue',details:{ '500': '3483BA',
-                '900':'2B6C9A'},extend:"blue"},
-            {name:'accentOrange',details:{'A200': 'F08C38'},extend:"orange"},
-        ],
-        "primaryPalette": {
-            name:"primaryBlue",
-            details:{ 'hue-2':'900'}
-        },
-        "accentPalette": {
-            name:"accentOrange"
-        },
-        //,
-        "warnPalette": {
-        },
+        "primaryPalette":{name:'teal',details:{ 'default':'700'}},
+        "accentPalette": {name:'orange',details:{'default':'800','hue-1':'A100','hue-2': 'A400'}},
+        "warnPalette": {name:'red',details:{'default':'600'}},
         "backgroundPalette": { }
     }
     window.__env.themes = themes;
