2c212c54f534ae5b80024422add88de137d7bbdb
Tim Harsch <tim.harsch@thinkbiganalytics.com>
2018-10-04 14:35:20 -0700

    KYLO-2694: fix issues with some formulas in formula bar

--- commons/commons-spark/commons-spark-livy-2.10/src/main/scala/com/thinkbiganalytics/spark/utils/LivyPaginator.scala
+++ commons/commons-spark/commons-spark-livy-2.10/src/main/scala/com/thinkbiganalytics/spark/utils/LivyPaginator.scala
@@ -1,8 +1,8 @@
 package com.thinkbiganalytics.spark.utils
 
 import com.thinkbiganalytics.spark.logger.LivyLogger
-import org.apache.spark.sql.functions.col
 import org.slf4j.LoggerFactory
+import org.apache.spark.sql.types.StructType
 
 object LivyPaginator {
   val logger = LoggerFactory.getLogger(LivyPaginator.getClass)
@@ -15,22 +15,18 @@ object LivyPaginator {
     LivyLogger.time {
       actualCols = df.columns.length
 
-      val lastCol = actualCols - 1
-      val dfStartCol = if (lastCol >= startCol) startCol else lastCol
-      val dfStopCol = if (lastCol >= stopCol) stopCol else lastCol
+      val schema = StructType(df.schema.slice(startCol, stopCol));
+      val dfAsArray = df.collect
 
-      val df2 = df.select(dfStartCol to dfStopCol map df.columns map col: _*)
-      val dl = df2.collect
-
-      actualRows = dl.size
-
-      df.columns.slice(dfStartCol, dfStopCol)
+      actualRows = dfAsArray.size
 
       val dfStartRow = if (actualRows >= pageStart) pageStart else actualRows
       val dfStopRow = if (actualRows >= pageStop) pageStop else actualRows
 
-      val pagedRows = dl.slice(dfStartRow, dfStopRow).map(_.toSeq)
-      dfRows = List(df2.schema.json, pagedRows)
+      val pagedRows = dfAsArray.slice(dfStartRow, dfStopRow)  // slice the rows
+          .map(_.toSeq.slice(startCol, stopCol)) // slice the columns
+
+      dfRows = List(schema.json, pagedRows)
     }
     return List(dfRows, actualCols, actualRows)
   }
