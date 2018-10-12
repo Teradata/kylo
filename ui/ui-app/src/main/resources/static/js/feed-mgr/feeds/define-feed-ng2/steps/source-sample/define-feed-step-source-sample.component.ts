5c0defd50ca98e9bb28c928c226a62b8fdac667b
scottreisdorf <scott.reisdorf@thinkbiganalytics.com>
2018-10-12 13:21:56 -0700

    fix step selection in source sample

--- ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/define-feed-ng2/steps/source-sample/define-feed-step-source-sample.component.ts
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/feeds/define-feed-ng2/steps/source-sample/define-feed-step-source-sample.component.ts
@@ -87,7 +87,7 @@ export class DefineFeedStepSourceSampleComponent implements OnInit, OnDestroy{
         else {
             this.selectionService.singleSelectionStrategy();
         }
-        this.step = this.feed.steps.find(step => step.systemName == FeedStepConstants.STEP_FEED_SOURCE);
+        this.step = this.feed.steps.find(step => step.systemName == FeedStepConstants.STEP_FEED_TARGET);
         this.step.visited = true;
 
 
