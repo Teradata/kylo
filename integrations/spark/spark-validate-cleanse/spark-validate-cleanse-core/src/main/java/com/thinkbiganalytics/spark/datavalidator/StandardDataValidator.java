3c7321e0122d4eee0c919e2cef072b3c633a33d8
Jagrut Sharma <jagrut.sharma@thinkbiganalytics.com>
2018-11-08 08:07:12 -0800

    KYLO-2728 Unit test for empty result set

--- integrations/spark/spark-validate-cleanse/spark-validate-cleanse-core/src/main/java/com/thinkbiganalytics/spark/datavalidator/StandardDataValidator.java
+++ integrations/spark/spark-validate-cleanse/spark-validate-cleanse-core/src/main/java/com/thinkbiganalytics/spark/datavalidator/StandardDataValidator.java
@@ -44,7 +44,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nonnull;
 
