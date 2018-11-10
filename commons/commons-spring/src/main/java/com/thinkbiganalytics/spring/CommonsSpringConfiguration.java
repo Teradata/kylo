6dba3ee8c3cb2479e1daf63919d6d7c569378a94
Tim Harsch <tim.harsch@thinkbiganalytics.com>
2018-11-09 17:34:51 -0800

    KYLO-3021: fix file headers in new spring module and commons jdbc

--- commons/commons-spring/src/main/java/com/thinkbiganalytics/spring/CommonsSpringConfiguration.java
+++ commons/commons-spring/src/main/java/com/thinkbiganalytics/spring/CommonsSpringConfiguration.java
@@ -2,16 +2,16 @@ package com.thinkbiganalytics.spring;
 
 /*-
  * #%L
- * thinkbig-commons-util
+ * kylo-commons-spring
  * %%
- * Copyright (C) 2017 ThinkBig Analytics
+ * Copyright (C) 2017 - 2018 ThinkBig Analytics, a Teradata Company
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package com.thinkbiganalytics.spring;
  * limitations under the License.
  * #L%
  */
+
 
 import com.thinkbiganalytics.UsernameCaseStrategyUtil;
 
