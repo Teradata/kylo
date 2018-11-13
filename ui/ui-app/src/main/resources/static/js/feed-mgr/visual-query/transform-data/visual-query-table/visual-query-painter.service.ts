d9929c7a3a0f07955ee5401166731067a1b7f1b2
matt-hutton <matt.hutton@thinkbiganalytics.com>
2018-11-13 10:35:59 -0800

    firefox support for wrangler cell selection

--- ui/ui-app/src/main/resources/static/js/feed-mgr/visual-query/transform-data/visual-query-table/visual-query-painter.service.ts
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/visual-query/transform-data/visual-query-table/visual-query-painter.service.ts
@@ -461,7 +461,8 @@ export class VisualQueryPainterService extends fattable.Painter {
         const isNull = cell.hasClass("null");
         const selection = this.$window.getSelection();
 
-        if (event.button != 0 || !(selection.extentNode.nodeName == '#text') || !(this.selectedCell == event.target || $.contains(this.selectedCell, event.target))) {
+        if (event.button != 0 || !(selection.focusNode.nodeType == 3) || !(this.selectedCell == event.target || $.contains(this.selectedCell, event.target))) {
+            selection.clear();
             return;
         }
 
