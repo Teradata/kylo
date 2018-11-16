b18cdc827963e5226f33e363e46f625e73697e55
Tim Harsch <tim.harsch@thinkbiganalytics.com>
2018-11-16 14:44:14 -0800

    KYLO-3052: improve handling of missing profiles on server startup

--- services/service-app/src/main/java/com/thinkbiganalytics/server/KyloServerApplication.java
+++ services/service-app/src/main/java/com/thinkbiganalytics/server/KyloServerApplication.java
@@ -20,10 +20,12 @@ package com.thinkbiganalytics.server;
  * #L%
  */
 
+import com.google.common.collect.Sets;
 import com.thinkbiganalytics.KyloVersion;
 import com.thinkbiganalytics.KyloVersionUtil;
 import com.thinkbiganalytics.metadata.config.OperationalMetadataConfig;
 import com.thinkbiganalytics.rest.SpringJerseyConfiguration;
+import com.thinkbiganalytics.rest.exception.profiles.KyloProfileException;
 import com.thinkbiganalytics.security.core.SecurityCoreConfig;
 import com.thinkbiganalytics.server.upgrade.KyloUpgrader;
 
@@ -32,10 +34,10 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -44,13 +46,13 @@ import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import java.util.Arrays;
+import java.util.Set;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 
 @Configuration
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableConfigurationProperties
 @Import({DatabaseConfiguration.class, OperationalMetadataConfig.class, SpringJerseyConfiguration.class, SecurityCoreConfig.class})
 @ComponentScan("com.thinkbiganalytics")
@@ -78,7 +80,9 @@ public class KyloServerApplication implements SchedulingConfigurer {
 
         // Run services
         System.setProperty(SpringApplication.BANNER_LOCATION_PROPERTY, "banner.txt");
-        SpringApplication.run("classpath:application-context.xml", args);
+        SpringApplication application = new SpringApplication("classpath:application-context.xml");
+        application.addInitializers(ctx -> validateMinProfiles(ctx));
+        application.run(args);
     }
 
     @Bean(destroyMethod = "shutdown")
@@ -102,6 +106,36 @@ public class KyloServerApplication implements SchedulingConfigurer {
                 }
             }
         };
+    }
+
+    private static void validateMinProfiles(ConfigurableApplicationContext ctx) {
+        Set<String> profiles = Sets.newHashSet(ctx.getEnvironment().getActiveProfiles());
+
+        Set<String> minProfiles = Sets.newHashSet("native", "jms-activemq");
+        if (!profiles.containsAll(minProfiles)) {
+            throw new KyloProfileException("Missing one or more of the following profiles on start up: " + minProfiles.toString());
+        }
+
+        if (profiles.contains("kylo-shell")) {
+            if (!profiles.contains("auth-spark")) {
+                throw new KyloProfileException("kylo-shell defined and auth-spark profile is missing");
+            }
+        } else {
+            if (!profiles.contains("kylo-livy")) {
+                // TODO: change error message to describe that it must be kylo-shell or kylo-livy once Livy is officially supported
+                throw new KyloProfileException("kylo-shell profile must be defined");
+            }
+        }
+
+        Set<String> searchProfiles = Sets.newHashSet("search-es", "search-esr");
+        if (Sets.intersection(profiles, searchProfiles).size() > 1) {
+            throw new KyloProfileException("Cannot specify more than one search profile on startup");
+        }
+
+        Set<String> nifiProfiles = Sets.newHashSet("nifi-v1", "nifi-v1.1", "nifi-v1.2", "nifi-v1.3", "nifi-v1.4", "nifi-v1.5");
+        if (Sets.intersection(profiles, nifiProfiles).size() != 1) {
+            throw new KyloProfileException("Must specify one and only one valid NiFi profile");
+        }
     }
 
 }
