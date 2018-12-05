6f30ee14b3af20f307817f0a909fa9037da7cd8d
Riaz Uddin <riaz.uddin@teradata.com>
2018-12-05 14:01:22 -0800

    KYLO-3173: changed default repository

--- services/repository-service/src/main/java/com/thinkbiganalytics/repository/RepositoryConfig.java
+++ services/repository-service/src/main/java/com/thinkbiganalytics/repository/RepositoryConfig.java
@@ -31,6 +31,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.core.convert.support.DefaultConversionService;
@@ -58,6 +59,7 @@ public class RepositoryConfig {
     }
 
     @Bean
+    @Profile("!kyloUpgrade")
     public RepositoryMonitor repositoryMonitor() { return new RepositoryMonitor(); }
 
     @Bean
