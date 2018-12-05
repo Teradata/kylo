c6a97924f068e59f3336caf07b40c2c832a04528
Aakifa <aakifa.ishtiaq@teradata.com>
2018-12-05 14:27:07 +0500

    painter-service header UI fixed

--- ui/ui-app/src/main/resources/static/js/feed-mgr/visual-query/transform-data/visual-query-table/call-menu.component.ts
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/visual-query/transform-data/visual-query-table/call-menu.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, ViewEncapsulation } from "@angular/core";
 @Component({
     selector: 'vs-cell-menu',
     templateUrl: 'js/feed-mgr/visual-query/transform-data/visual-query-table/cell-menu.template.html',
-    styleUrls: ['js/feed-mgr/visual-query/transform-data/visual-query-table/cell-menu.template.css'],
+    styleUrls: ['js/feed-mgr/visual-query/transform-data/visual-query-table/cell-menu.template.scss'],
     encapsulation: ViewEncapsulation.None
 })
 export class CellMenuComponent {
@@ -12,5 +12,12 @@ export class CellMenuComponent {
     @Input("selection") selection : any;
     @Input("value") value : any;
     @Input("header") header : any;
+
+    @Input("DataCategory") DataCategory : any;
+    @Input("selectionDisplay") selectionDisplay : any;
+    @Input("range") range : any;
+
+    @Input("table") table : any;
+    @Input("displayValue") displayValue : any;
     
 }
