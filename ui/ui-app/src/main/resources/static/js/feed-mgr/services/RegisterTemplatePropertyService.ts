9b2841e8458e74c89471ce9f46c2043823be56d8
Gregory Hart <greg.hart@thinkbiganalytics.com>
2019-01-23 10:10:48 -0800

    KYLO-3254 Display metadata properties in autocomplete for processor values

--- ui/ui-app/src/main/resources/static/js/feed-mgr/services/RegisterTemplatePropertyService.ts
+++ ui/ui-app/src/main/resources/static/js/feed-mgr/services/RegisterTemplatePropertyService.ts
@@ -127,6 +127,7 @@ export class RegisterTemplatePropertyService {
     init() {
         this.getCodeMirrorTypes();
         this.fetchConfigurationProperties();
+        this.fetchMetadataProperties();
     }
 
     /**
